201be135faa6f7c97e1c856345758db9944502c8
Arne Seime <arne.seime@greenbird.com>
2015-12-04 14:20:18 +0100

    POC-95 Initial example of authenticating against keycloak server using spring security.
    TODO: 
    Secure correct URLs and/or add @Secured to endpoints
    Proper keycloak config pointing to vagrant hosted server

--- src/main/java/no/rutebanken/tiamat/filter/CorsResponseFilter.java
+++ src/main/java/no/rutebanken/tiamat/filter/CorsResponseFilter.java
@@ -13,6 +13,7 @@ public class CorsResponseFilter implements ContainerResponseFilter {
 
             headers.add("Access-Control-Allow-Origin", "*");
             headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
-            headers.add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Codingpedia");
+        	headers.add("Access-Control-Max-Age", "3600");
+            headers.add("Access-Control-Allow-Headers", "accept, authorization, X-Requested-With, Content-Type, X-Codingpedia");
         }
 }
--- src/main/java/no/rutebanken/tiamat/rest/ifopt/StopPlaceResource.java
+++ src/main/java/no/rutebanken/tiamat/rest/ifopt/StopPlaceResource.java
@@ -1,21 +1,44 @@
 package no.rutebanken.tiamat.rest.ifopt;
 
-import no.rutebanken.tiamat.ifopt.transfer.assembler.StopPlaceAssembler;
-import no.rutebanken.tiamat.ifopt.transfer.disassembler.StopPlaceDisassembler;
-import no.rutebanken.tiamat.ifopt.transfer.dto.StopPlaceDTO;
-import no.rutebanken.tiamat.repository.ifopt.StopPlaceRepository;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
+
+import org.apache.commons.lang3.StringUtils;
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.KeycloakSecurityContext;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.keycloak.representations.AccessToken;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import uk.org.netex.netex.*;
 
-import javax.ws.rs.*;
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.stream.Collectors;
+import no.rutebanken.tiamat.ifopt.transfer.assembler.StopPlaceAssembler;
+import no.rutebanken.tiamat.ifopt.transfer.disassembler.StopPlaceDisassembler;
+import no.rutebanken.tiamat.ifopt.transfer.dto.StopPlaceDTO;
+import no.rutebanken.tiamat.repository.ifopt.StopPlaceRepository;
+import uk.org.netex.netex.AirSubmodeEnumeration;
+import uk.org.netex.netex.CoachSubmodeEnumeration;
+import uk.org.netex.netex.FunicularSubmodeEnumeration;
+import uk.org.netex.netex.LocationStructure;
+import uk.org.netex.netex.MultilingualString;
+import uk.org.netex.netex.SimplePoint_VersionStructure;
+import uk.org.netex.netex.StopPlace;
+import uk.org.netex.netex.StopTypeEnumeration;
+import uk.org.netex.netex.VehicleModeEnumeration;
 
 @Component
 @Produces("application/json")
@@ -40,6 +63,24 @@ public class StopPlaceResource {
             @QueryParam(value="name") String name) {
 
 
+    	// Example reading details about authenticated user
+    	KeycloakAuthenticationToken auth = (KeycloakAuthenticationToken)SecurityContextHolder.getContext().getAuthentication();
+    	
+    	@SuppressWarnings("unchecked")
+		KeycloakPrincipal<KeycloakSecurityContext> principal = (KeycloakPrincipal<KeycloakSecurityContext>)auth.getPrincipal();
+    	AccessToken token = principal.getKeycloakSecurityContext().getToken();
+    	String email = token.getEmail();
+    	String firstname = token.getGivenName();
+    	String lastname = token.getFamilyName();
+    	String preferredUsername = token.getPreferredUsername();
+    	String agencyid = (String) token.getOtherClaims().get("agencyid");
+    	String[] representsAgencies = StringUtils.split(agencyid, ";");
+    	
+    	// all means all agencies, if not a semicolon delimited list of agencies
+    	
+    	logger.info("Logged in "+principal+" with preferred username "+preferredUsername+", name is "+firstname+" "+lastname+" and has email address "+email+" and represents agencie(s) "+representsAgencies);;
+    	
+    	
         logger.info("Get stop places with names that contains {}", name);
 
         Pageable pageable = new PageRequest(page, size);
