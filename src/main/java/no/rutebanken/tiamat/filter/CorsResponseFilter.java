806a0aa173f306a5f63c633065205c619042ac4f
Cristoffer Solem <cristoffer.solem@netlight.com>
2015-12-07 14:16:21 +0100

    POC-95 Add Authorization header to CORS

--- src/main/java/no/rutebanken/tiamat/filter/CorsResponseFilter.java
+++ src/main/java/no/rutebanken/tiamat/filter/CorsResponseFilter.java
@@ -13,6 +13,6 @@ public class CorsResponseFilter implements ContainerResponseFilter {
 
             headers.add("Access-Control-Allow-Origin", "*");
             headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
-            headers.add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Codingpedia");
+            headers.add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization");
         }
 }
