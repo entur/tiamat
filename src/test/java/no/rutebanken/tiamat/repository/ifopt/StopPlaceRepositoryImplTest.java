cdbb4f0c4eae7b1e947f7168e3387711b43f38ed
Cristoffer Solem <cristoffer.solem@netlight.com>
2015-12-14 15:07:13 +0100

    POC-90 Remove pageable (for now)

--- src/test/java/no/rutebanken/tiamat/repository/ifopt/StopPlaceRepositoryImplTest.java
+++ src/test/java/no/rutebanken/tiamat/repository/ifopt/StopPlaceRepositoryImplTest.java
@@ -7,8 +7,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import uk.org.netex.netex.SimplePoint;
@@ -48,7 +46,6 @@ public class StopPlaceRepositoryImplTest {
         stopPlace.setCentroid(centroid);
         stopPlaceRepository.save(stopPlace);
 
-        Pageable pageable = new PageRequest(0,100);
         List<StopPlace> result = stopPlaceRepository.findStopPlacesWithin(southEastLongitude, southEastLatitude, northWestLongitude, northWestLatitude);
 
         assertThat(result).isNotEmpty();
