9cb349444d5261954450502e0b7273b4f077d872
Cristoffer Solem <cristoffer.solem@netlight.com>
2016-01-11 17:28:28 +0100

    POC-130 Cleanup stop place correlation service.. Adjust logging"

--- src/main/java/no/rutebanken/tiamat/service/StopPlaceFromQuaysCorrelationService.java
+++ src/main/java/no/rutebanken/tiamat/service/StopPlaceFromQuaysCorrelationService.java
@@ -1,24 +1,20 @@
 package no.rutebanken.tiamat.service;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
+import no.rutebanken.tiamat.repository.ifopt.QuayRepository;
+import no.rutebanken.tiamat.repository.ifopt.StopPlaceRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-
-import no.rutebanken.tiamat.repository.ifopt.QuayRepository;
-import no.rutebanken.tiamat.repository.ifopt.StopPlaceRepository;
 import uk.org.netex.netex.MultilingualString;
 import uk.org.netex.netex.Quay;
 import uk.org.netex.netex.StopPlace;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @Service
 public class StopPlaceFromQuaysCorrelationService {
@@ -30,67 +26,59 @@ public class StopPlaceFromQuaysCorrelationService {
 	@Autowired
 	private StopPlaceRepository stopPlaceRepository;
 
+    private final AtomicInteger stopPlaceCounter = new AtomicInteger();
+
 
 	/**
 	 * Creates stopPlace objects based on quays by combining quays with the same
 	 * name and close location
 	 */
 	public void correlate() {
+
+        logger.trace("Loading quays from repository");
 		List<Quay> quays = quayRepository.findAll();
-		logger.debug("Sorting all quays. Total amount: {}", quays.size());
-		for (int i = 0; i < 10; i++) {
-			logger.debug(quays.get(i).getName().toString());
-		}
-		
-		List<String> usedQuays = new ArrayList<>();
+
+        logger.trace("Got {} quays", quays.size());
+
 		Map<String, List<Quay>> distinctQuays = quays.stream()
-				.collect(Collectors.groupingBy(q -> q.getName().getValue()));
-		distinctQuays.keySet().forEach(name -> {
-			StopPlace sp = new StopPlace();
-			sp.setName(new MultilingualString(name, "no", ""));
-			//sp.setQuays(distinctQuays.get(name));
-			sp.setQuays(new ArrayList<>());
-			distinctQuays.get(name).forEach(item -> {
-				logger.trace("About to add the following quay: {}. contains? {}", item.getId(), usedQuays.contains(item.getId()));
-				if (usedQuays.contains(item.getId())){
-					logger.warn("Already created QUAY WITH ID {}", item.getId());
+				.collect(Collectors.groupingBy(quay -> quay.getName().getValue()));
+
+        logger.trace("Got {} distinct quays based on name", distinctQuays.size());
+
+        List<String> quaysAlreadyProcessed = new ArrayList<>();
+
+        distinctQuays.keySet().forEach(quayGroupName -> {
+
+            logger.trace("Processing quay with name {}", quayGroupName);
+
+			StopPlace stopPlace = new StopPlace();
+			stopPlace.setName(new MultilingualString(quayGroupName, "no", ""));
+
+			stopPlace.setQuays(new ArrayList<>());
+
+            distinctQuays.get(quayGroupName).forEach(item -> {
+
+				logger.trace("About to add Quay with name {} and id {} to stop place", item.getName(), item.getId());
+				if (quaysAlreadyProcessed.contains(item.getId())){
+					logger.warn("Already created quay with name {} and id {}", item.getName(), item.getId());
 				}else{
-					sp.getQuays().add(item);
-					usedQuays.add(item.getId());
-			logger.trace("Adding quay to used quay: {}", item.getId());
+					stopPlace.getQuays().add(item);
+                    quaysAlreadyProcessed.add(item.getId());
+                    quayRepository.save(item);
 				}
 			});
+
 			try{	
-				stopPlaceRepository.save(sp);
+				stopPlaceRepository.save(stopPlace);
+                logger.trace("Created stop place number {} with name {} and id {}",
+                        stopPlaceCounter.incrementAndGet(), stopPlace.getName(), stopPlace.getId());
 			}
 			catch(Exception e){
-				logger.debug(e.getMessage());
+				logger.warn("Caught exception when saving stop place with name {}", quayGroupName, e);
 			}
-				//usedQuayIds = new ArrayList<String>(0);
-				logger.trace("Created new stop place# {}: {} with id {}", stopPlaceRepository.count(), sp.getName(),
-						sp.getId());
-				
-
 		});
 
-		// quays.stream()
-		// .map(Wrapper::new).distinct().map(Wrapper::unwrap)
-		// .forEach(item -> {
-		// List<Quay> relatedQuays = quays.stream()
-		// .filter(q ->
-		// q.getName().getValue().equals(item.getName().getValue()))
-		// .collect(Collectors.toList());
-		// relatedQuays.forEach(q -> q.setId(null));
-		// StopPlace sp = new StopPlace();
-		// sp.setName(new MultilingualString(item.getName().getValue(), "no",
-		// ""));
-		// sp.setDescription(new
-		// MultilingualString(item.getDescription().getValue(), "no", ""));
-		// sp.setQuays(relatedQuays);
-		// stopPlaceRepository.save(sp);
-		// });
-
-		logger.debug("Amount of created stop places: {}", stopPlaceRepository.count());
+		logger.debug("Amount of created stop places: {}", stopPlaceCounter.get());
 	}
 
 }
