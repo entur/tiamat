165006128b73a2cbb3a9488ab3fd15fa2fb97a17
Cristoffer Solem <cristoffer.solem@netlight.com>
2016-01-12 11:04:44 +0100

    POC-130 Check if quays are close before adding to stop place. Check if stop place has quays before saving it

--- src/main/java/no/rutebanken/tiamat/service/StopPlaceFromQuaysCorrelationService.java
+++ src/main/java/no/rutebanken/tiamat/service/StopPlaceFromQuaysCorrelationService.java
@@ -1,6 +1,7 @@
 package no.rutebanken.tiamat.service;
 
 import com.vividsolutions.jts.algorithm.CentroidPoint;
+import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.Point;
 import no.rutebanken.tiamat.pelias.CountyAndMunicipalityLookupService;
@@ -25,20 +26,33 @@ import java.util.stream.Collectors;
 public class StopPlaceFromQuaysCorrelationService {
     private static final Logger logger = LoggerFactory.getLogger(StopPlaceFromQuaysCorrelationService.class);
 
-    @Autowired
-    public QuayRepository quayRepository;
+    /**
+     * The max distance for checking if two quays are nearby each other.
+     * http://gis.stackexchange.com/questions/28799/what-is-the-unit-of-measurement-for-buffer-calculation
+     * https://en.wikipedia.org/wiki/Decimal_degrees
+     */
+    public static final double DISTANCE = 0.008;
 
-    @Autowired
-    private StopPlaceRepository stopPlaceRepository;
+    private final QuayRepository quayRepository;
+
+    private final StopPlaceRepository stopPlaceRepository;
 
     private final AtomicInteger stopPlaceCounter = new AtomicInteger();
 
-    @Autowired
-    private GeometryFactory geometryFactory;
+    private final GeometryFactory geometryFactory;
+
+    private final CountyAndMunicipalityLookupService countyAndMunicipalityLookupService;
 
     @Autowired
-    private CountyAndMunicipalityLookupService countyAndMunicipalityLookupService;
-
+    public StopPlaceFromQuaysCorrelationService(QuayRepository quayRepository,
+                                                StopPlaceRepository stopPlaceRepository,
+                                                GeometryFactory geometryFactory,
+                                                CountyAndMunicipalityLookupService countyAndMunicipalityLookupService) {
+        this.quayRepository = quayRepository;
+        this.stopPlaceRepository = stopPlaceRepository;
+        this.geometryFactory = geometryFactory;
+        this.countyAndMunicipalityLookupService = countyAndMunicipalityLookupService;
+    }
 
     /**
      * Creates stopPlace objects based on quays by combining quays with the same
@@ -67,37 +81,100 @@ public class StopPlaceFromQuaysCorrelationService {
 
             stopPlace.setQuays(new ArrayList<>());
 
-            distinctQuays.get(quayGroupName).forEach(item -> {
+            distinctQuays.get(quayGroupName).forEach(quay -> {
 
-                logger.trace("About to add Quay with name {} and id {} to stop place", item.getName(), item.getId());
-                if (quaysAlreadyProcessed.contains(item.getId())) {
-                    logger.warn("Already created quay with name {} and id {}", item.getName(), item.getId());
+                boolean addQuay = false;
+
+                if (quaysAlreadyProcessed.contains(quay.getId())) {
+
+                    logger.warn("Already created quay with name {} and id {}", quay.getName(), quay.getId());
+
+                } else if (stopPlace.getQuays().isEmpty()) {
+
+                    logger.debug("There are no quays related to stop place {} yet. Will add quay.", stopPlace.getName());
+                    addQuay = true;
+
+                } else if (quayIsCloseToExistingQuays(quay, stopPlace.getQuays())){
+
+                    logger.info("Quay {}, {} is close enough to be added",
+                            quay.getName(),
+                            quay.getCentroid().getLocation().toText());
+                    addQuay = true;
                 } else {
-                    stopPlace.getQuays().add(item);
-                    quaysAlreadyProcessed.add(item.getId());
+                    logger.info("Ignoring quay {} {}", quay.getName(), quay.getCentroid().getLocation().toText());
                 }
+
+                if(addQuay) {
+                    logger.trace("About to add Quay with name {} and id {} to stop place", quay.getName(), quay.getId());
+
+                    stopPlace.getQuays().add(quay);
+
+                    quaysAlreadyProcessed.add(quay.getId());
+                }
+
             });
 
-            stopPlace.setCentroid(new SimplePoint());
-            stopPlace.getCentroid().setLocation(calculateCentroidForStopPlace(stopPlace.getQuays()));
-            try {
-                countyAndMunicipalityLookupService.populateCountyAndMunicipality(stopPlace);
-                stopPlaceRepository.save(stopPlace);
-                logger.debug("Created stop place number {} with name {} and {} quays (id {})",
-                        stopPlaceCounter.incrementAndGet(), stopPlace.getName(), stopPlace.getQuays().size(), stopPlace.getId());
-            } catch (Exception e) {
-                logger.warn("Caught exception when saving stop place with name {}", quayGroupName, e);
+            if(stopPlace.getQuays().isEmpty()) {
+                logger.warn("No quays were added to stop place {} {}. Skipping...", stopPlace.getName(), stopPlace.getId());
+            } else {
+
+                stopPlace.setCentroid(new SimplePoint());
+                stopPlace.getCentroid().setLocation(calculateCentroidForStopPlace(stopPlace.getQuays()));
+
+                try {
+                    countyAndMunicipalityLookupService.populateCountyAndMunicipality(stopPlace);
+                    stopPlaceRepository.save(stopPlace);
+                    logger.debug("Created stop place number {} with name {} and {} quays (id {})",
+                            stopPlaceCounter.incrementAndGet(), stopPlace.getName(), stopPlace.getQuays().size(), stopPlace.getId());
+                } catch (Exception e) {
+                    logger.warn("Caught exception when saving stop place with name {}", quayGroupName, e);
+                }
             }
         });
 
         logger.debug("Amount of created stop places: {}", stopPlaceCounter.get());
     }
 
+    public boolean quayIsCloseToExistingQuays(Quay otherQuay, List<Quay> existingQuays) {
+        return existingQuays.stream().allMatch(q ->areClose(otherQuay,q));
+    }
+
+    /**
+     * Check if two quays are close, using the DISTANCE constant.
+     *
+     * http://www.vividsolutions.com/jts/javadoc/com/vividsolutions/jts/geom/Geometry.html#buffer(double)
+     */
+    public boolean areClose(Quay quay, Quay otherQuay) {
+
+        if(quay == null || otherQuay == null) {
+            return false;
+        }
+        Geometry buffer = quay.getCentroid().getLocation().buffer(DISTANCE);
+        boolean intersects = buffer.intersects(otherQuay.getCentroid().getLocation());
+
+        if(intersects) {
+            logger.debug("Quay {} {} is close to quay {} {}",
+                    quay.getName(),
+                    quay.getCentroid().getLocation().toText(),
+                    otherQuay.getName(),
+                    otherQuay.getCentroid().getLocation().toText());
+            return true;
+        }
+
+        logger.debug("Quay {} {} is NOT close to quay {} {}",
+                quay.getName(),
+                quay.getCentroid().getLocation().toText(),
+                otherQuay.getName(),
+                otherQuay.getCentroid().getLocation().toText());
+        return false;
+    }
+
     public Point calculateCentroidForStopPlace(List<Quay> quays) {
         CentroidPoint centroidPoint = new CentroidPoint();
         quays.forEach(quay -> centroidPoint.add(quay.getCentroid().getLocation()));
 
-        logger.debug("Created centroid for stop place based on quays. x: {}, y: {}", centroidPoint.getCentroid().x, centroidPoint.getCentroid().y);
+        logger.debug("Created centroid for stop place based on {} quays. x: {}, y: {}", quays.size(),
+                centroidPoint.getCentroid().x, centroidPoint.getCentroid().y);
 
         return geometryFactory.createPoint(centroidPoint.getCentroid());
     }
---
+++ src/test/java/no/rutebanken/tiamat/service/StopPlaceFromQuaysCorrelationServiceTest.java
@@ -0,0 +1,64 @@
+package no.rutebanken.tiamat.service;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import no.rutebanken.tiamat.pelias.CountyAndMunicipalityLookupService;
+import no.rutebanken.tiamat.repository.ifopt.QuayRepository;
+import no.rutebanken.tiamat.repository.ifopt.StopPlaceRepository;
+import org.junit.Test;
+import uk.org.netex.netex.Quay;
+import uk.org.netex.netex.SimplePoint;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.mockito.Mockito.mock;
+
+public class StopPlaceFromQuaysCorrelationServiceTest {
+    private GeometryFactory geometryFactory = new GeometryFactory();
+
+    private StopPlaceFromQuaysCorrelationService stopPlaceFromQuaysCorrelationService =
+            new StopPlaceFromQuaysCorrelationService(mock(QuayRepository.class),
+                    mock(StopPlaceRepository.class),
+                    geometryFactory,
+                    mock(CountyAndMunicipalityLookupService.class));
+
+    @Test
+    public void quaysAreNotClose() throws Exception {
+        Quay quay = quayWithCentroid(9.489552, 59.866439);
+        Quay nearbyQuay = quayWithCentroid(10.488425, 59.865190);
+        assertThat(stopPlaceFromQuaysCorrelationService.areClose(quay,nearbyQuay)).isFalse();
+    }
+
+    /**
+     * Typical two stop points nearby, but not close enough.
+     * About ~693 meters
+     * https://www.google.no/maps/search/59.865190,+10.488425
+     * https://www.google.no/maps/search/59.858964,+10.493528
+     *
+     * See also http://rechneronline.de/geo-coordinates/
+     */
+    @Test
+    public void quaysAreCloseButNotCloseEnough() throws Exception {
+        Quay quay = quayWithCentroid(10.488425, 59.865190);
+        Quay nearbyQuay = quayWithCentroid(10.493528, 59.858964);
+        assertThat(stopPlaceFromQuaysCorrelationService.areClose(quay,nearbyQuay)).isFalse();
+    }
+
+    /**
+     * Typical two stop points on each side on the road related to one stop place.
+     * https://www.google.no/maps/search/59.865190,+10.488425
+     * https://www.google.no/maps/search/59.866439,+10.489552
+     */
+    @Test
+    public void quaysAreClose() throws Exception {
+        Quay quay = quayWithCentroid(10.489552, 59.866439);
+        Quay nearbyQuay = quayWithCentroid(10.488425, 59.865190);
+        assertThat(stopPlaceFromQuaysCorrelationService.areClose(quay,nearbyQuay)).isTrue();
+    }
+
+    private Quay quayWithCentroid(double x, double y) {
+        Quay quay = new Quay();
+        quay.setCentroid(new SimplePoint());
+        quay.getCentroid().setLocation(geometryFactory.createPoint(new Coordinate(x, y, 0)));
+        return quay;
+    }
+}
