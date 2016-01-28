c7854a54974ae81edc7b9c1804f85ed19d66ee8d
Cristoffer Solem <cristoffer.solem@netlight.com>
2016-01-28 09:38:09 +0100

    POC-130 Import site frame's stop places with parallelStream.

--- src/main/java/no/rutebanken/tiamat/rest/ifopt/SiteFrameResource.java
+++ src/main/java/no/rutebanken/tiamat/rest/ifopt/SiteFrameResource.java
@@ -2,6 +2,7 @@ package no.rutebanken.tiamat.rest.ifopt;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.google.common.util.concurrent.Striped;
 import ma.glasnost.orika.MapperFacade;
 import ma.glasnost.orika.impl.DefaultMapperFactory;
 import no.rutebanken.tiamat.repository.ifopt.QuayRepository;
@@ -21,6 +22,8 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Component
 @Produces("application/xml")
@@ -86,65 +89,25 @@ public class SiteFrameResource {
     @Produces(MediaType.APPLICATION_XML)
     public String importSiteFrame(String xml) throws IOException {
         // Using xml mapper directly because of issues registering it properly in JerseyConfig
-        logger.info("Incoming xml {} characters long", xml.length());
+        logger.info("Incoming xml is {} characters long", xml.length());
+
+        AtomicInteger counter = new AtomicInteger(0);
 
         try {
             SiteFrame siteFrame = xmlMapper.readValue(xml, SiteFrame.class);
-            logger.info("Got site frame {}", siteFrame);
-            siteFrame.getTopographicPlaces().getTopographicPlace().forEach(place -> logger.info("{} - {}", place.getName(), place.getId()));
+            logger.info("Received site frame for import. It contains {} topographical places and {} stop places. Starting import,",
+                    siteFrame.getTopographicPlaces().getTopographicPlace().size(),
+                    siteFrame.getStopPlaces().getStopPlace().size());
 
             siteFrame.getStopPlaces().getStopPlace()
-                    .stream()
+                    .parallelStream()
                     .forEach(stopPlace -> {
-
-                        if (stopPlace.getCentroid() == null
-                                || stopPlace.getCentroid().getLocation() == null
-                                || stopPlace.getCentroid().getLocation().getGeometryPoint() == null) {
-                            logger.warn("Ignoring stop place {} - {} because it lacks geomery", stopPlace.getName(), stopPlace.getId());
+                        try {
+                            importStopPlace(stopPlace, siteFrame, counter);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
                         }
-
-                        StopPlace existing = stopPlaceRepository
-                                .findByNameValueAndCentroidLocationGeometryPoint(
-                                        stopPlace.getName().getValue(),
-                                        stopPlace.getCentroid().getLocation().getGeometryPoint());
-
-                        if (existing != null) {
-                            logger.warn("Found existing stop place {} with the same name and same location {}. Delete...",
-                                    stopPlace.getName(), stopPlace.getCentroid().getLocation().getGeometryPoint().toText());
-                            stopPlaceRepository.delete(existing.getId());
-                        }
-
-                        if (stopPlace.getTopographicPlaceRef() != null) {
-                            Optional<TopographicPlace> optional = findOrCreateTopographicalPlace(
-                                    siteFrame.getTopographicPlaces().getTopographicPlace(),
-                                    stopPlace.getTopographicPlaceRef());
-
-                            if (!optional.isPresent()) {
-                                logger.warn("Got no topographic places back for stop place {} {}", stopPlace.getName(), stopPlace.getId());
-                            }
-
-                            optional.ifPresent(topographicPlace -> {
-                                logger.info("Setting topographical ref {} on stop place {} {}",
-                                        topographicPlace.getId(), stopPlace.getName(), stopPlace.getId());
-                                TopographicPlaceRefStructure newRef = new TopographicPlaceRefStructure();
-                                newRef.setRef(topographicPlace.getId());
-                                stopPlace.setTopographicPlaceRef(newRef);
-                            });
-                        }
-
-                        stopPlace.setId(null);
-
-                        if (stopPlace.getQuays() != null) {
-                            stopPlace.getQuays().forEach(quay -> {
-                                quay.setId(null);
-                                quayRepository.save(quay);
-                            });
-                        }
-
-                        stopPlaceRepository.save(stopPlace);
-                        logger.info("Saving stop place {} {}", stopPlace.getName(), stopPlace.getId());
                     });
-
 
             return "Saved " + siteFrame.getTopographicPlaces().getTopographicPlace().size()
                     + " topographical places and " + siteFrame.getStopPlaces().getStopPlace().size() + " stop places";
@@ -155,6 +118,64 @@ public class SiteFrameResource {
         }
     }
 
+    private void importStopPlace(StopPlace stopPlace, SiteFrame siteFrame, AtomicInteger counter) throws InterruptedException {
+        if (stopPlace.getCentroid() == null
+                || stopPlace.getCentroid().getLocation() == null
+                || stopPlace.getCentroid().getLocation().getGeometryPoint() == null) {
+            logger.info("Ignoring stop place {} - {} because it lacks geometry", stopPlace.getName(), stopPlace.getId());
+            return;
+        }
+
+        StopPlace existing = stopPlaceRepository
+                .findByNameValueAndCentroidLocationGeometryPoint(
+                        stopPlace.getName().getValue(),
+                        stopPlace.getCentroid().getLocation().getGeometryPoint());
+
+        if (existing != null) {
+            logger.debug("Found existing stop place {} with the same name and same location {}. Delete...",
+                    stopPlace.getName(), stopPlace.getCentroid().getLocation().getGeometryPoint().toText());
+            stopPlaceRepository.delete(existing.getId());
+        }
+
+        if (stopPlace.getTopographicPlaceRef() != null) {
+            Optional<TopographicPlace> optional = findOrCreateTopographicalPlace(
+                    siteFrame.getTopographicPlaces().getTopographicPlace(),
+                    stopPlace.getTopographicPlaceRef());
+
+            if (!optional.isPresent()) {
+                logger.warn("Got no topographic places back for stop place {} {}", stopPlace.getName(), stopPlace.getId());
+            }
+
+            optional.ifPresent(topographicPlace -> {
+                logger.debug("Setting topographical ref {} on stop place {} {}",
+                        topographicPlace.getId(), stopPlace.getName(), stopPlace.getId());
+                TopographicPlaceRefStructure newRef = new TopographicPlaceRefStructure();
+                newRef.setRef(topographicPlace.getId());
+                stopPlace.setTopographicPlaceRef(newRef);
+            });
+        }
+
+        stopPlace.setId(null);
+
+        if (stopPlace.getQuays() != null) {
+            stopPlace.getQuays().forEach(quay -> {
+                quay.setId(null);
+                quayRepository.save(quay);
+            });
+        }
+
+        stopPlaceRepository.save(stopPlace);
+        counter.incrementAndGet();
+        logger.debug("Saving stop place {} {}", stopPlace.getName(), stopPlace.getId());
+
+        if (counter.get() % 100 == 0) {
+            int total = siteFrame.getStopPlaces().getStopPlace().size();
+            logger.info("Stop place number {} of {}. {}%", counter.get(), siteFrame.getStopPlaces().getStopPlace().size(), (counter.get() * 100f) / total);
+        }
+    }
+
+    private Striped<Semaphore> stripedSemaphores = Striped.lazyWeakSemaphore(Integer.MAX_VALUE, 1);
+
 
     /**
      * fixme: Make this more readable and clean
@@ -163,39 +184,49 @@ public class SiteFrameResource {
      * Use existing IDs to resolve references to parent topographical places,
      * but used the genererated IDs from saving in references.
      */
-    public Optional<TopographicPlace> findOrCreateTopographicalPlace(List<TopographicPlace> places, TopographicPlaceRefStructure topographicPlaceRef) {
+    public Optional<TopographicPlace> findOrCreateTopographicalPlace(List<TopographicPlace> places, TopographicPlaceRefStructure topographicPlaceRef) throws InterruptedException {
 
-        Optional<TopographicPlace> optional = findTopographicPlaceFromRefOrRepository(places, topographicPlaceRef);
+        // Striped locking on the ref string.
+        Semaphore semaphore = stripedSemaphores.get(topographicPlaceRef.getRef());
 
-        if (optional.isPresent()) {
+        semaphore.acquire();
 
-            TopographicPlace topographicPlaceFromRef = optional.get();
+        try {
+            Optional<TopographicPlace> optional = findTopographicPlaceFromRefOrRepository(places, topographicPlaceRef);
 
-            logger.debug("Topographical place found from ref {}", topographicPlaceRef.getRef());
+            if (optional.isPresent()) {
 
-            TopographicPlace topographicPlace = findSimilarOrUse(topographicPlaceFromRef);
+                TopographicPlace topographicPlaceFromRef = optional.get();
 
-            if (topographicPlace.getParentTopographicPlaceRef() != null) {
-                logger.debug("The topographical place {} contains a parent reference {}", topographicPlace.getName(),
-                        topographicPlace.getParentTopographicPlaceRef().getRef());
+                logger.debug("Topographical place found from ref {}", topographicPlaceRef.getRef());
 
-                findOrCreateTopographicalPlace(places, topographicPlace.getParentTopographicPlaceRef())
-                        .ifPresent(parent -> topographicPlace.getParentTopographicPlaceRef().setRef(parent.getId()));
+                TopographicPlace topographicPlace = findSimilarOrUse(topographicPlaceFromRef);
+
+                if (topographicPlace.getParentTopographicPlaceRef() != null) {
+                    logger.debug("The topographical place {} contains a parent reference {}", topographicPlace.getName(),
+                            topographicPlace.getParentTopographicPlaceRef().getRef());
+
+                    findOrCreateTopographicalPlace(places, topographicPlace.getParentTopographicPlaceRef())
+                            .ifPresent(parent -> topographicPlace.getParentTopographicPlaceRef().setRef(parent.getId()));
+                }
+
+                // if(!topographicPlaceRepository.exists(topographicPlace.getId())) {
+                topographicPlaceRepository.save(topographicPlace);
+                logger.debug("Saved {} place {} of type {} - got id {}", topographicPlace.getTopographicPlaceType(), topographicPlace.getName(),
+                        topographicPlace.getTopographicPlaceType(), topographicPlace.getId());
+                //}
+
+                return Optional.of(topographicPlace);
             }
 
-            // if(!topographicPlaceRepository.exists(topographicPlace.getId())) {
-            topographicPlaceRepository.save(topographicPlace);
-            logger.info("Saved {} place {} of type {} - got id {}", topographicPlace.getTopographicPlaceType(), topographicPlace.getName(),
-                    topographicPlace.getTopographicPlaceType(), topographicPlace.getId());
-            //}
+            logger.warn("Found no topographical place from ref {} looking in {} topographical places", topographicPlaceRef.getRef(), places.size());
+            places.forEach(place -> logger.info("{} - {}", place.getName(), place.getId()));
 
-            return Optional.of(topographicPlace);
+            return Optional.empty();
+        } finally {
+            semaphore.release();
         }
 
-        logger.warn("Found no topographical place from ref {} looking in {} topographical places", topographicPlaceRef.getRef(), places.size());
-        places.forEach(place -> logger.info("{} - {}", place.getName(), place.getId()));
-
-        return Optional.empty();
     }
 
     /**
@@ -211,7 +242,7 @@ public class SiteFrameResource {
                         topographicPlaceFromRef.getCountryRef().getRef(),
                         topographicPlaceFromRef.getTopographicPlaceType())
                 .stream()
-                .peek(topographicPlace1 -> logger.info("Found already persisted {} with name {} and id {}",
+                .peek(topographicPlace1 -> logger.debug("Found already persisted {} with name {} and id {}",
                         topographicPlace1.getTopographicPlaceType(),
                         topographicPlace1.getName(),
                         topographicPlace1.getId()))
@@ -236,7 +267,7 @@ public class SiteFrameResource {
                 .stream()
                 .filter(topographicPlace -> topographicPlace.getId() != null)
                 .filter(topographicPlace -> topographicPlace.getId().equals(topographicPlaceRef.getRef()))
-                .peek(topographicPlace -> logger.info("Looking at topographical place with name {} and id {}", topographicPlace.getName(), topographicPlace.getId()))
+                .peek(topographicPlace -> logger.trace("Looking at topographical place with name {} and id {}", topographicPlace.getName(), topographicPlace.getId()))
                 .findFirst();
 
         //If another stop place or municipality was processed, the topographic place is already persisted.
