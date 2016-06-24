cbf0a5328e3ecc647d18300c60dd558716004058
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-06-24 12:11:00 +0200

    NRP-452 remove import

--- src/test/java/no/rutebanken/tiamat/rest/netex/siteframe/SiteFrameResourceIntegrationTest.java
+++ src/test/java/no/rutebanken/tiamat/rest/netex/siteframe/SiteFrameResourceIntegrationTest.java
@@ -15,7 +15,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
