9e7bd1c0b3d3d7916e888ba2ac4dda16055765fa
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-06-27 11:21:04 +0200

    NRP-460 Only one header, first in stops.txt

--- script/extraction.sh
+++ script/extraction.sh
@@ -7,6 +7,8 @@
 destinationFile="stops.txt"
 folder="gtfs-"$(date +%Y-%m-%d-%H:%M:%S)
 
+stopsHeader="stop_id,stop_code,stop_name,stop_desc,stop_lat,stop_lon,zone_id,stop_url,location_type,parent_station,wheelchair_boarding,stop_timezone"
+
 rm $destinationFile
 touch $destinationFile
 mkdir $folder
@@ -14,11 +16,10 @@ echo "Created folder $folder"
 
 aws s3 cp s3://rutebanken-marduk/outbound/gtfs/ $folder --recursive
 
-
 for file in $folder/*zip
 do
   echo "Appending stops.txt from $file to $destinationFile"
-  unzip -p $file stops.txt >> $destinationFile
+  unzip -p $file stops.txt | grep -v $stopsHeader >> $destinationFile
 done
 
 lines=$(wc -l $destinationFile)
@@ -28,6 +29,17 @@ echo "Removing duplicates and sorting"
 sort $destinationFile | uniq > $destinationFile-tmp
 mv $destinationFile-tmp $destinationFile
 
+sed -i "1s/^/$stopsHeader\n/" $destinationFile
+
 lines=$(wc -l $destinationFile)
 
 echo "$lines"
+
+
+destinationDir="../src/main/resources/stops/"
+echo "Copying $destinationFile into $destinationDir"
+cp $destinationFile $destinationDir
+
+cd ..
+
+mvn clean install spring-boot:run -Dspring.config.location=src/test/resources/application.properties -DskipTests -Dspring.profiles.active=geodb,bootstrap -Dserver.port=1999
