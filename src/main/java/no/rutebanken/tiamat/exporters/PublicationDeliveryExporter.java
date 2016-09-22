70e770db9c8d5fad75c82e639894d08bf5011360
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-09-22 09:07:52 +0200

    NRP-615 Add logging to publication delivery exporter

--- src/main/java/no/rutebanken/tiamat/exporters/PublicationDeliveryExporter.java
+++ src/main/java/no/rutebanken/tiamat/exporters/PublicationDeliveryExporter.java
@@ -9,6 +9,8 @@ import no.rutebanken.tiamat.model.TopographicPlacesInFrame_RelStructure;
 import no.rutebanken.tiamat.netexmapping.NetexMapper;
 import no.rutebanken.tiamat.repository.StopPlaceRepository;
 import no.rutebanken.tiamat.repository.TopographicPlaceRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,6 +21,7 @@ import java.util.Iterator;
 @Component
 @Transactional
 public class PublicationDeliveryExporter {
+    private static final Logger logger = LoggerFactory.getLogger(PublicationDeliveryExporter.class);
     private StopPlaceRepository stopPlaceRepository;
     private TopographicPlaceRepository topographicPlaceRepository;
     private NetexMapper netexMapper;
@@ -32,6 +35,7 @@ public class PublicationDeliveryExporter {
 
     public PublicationDeliveryStructure exportAllStopPlaces() throws JAXBException {
 
+        logger.info("Preparing publication delivery export");
         SiteFrame siteFrame = new SiteFrame();
 
         Iterable<StopPlace> iterableStopPlaces = stopPlaceRepository.findAll();
@@ -39,7 +43,7 @@ public class PublicationDeliveryExporter {
         StopPlacesInFrame_RelStructure stopPlacesInFrame_relStructure = new StopPlacesInFrame_RelStructure();
 
         iterableStopPlaces.forEach(stopPlace -> stopPlacesInFrame_relStructure.getStopPlace().add(stopPlace));
-
+        logger.info("Adding {} stop places", stopPlacesInFrame_relStructure.getStopPlace().size());
         siteFrame.setStopPlaces(stopPlacesInFrame_relStructure);
 
         Iterator<TopographicPlace> topographicPlaceIterable = topographicPlaceRepository.findAll().iterator();
@@ -47,11 +51,13 @@ public class PublicationDeliveryExporter {
         TopographicPlacesInFrame_RelStructure topographicPlaces = new TopographicPlacesInFrame_RelStructure();
         topographicPlaceIterable
                 .forEachRemaining(topographicPlace -> topographicPlaces.getTopographicPlace().add(topographicPlace));
-
+        logger.info("Adding {} topographic places", topographicPlaces.getTopographicPlace().size());
         siteFrame.setTopographicPlaces(topographicPlaces);
 
+        logger.info("Mapping site frame to netex model");
         no.rutebanken.netex.model.SiteFrame convertedSiteFrame = netexMapper.mapToNetexModel(siteFrame);
 
+        logger.info("Returning publication delivery");
         return new PublicationDeliveryStructure()
                 .withDataObjects(new PublicationDeliveryStructure.DataObjects()
                         .withCompositeFrameOrCommonFrame(new ObjectFactory().createSiteFrame(convertedSiteFrame)));
