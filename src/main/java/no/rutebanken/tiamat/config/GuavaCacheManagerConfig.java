7bf47e5262ccf6e451b2d70184cf7dd66ccb5bfa
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-09-26 11:36:03 +0200

    NRP-615 Remove unused constant

--- src/main/java/no/rutebanken/tiamat/config/GuavaCacheManagerConfig.java
+++ src/main/java/no/rutebanken/tiamat/config/GuavaCacheManagerConfig.java
@@ -3,22 +3,18 @@ package no.rutebanken.tiamat.config;
 import com.google.common.cache.CacheBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.guava.GuavaCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.validation.constraints.Max;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class GuavaCacheManagerConfig {
 
     private static final Logger logger = LoggerFactory.getLogger(GuavaCacheManagerConfig.class);
-
-    private static final int MAX_CACHE_SIZE = 20000;
 
     @Bean(name = "guavaCacheManager")
     public CacheManager guavaCacheManager(
