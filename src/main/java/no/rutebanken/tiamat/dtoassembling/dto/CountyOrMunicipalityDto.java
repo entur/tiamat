70ef43e09ed6eee46d20151582420d035657ce7f
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-10-10 16:01:10 +0200

    NRP-630 Ignore serialization of county name

--- src/main/java/no/rutebanken/tiamat/dtoassembling/dto/CountyOrMunicipalityDto.java
+++ src/main/java/no/rutebanken/tiamat/dtoassembling/dto/CountyOrMunicipalityDto.java
@@ -1,8 +1,12 @@
 package no.rutebanken.tiamat.dtoassembling.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 public class CountyOrMunicipalityDto {
     public String ref;
     public String name;
     public String type;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     public String county;
 }
