c2372229d2c5de8f8da63be04f8c0be58befc23c
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-10-18 10:29:31 +0200

    NRP-28 Reenable county and municipalty lookup service.

--- src/main/java/org/rutebanken/tiamat/importers/DefaultStopPlaceImporter.java
+++ src/main/java/org/rutebanken/tiamat/importers/DefaultStopPlaceImporter.java
@@ -176,11 +176,11 @@ public class DefaultStopPlaceImporter implements StopPlaceImporter {
     }
 
     private void lookupCountyAndMunicipality(StopPlace stopPlace, AtomicInteger topographicPlacesCreatedCounter) {
-//        try {
-//            countyAndMunicipalityLookupService.populateCountyAndMunicipality(stopPlace, topographicPlacesCreatedCounter);
-//        } catch (IOException|InterruptedException e) {
-//            logger.warn("Could not lookup county and municipality for stop place with id {}", stopPlace.getId());
-//        }
+        try {
+            countyAndMunicipalityLookupService.populateCountyAndMunicipality(stopPlace, topographicPlacesCreatedCounter);
+        } catch (IOException|InterruptedException e) {
+            logger.warn("Could not lookup county and municipality for stop place with id {}", stopPlace.getId());
+        }
     }
 
     private StopPlace initializeLazyReferences(StopPlace stopPlace) {
