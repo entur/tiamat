d6d04aaff6bbc0af2e06b83cc9765d9e779d6c89
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-10-27 14:51:52 +0200

    NRP-700 remove even more xjb generated comments and annotations

--- src/main/java/org/rutebanken/tiamat/model/DataManagedObjectStructure.java
+++ src/main/java/org/rutebanken/tiamat/model/DataManagedObjectStructure.java
@@ -23,111 +23,43 @@ public abstract class DataManagedObjectStructure
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
     private Map<String,ValueList> keyValues = new HashMap<>();
 
-    @XmlElement(name = "Extensions")
     @Transient
     protected ExtensionsStructure extensions;
 
-    @XmlElement(name = "BrandingRef")
     @Transient
     protected BrandingRefStructure brandingRef;
 
-    @XmlAttribute(name = "responsibilitySetRef")
-    @XmlJavaTypeAdapter(NormalizedStringAdapter.class)
     @Transient
     protected String responsibilitySetRef;
 
-    /**
-     * A list of alternative Key values for an element.
-     * 
-     * @return
-     *     possible object is
-     *     {@link KeyListStructure }
-     *     
-     */
     public KeyListStructure getKeyList() {
         return keyList;
     }
 
-    /**
-     * Sets the value of the keyList property.
-     * 
-     * @param value
-     *     allowed object is
-     *     {@link KeyListStructure }
-     *     
-     */
     public void setKeyList(KeyListStructure value) {
         this.keyList = value;
     }
 
-    /**
-     * Gets the value of the extensions property.
-     * 
-     * @return
-     *     possible object is
-     *     {@link ExtensionsStructure }
-     *     
-     */
     public ExtensionsStructure getExtensions() {
         return extensions;
     }
 
-    /**
-     * Sets the value of the extensions property.
-     * 
-     * @param value
-     *     allowed object is
-     *     {@link ExtensionsStructure }
-     *     
-     */
     public void setExtensions(ExtensionsStructure value) {
         this.extensions = value;
     }
 
-    /**
-     * Gets the value of the brandingRef property.
-     * 
-     * @return
-     *     possible object is
-     *     {@link BrandingRefStructure }
-     *     
-     */
     public BrandingRefStructure getBrandingRef() {
         return brandingRef;
     }
 
-    /**
-     * Sets the value of the brandingRef property.
-     * 
-     * @param value
-     *     allowed object is
-     *     {@link BrandingRefStructure }
-     *     
-     */
     public void setBrandingRef(BrandingRefStructure value) {
         this.brandingRef = value;
     }
 
-    /**
-     * Gets the value of the responsibilitySetRef property.
-     * 
-     * @return
-     *     possible object is
-     *     {@link String }
-     *     
-     */
     public String getResponsibilitySetRef() {
         return responsibilitySetRef;
     }
 
-    /**
-     * Sets the value of the responsibilitySetRef property.
-     * 
-     * @param value
-     *     allowed object is
-     *     {@link String }
-     *     
-     */
     public void setResponsibilitySetRef(String value) {
         this.responsibilitySetRef = value;
     }
