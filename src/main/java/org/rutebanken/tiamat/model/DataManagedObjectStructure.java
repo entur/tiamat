398d2fa6f73e935c5e24f87d0bba07bb9384ccdf
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-10-27 14:52:35 +0200

    NRP-700 Organize imports

--- src/main/java/org/rutebanken/tiamat/model/DataManagedObjectStructure.java
+++ src/main/java/org/rutebanken/tiamat/model/DataManagedObjectStructure.java
@@ -1,16 +1,7 @@
 package org.rutebanken.tiamat.model;
 
 import javax.persistence.*;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlSeeAlso;
-import javax.xml.bind.annotation.XmlType;
-import javax.xml.bind.annotation.adapters.NormalizedStringAdapter;
-import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @MappedSuperclass
