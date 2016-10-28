aed99295ffd706697e4235e1415a6f6cc3a7c3c5
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-10-28 15:32:39 +0200

    NRP-700 Add to existing object ids. Use key parameter

--- src/main/java/org/rutebanken/tiamat/importers/KeyValueListAppender.java
+++ src/main/java/org/rutebanken/tiamat/importers/KeyValueListAppender.java
@@ -1,13 +1,11 @@
 package org.rutebanken.tiamat.importers;
 
 import org.rutebanken.tiamat.model.DataManagedObjectStructure;
-import org.rutebanken.tiamat.model.KeyValueStructure;
 import org.rutebanken.tiamat.netexmapping.NetexIdMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -17,17 +15,15 @@ public class KeyValueListAppender {
 
     public boolean appendToOriginalId(String key, DataManagedObjectStructure newObject, DataManagedObjectStructure existingObject) {
 
-        List<String> existingObjectIds = existingObject.getValues(NetexIdMapper.ORIGINAL_ID_KEY);
-        List<String> newObjectIds = newObject.getValues(NetexIdMapper.ORIGINAL_ID_KEY);
+        List<String> existingObjectIds = existingObject.getOrCreateValues(key);
+        List<String> newObjectIds = newObject.getOrCreateValues(key);
 
         boolean changed = false;
         for(String newOriginalId : newObjectIds) {
-            for(String existingId : existingObjectIds) {
-                if(!newOriginalId.equals(existingId)) {
-                    logger.info("Adding new original ID '{}' to existing object {}", newOriginalId, existingObject);
-                    existingObjectIds.add(newOriginalId);
-                    changed = true;
-                }
+            if(!existingObjectIds.contains(newOriginalId)) {
+                logger.info("Adding new original ID '{}' to existing object {}", newOriginalId, existingObject);
+                existingObjectIds.add(newOriginalId);
+                changed = true;
             }
         }
         return changed;
