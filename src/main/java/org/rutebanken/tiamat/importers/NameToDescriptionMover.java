9cd408c0c4bb58a79fc87033cd789acdf966ddb3
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-11-07 16:40:51 +0100

    NRP-747 Update description from name, also for stop place's quays. Call this logic from site frame importer

--- src/main/java/org/rutebanken/tiamat/importers/NameToDescriptionMover.java
+++ src/main/java/org/rutebanken/tiamat/importers/NameToDescriptionMover.java
@@ -1,6 +1,7 @@
 package org.rutebanken.tiamat.importers;
 
 
+import org.rutebanken.tiamat.model.GroupOfEntities_VersionStructure;
 import org.rutebanken.tiamat.model.MultilingualString;
 import org.rutebanken.tiamat.model.StopPlace;
 import org.slf4j.Logger;
@@ -19,24 +20,31 @@ public class NameToDescriptionMover {
     private static final Logger logger = LoggerFactory.getLogger(NameToDescriptionMover.class);
     private static final Pattern pattern = Pattern.compile("\\((.{3,})\\)");
 
-    public void move(StopPlace stopPlace) {
+    public StopPlace updateDescriptionFromName(StopPlace stopPlace) {
 
-        if(stopPlace.getName() != null && stopPlace.getName().getValue() != null) {
+        updateEntityDescriptionFromName(stopPlace);
+        if(stopPlace.getQuays() != null) {
+            stopPlace.getQuays().forEach(quay -> updateEntityDescriptionFromName(quay));
+        }
+        return stopPlace;
+    }
 
-            String name = stopPlace.getName().getValue();
+    public void updateEntityDescriptionFromName(GroupOfEntities_VersionStructure entity) {
+        if(entity.getName() != null && entity.getName().getValue() != null) {
+            String name = entity.getName().getValue();
+
             Matcher matcher = pattern.matcher(name);
 
-            while(matcher.find()) {
+            while (matcher.find()) {
                 logger.info("Matching {}", name);
                 if (matcher.groupCount() > 0) {
                     String description = matcher.group(1).trim();
                     logger.info("Extracted description {}", description);
-                    stopPlace.setDescription(new MultilingualString(description));
+                    entity.setDescription(new MultilingualString(description));
                 }
             }
 
-            stopPlace.getName().setValue(matcher.replaceAll("").trim());
+            entity.getName().setValue(matcher.replaceAll("").trim());
         }
     }
-
 }
