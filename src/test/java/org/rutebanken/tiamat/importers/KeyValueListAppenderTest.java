2f33b15aa98f58635b34985d95cfb1faf3a8dbed
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-11-07 21:00:26 +0100

    Implement test for key value list appender

---
+++ src/test/java/org/rutebanken/tiamat/importers/KeyValueListAppenderTest.java
@@ -0,0 +1,49 @@
+package org.rutebanken.tiamat.importers;
+
+import org.junit.Test;
+import org.rutebanken.tiamat.model.StopPlace;
+import org.rutebanken.tiamat.model.Value;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class KeyValueListAppenderTest {
+    
+    @Test
+    public void twoValuesMustNotBeAddedTwice() {
+        StopPlace newStopPlace = new StopPlace();
+        newStopPlace.getKeyValues().put("key", new Value("value"));
+
+        StopPlace existingStopPlace = new StopPlace();
+        existingStopPlace.getKeyValues().put("key", new Value("value"));
+
+        boolean changed = new KeyValueListAppender().appendToOriginalId("key", newStopPlace, existingStopPlace);
+        assertThat(changed).isFalse();
+        assertThat(existingStopPlace.getKeyValues().get("key").getItems()).hasSize(1);
+    }
+
+
+    @Test
+    public void addNewValues() {
+        StopPlace newStopPlace = new StopPlace();
+        newStopPlace.getKeyValues().put("key", new Value("newValue"));
+
+        StopPlace existingStopPlace = new StopPlace();
+        existingStopPlace.getKeyValues().put("key", new Value("oldValue"));
+
+        boolean changed = new KeyValueListAppender().appendToOriginalId("key", newStopPlace, existingStopPlace);
+        assertThat(changed).isTrue();
+        assertThat(existingStopPlace.getKeyValues().get("key").getItems()).hasSize(2);
+    }
+
+    @Test
+    public void keepExistingValues() {
+        StopPlace newStopPlace = new StopPlace();
+
+        StopPlace existingStopPlace = new StopPlace();
+        existingStopPlace.getKeyValues().put("key", new Value("oldValue"));
+
+        boolean changed = new KeyValueListAppender().appendToOriginalId("key", newStopPlace, existingStopPlace);
+        assertThat(changed).isFalse();
+        assertThat(existingStopPlace.getKeyValues().get("key").getItems()).hasSize(1);
+    }
+}
