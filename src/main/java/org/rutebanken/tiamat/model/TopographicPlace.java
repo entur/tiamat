4e460b5eb690f4668c267ca17aec611add248d16
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-11-22 21:16:20 +0100

    Index on parent topographic place ref

--- src/main/java/org/rutebanken/tiamat/model/TopographicPlace.java
+++ src/main/java/org/rutebanken/tiamat/model/TopographicPlace.java
@@ -3,6 +3,7 @@ package org.rutebanken.tiamat.model;
 import javax.persistence.*;
 
 @Entity
+@Table(indexes = {@Index(name = "parent_topographic_ref_index", columnList = "parent_topographic_ref")})
 public class TopographicPlace
         extends Place_VersionStructure {
 
