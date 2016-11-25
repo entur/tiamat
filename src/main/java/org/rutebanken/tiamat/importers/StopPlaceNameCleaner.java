6d70b3f44714a9180517df8c498698ca0913c67c
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-11-25 16:41:26 +0100

    NRP-822 Clean stop place and quay names if unclosed parenthesis and square brackets

--- src/main/java/org/rutebanken/tiamat/importers/StopPlaceNameCleaner.java
+++ src/main/java/org/rutebanken/tiamat/importers/StopPlaceNameCleaner.java
@@ -5,6 +5,8 @@ import org.rutebanken.tiamat.model.StopPlace;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.regex.Pattern;
+
 @Component
 public class StopPlaceNameCleaner {
 
@@ -14,6 +16,8 @@ public class StopPlaceNameCleaner {
     public StopPlaceNameCleaner(WordsRemover wordsRemover) {
         this.wordsRemover = wordsRemover;
     }
+
+    private static final Pattern pattern = Pattern.compile("\\(\\w\\s+!\\)");
 
     public StopPlace cleanNames(StopPlace stopPlace) {
         cleanAndSetName(stopPlace);
@@ -27,7 +31,20 @@ public class StopPlaceNameCleaner {
 
     private void cleanAndSetName(GroupOfEntities_VersionStructure entity) {
         if(entity.getName() != null && entity.getName().getValue() != null) {
-            entity.getName().setValue(wordsRemover.remove(entity.getName().getValue()));
+            String name = wordsRemover.remove(entity.getName().getValue());
+
+            name = replaceIfUnclosed(name, '(', ')');
+            name = replaceIfUnclosed(name, '[', ']');
+
+            entity.getName().setValue(name.trim());
         }
+    }
+
+    private String replaceIfUnclosed(String name, char starChar, char endChar) {
+        int startIndex= name.indexOf(starChar);
+        if ( startIndex > 0 && name.indexOf(endChar) == -1) {
+            name = name.substring(0, startIndex-1);
+        }
+        return name;
     }
 }
