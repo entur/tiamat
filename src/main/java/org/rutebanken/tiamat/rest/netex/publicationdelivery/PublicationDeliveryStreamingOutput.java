54a9d153ad5392ea1020c1e44e1e875c28515bf5
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-11-29 17:21:53 +0100

    NRP-772 Log marshalled publication delivery xml before writing to output stream if debug log level is set. Helps debugging and testing.

--- src/main/java/org/rutebanken/tiamat/rest/netex/publicationdelivery/PublicationDeliveryStreamingOutput.java
+++ src/main/java/org/rutebanken/tiamat/rest/netex/publicationdelivery/PublicationDeliveryStreamingOutput.java
@@ -3,11 +3,14 @@ package org.rutebanken.tiamat.rest.netex.publicationdelivery;
 import org.rutebanken.netex.model.ObjectFactory;
 import org.rutebanken.netex.model.PublicationDeliveryStructure;
 import org.rutebanken.netex.validation.NeTExValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.xml.sax.SAXException;
 
 import javax.ws.rs.core.StreamingOutput;
 import javax.xml.bind.*;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -15,6 +18,8 @@ import static javax.xml.bind.JAXBContext.newInstance;
 
 @Component
 public class PublicationDeliveryStreamingOutput {
+
+    private static final Logger logger = LoggerFactory.getLogger(PublicationDeliveryStreamingOutput.class);
 
     private static final JAXBContext jaxbContext;
 
@@ -35,17 +40,30 @@ public class PublicationDeliveryStreamingOutput {
 
     public StreamingOutput stream(PublicationDeliveryStructure publicationDelivery) throws JAXBException, IOException, SAXException {
         Marshaller marshaller = jaxbContext.createMarshaller();
+
+        JAXBElement<PublicationDeliveryStructure> jaxPublicationDelivery = objectFactory.createPublicationDelivery(publicationDelivery);
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+        logXmlIfDebugEnabled(jaxPublicationDelivery, marshaller);
+
         marshaller.setSchema(neTExValidator.getSchema());
 
-        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
         return outputStream -> {
             try {
-                marshaller.marshal(objectFactory.createPublicationDelivery(publicationDelivery), outputStream);
+                marshaller.marshal(jaxPublicationDelivery, outputStream);
             } catch (JAXBException e) {
                 throw new RuntimeException("Could not stream site frame", e);
             }
         };
 
+    }
+
+    private void logXmlIfDebugEnabled(JAXBElement<PublicationDeliveryStructure> jaxPublicationDelivery, Marshaller marshaller) throws JAXBException {
+        if(logger.isDebugEnabled()) {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            marshaller.marshal(jaxPublicationDelivery, byteArrayOutputStream);
+            logger.debug("Logging marshalled NeTEx XML as debug is enabled. \n{}", byteArrayOutputStream.toString());
+        }
     }
 
 }
