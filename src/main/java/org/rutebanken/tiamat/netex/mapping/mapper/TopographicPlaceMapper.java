c19f39d8465dc63603327e13600855d0e47fb3fc
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-11-30 09:38:39 +0100

    NRP-772 Null check when mapping topographic place to netex

--- src/main/java/org/rutebanken/tiamat/netex/mapping/mapper/TopographicPlaceMapper.java
+++ src/main/java/org/rutebanken/tiamat/netex/mapping/mapper/TopographicPlaceMapper.java
@@ -2,6 +2,7 @@ package org.rutebanken.tiamat.netex.mapping.mapper;
 
 import ma.glasnost.orika.CustomMapper;
 import ma.glasnost.orika.MappingContext;
+import org.rutebanken.netex.model.CountryRef;
 import org.rutebanken.netex.model.MultilingualString;
 import org.rutebanken.netex.model.TopographicPlace;
 import org.rutebanken.netex.model.TopographicPlaceDescriptor_VersionedChildStructure;
@@ -20,8 +21,13 @@ public class TopographicPlaceMapper extends CustomMapper<TopographicPlace, org.r
     public void mapBtoA(org.rutebanken.tiamat.model.TopographicPlace tiamatTopographicPlace, TopographicPlace netexTopographicPlace, MappingContext context) {
         super.mapBtoA(tiamatTopographicPlace, netexTopographicPlace, context);
         netexTopographicPlace.withDescriptor(
-                new TopographicPlaceDescriptor_VersionedChildStructure()
-                        .withName(new MultilingualString().withValue(tiamatTopographicPlace.getName().getValue())));
+                new TopographicPlaceDescriptor_VersionedChildStructure());
+
+        if(tiamatTopographicPlace.getName() != null) {
+            netexTopographicPlace.getDescriptor().withName(new MultilingualString().withValue(tiamatTopographicPlace.getName().getValue()));
+        }
+
+        // TODO: versioning
         netexTopographicPlace.setVersion("any");
 
     }
