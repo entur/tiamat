ad1bb32bda3093a2616756f6f8a90529999f2de1
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-12-15 14:01:32 +0100

    NRP-933 Change ID generator to use db function to get next id for quay and stop place. Temporary add it to import.sql (even though it needs to be run manually).

--- src/main/java/org/rutebanken/tiamat/repository/OptionalIdGenerator.java
+++ src/main/java/org/rutebanken/tiamat/repository/OptionalIdGenerator.java
@@ -1,16 +1,28 @@
 package org.rutebanken.tiamat.repository;
 
 import org.hibernate.HibernateException;
+import org.hibernate.SQLQuery;
 import org.hibernate.engine.spi.SessionImplementor;
 import org.hibernate.id.SequenceGenerator;
 import org.hibernate.id.enhanced.SequenceStyleGenerator;
+import org.hibernate.internal.SQLQueryImpl;
+import org.hibernate.internal.SessionImpl;
+import org.hibernate.metadata.ClassMetadata;
+import org.hibernate.persister.entity.AbstractEntityPersister;
+import org.hibernate.persister.entity.SingleTableEntityPersister;
 import org.rutebanken.tiamat.model.EntityStructure;
+import org.rutebanken.tiamat.model.Quay;
+import org.rutebanken.tiamat.model.StopPlace;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
+import java.math.BigInteger;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+import java.util.List;
 
 /**
  * Generate ID if not already set.
@@ -25,15 +37,37 @@ public class OptionalIdGenerator extends SequenceStyleGenerator {
 
         if (obj == null) throw new HibernateException(new NullPointerException());
 
-        if ((((EntityStructure) obj).getId()) == null) {
-            logger.info("{} does not already have ID. Generating it.", obj);
-            Serializable id = super.generate(session, obj);
-            logger.info("Generated id {}", id);
+        if(obj instanceof EntityStructure) {
+            EntityStructure entityStructure = (EntityStructure) obj;
 
-            return id;
-        } else {
-            logger.info("{} does already have ID. Trying to insert it", obj);
-            return ((EntityStructure) obj).getId();
+            logger.info("{} is instance of entity structre", entityStructure);
+
+            if((entityStructure instanceof StopPlace || entityStructure instanceof Quay)) {
+                if(entityStructure.getId() == null) {
+
+                    logger.info("{} does not already have ID. Generating it.", entityStructure);
+
+                    SessionImpl sessionImpl = (SessionImpl) session;
+                    String tableName = ((SingleTableEntityPersister) (sessionImpl).getSessionFactory().getAllClassMetadata().get(obj.getClass().getCanonicalName())).getTableName();
+
+                    String sql = "SELECT generate_next_available_id('" + tableName + "')";
+
+                    SQLQuery sqlQuery = sessionImpl.createSQLQuery(sql);
+                    List list = sqlQuery.list();
+
+                    if (list.size() > 0) {
+                        return ((BigInteger) list.get(0)).longValue();
+                    }
+                    logger.warn("Could not get next available ID for table_name {}", tableName);
+                } else {
+                    // TODO update next available ID. Do it in the db function as it needs to know about it.
+                    logger.info("{} does already have ID. Trying to use it", entityStructure);
+                    return entityStructure.getId();
+                }
+            }
         }
+        Serializable id = super.generate(session, obj);
+        logger.info("Generated id {}", id);
+        return id;
     }
 }
