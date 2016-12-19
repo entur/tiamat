f0bd7edd5c1f522101d745d9f40366e2e54e02b4
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-12-19 10:35:12 +0100

    NRP-933 Implement configuration class for creating generator function in psql. H2 not supported yet.

---
+++ src/main/java/org/rutebanken/tiamat/config/CreateIdGeneratorFunction.java
@@ -0,0 +1,60 @@
+package org.rutebanken.tiamat.config;
+
+import com.vividsolutions.jts.geom.GeometryFactory;
+import org.rutebanken.tiamat.gtfs.GtfsStopsReader;
+import org.rutebanken.tiamat.nvdb.service.NvdbStopPlaceRetrievalService;
+import org.rutebanken.tiamat.service.StopPlaceFromQuaysCorrelationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Create funciton in database for generating IDs
+ * See https://rutebanken.atlassian.net/browse/NRP-23 for liquibase implementation.
+ */
+@Configuration
+public class CreateIdGeneratorFunction implements InitializingBean {
+
+    private static final Logger logger = LoggerFactory.getLogger(CreateIdGeneratorFunction.class);
+
+    private static final String SQL = "CREATE OR REPLACE FUNCTION generate_next_available_id(entity text) RETURNS integer AS $$\n" +
+            "DECLARE\n" +
+            "    next_value integer;\n" +
+            "BEGIN\n" +
+            "    SELECT id FROM (SELECT id_value + 1 AS id\n" +
+            "      FROM id_generator g1\n" +
+            "      WHERE NOT EXISTS(SELECT null FROM id_generator g2 WHERE g2.id_value = g1.id_value + 1 AND g2.table_name = entity)\n" +
+            "      UNION\n" +
+            "      SELECT 1 AS id\n" +
+            "      WHERE NOT EXISTS (SELECT null FROM id_generator WHERE id_value = 1 AND table_name = entity)\n" +
+            "      ) ot\n" +
+            "    ORDER BY 1 INTO next_value;\n" +
+            "\n" +
+            "    INSERT INTO id_generator(table_name, id_value) values(entity, next_value);\n" +
+            "  RETURN next_value;\n" +
+            "END;\n" +
+            "$$ LANGUAGE 'plpgsql'";
+
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        if(jdbcTemplate.getDataSource().getConnection().getMetaData().getDatabaseProductName().contains("H2")) {
+            logger.warn("Does not create function for generating next available ID for H2. Not implemented.");
+        } else {
+            logger.info("Executing create function\n{}", SQL);
+            jdbcTemplate.execute(SQL);
+            logger.info("Executed create function");
+        }
+    }
+}
