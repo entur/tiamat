0667ad45715126e9a8cc55332c845521cc1a6383
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-01-06 14:05:46 +0100

    NRP-924 Test adding stop places into publication delivery with topographic places added in site frame

--- src/test/java/org/rutebanken/tiamat/exporters/StreamingPublicationDeliveryTest.java
+++ src/test/java/org/rutebanken/tiamat/exporters/StreamingPublicationDeliveryTest.java
@@ -7,17 +7,17 @@ import org.rutebanken.tiamat.netex.mapping.NetexMapper;
 import org.rutebanken.tiamat.repository.StopPlaceRepositoryImpl;
 
 import java.io.ByteArrayOutputStream;
-import java.io.OutputStream;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StreamingPublicationDeliveryTest {
 
+    private StreamingPublicationDelivery streamingPublicationDelivery = new StreamingPublicationDelivery(new NetexMapper());
+
     @Test
-    public void stream() throws Exception {
+    public void streamStopPlaceIntoPublicationDelivery() throws Exception {
 
         String publicationDeliveryXml = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
                 "<PublicationDelivery xmlns=\"http://www.netex.org.uk/netex\" xmlns:ns2=\"http://www.opengis.net/gml/3.2\" xmlns:ns3=\"http://www.siri.org.uk/siri\" version=\"any\">\n" +
@@ -29,10 +29,8 @@ public class StreamingPublicationDeliveryTest {
                 "   </dataObjects>\n" +
                 "</PublicationDelivery>\n";
 
-        StreamingPublicationDelivery streamingPublicationDelivery = new StreamingPublicationDelivery(new NetexMapper());
 
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-
 
         StopPlace stopPlace = new StopPlace(new EmbeddableMultilingualString("stop place in publication delivery"));
         stopPlace.setId(10L);
@@ -49,8 +47,51 @@ public class StreamingPublicationDeliveryTest {
                 .contains("<StopPlace")
                 .contains("</PublicationDelivery")
                 .contains("</dataObjects>");
+    }
+
+    @Test
+    public void streamStopPlaceIntoPublicationDeliveryWithTopographicPlace() throws Exception {
+
+        String publicationDeliveryXml = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
+                "<PublicationDelivery xmlns=\"http://www.netex.org.uk/netex\" xmlns:ns2=\"http://www.opengis.net/gml/3.2\" xmlns:ns3=\"http://www.siri.org.uk/siri\" version=\"any\">\n" +
+                "    <PublicationTimestamp>2017-01-06T13:09:42.338+01:00</PublicationTimestamp>\n" +
+                "    <ParticipantRef>NSR</ParticipantRef>\n" +
+                "    <dataObjects>\n" +
+                "        <SiteFrame created=\"2017-01-06T13:09:42.272+01:00\" modification=\"new\" version=\"any\" id=\"NSR:SiteFrame:1\">\n" +
+                "           <topographicPlaces>\n" +
+                "               <TopographicPlace modification=\"new\" version=\"any\" id=\"NSR:TopographicPlace:3\">\n" +
+                "                    <Name lang=\"no\">Akershus</Name>\n" +
+                "                    <Descriptor>\n" +
+                "                        <Name>Akershus</Name>\n" +
+                "                    </Descriptor>\n" +
+                "                    <TopographicPlaceType>county</TopographicPlaceType>\n" +
+                "                    <CountryRef ref=\"no\"/>\n" +
+                "                </TopographicPlace>\n" +
+                "           </topographicPlaces>\n" +
+                "        </SiteFrame>\n" +
+                "   </dataObjects>\n" +
+                "</PublicationDelivery>\n";
 
 
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        StopPlace stopPlace = new StopPlace(new EmbeddableMultilingualString("stop place in publication delivery"));
+        stopPlace.setId(10L);
+
+        BlockingQueue<StopPlace> stopPlaces = new ArrayBlockingQueue<>(2);
+        stopPlaces.put(stopPlace);
+        stopPlaces.put(StopPlaceRepositoryImpl.POISON_PILL);
+
+        streamingPublicationDelivery.stream(publicationDeliveryXml, stopPlaces, byteArrayOutputStream);
+
+        String xml = byteArrayOutputStream.toString();
+
+        assertThat(xml)
+                .contains("<StopPlace")
+                .contains("<topographicPlaces")
+                .contains("</topographicPlaces>")
+                .contains("</PublicationDelivery")
+                .contains("</dataObjects>");
     }
 
 }
