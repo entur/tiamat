8bf6c6a02b6221c2f3093be057d6c718f757b186
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-01-07 12:54:19 +0100

    NRP-924 Speed up loading imported ids: create index value_id_index on value_items (value_id)

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,3 +2,4 @@ CREATE INDEX stop_place_centroid_index ON stop_place USING GIST ( centroid );
 
 DROP TABLE IF EXISTS id_generator;
 CREATE TABLE id_generator(table_name text, id_value bigint);
+CREATE index value_id_index ON value_items (value_id);
