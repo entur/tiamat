82c29765026a42f42b9cb26de7b2f7c664c682e9
Lasse Tyrihjell <lassetyr@gmail.com>
2017-01-12 09:24:51 +0100

    - Springifying initialization - Setting responsecode based on result/errortype

--- src/main/java/org/rutebanken/tiamat/rest/dto/GraphQLResource.java
+++ src/main/java/org/rutebanken/tiamat/rest/dto/GraphQLResource.java
@@ -1,8 +1,12 @@
 package org.rutebanken.tiamat.rest.dto;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.Executors;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import graphql.ExecutionResult;
+import graphql.GraphQL;
+import graphql.GraphQLError;
+import graphql.execution.ExecutorServiceExecutionStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.ws.rs.Consumes;
@@ -11,35 +15,29 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
-import org.rutebanken.tiamat.repository.StopPlaceRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
-
-import graphql.ExecutionResult;
-import graphql.GraphQL;
-import graphql.execution.ExecutorServiceExecutionStrategy;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Executors;
 
 @Component
 @Path("/graphql")
 public class GraphQLResource {
 
 	@Autowired
-	private StopPlaceRepository stopPlaceRepository;
-	
+	private StopPlaceRegisterGraphQLSchema stopPlaceRegisterGraphQLSchema;
+
 	public GraphQLResource() {
 	}
 	
 	@PostConstruct
 	public void init() {
-		graphQL = new GraphQL(new StopPlaceRegisterGraphQLSchema(stopPlaceRepository).stopPlaceRegisterSchema,
+		graphQL = new GraphQL(stopPlaceRegisterGraphQLSchema.stopPlaceRegisterSchema,
 				new ExecutorServiceExecutionStrategy(Executors
 						.newCachedThreadPool(new ThreadFactoryBuilder().setNameFormat("GraphQLExecutor--%d").build())));
 		
 	}
-	
+
 	private GraphQL graphQL;
 
 	
@@ -69,9 +67,26 @@ public class GraphQLResource {
 		Response.ResponseBuilder res = Response.status(Response.Status.OK);
 		HashMap<String, Object> content = new HashMap<>();
 		if (!executionResult.getErrors().isEmpty()) {
-			// TODO: Put correct error code, eg. 400 for synax error
-			res = Response.status(Response.Status.INTERNAL_SERVER_ERROR);
-			content.put("errors", executionResult.getErrors());
+			List<GraphQLError> errors = executionResult.getErrors();
+
+			Response.Status status = Response.Status.INTERNAL_SERVER_ERROR;
+			for (GraphQLError error : errors) {
+				switch(error.getErrorType()) {
+					case InvalidSyntax:
+						status = Response.Status.BAD_REQUEST;
+						break;
+					case ValidationError:
+						status = Response.Status.BAD_REQUEST;
+						break;
+					case DataFetchingException:
+						status = Response.Status.INTERNAL_SERVER_ERROR;
+						break;
+				}
+			}
+
+			res = Response.status(status);
+
+			content.put("errors", errors);
 		}
 		if (executionResult.getData() != null) {
 			content.put("data", executionResult.getData());
