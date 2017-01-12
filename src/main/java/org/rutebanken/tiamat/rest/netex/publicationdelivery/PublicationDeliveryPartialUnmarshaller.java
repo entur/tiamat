7740e1c588f02703227f00b213e4155f34129c92
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-01-12 14:38:25 +0100

    NRP-1055 Refactor TypesEventFilter to separate java file

--- src/main/java/org/rutebanken/tiamat/rest/netex/publicationdelivery/PublicationDeliveryPartialUnmarshaller.java
+++ src/main/java/org/rutebanken/tiamat/rest/netex/publicationdelivery/PublicationDeliveryPartialUnmarshaller.java
@@ -104,52 +104,6 @@ public class PublicationDeliveryPartialUnmarshaller {
         return publicationDeliveryStructure;
     }
 
-    public class TypesEventFilter implements EventFilter {
-        private boolean deleteSection = false;
-        private final List<String> ignoreTypes;
-
-        public TypesEventFilter(String ...ignoreTypes) {
-            this.ignoreTypes = new ArrayList<>(ignoreTypes.length);
-            for(String ignoreType : ignoreTypes) {
-                this.ignoreTypes.add(ignoreType);
-            }
-        }
-
-        @Override
-        public boolean accept(XMLEvent event) {
-            if(event.isEndElement()) {
-                EndElement endElement = event.asEndElement();
-                if(doesMatch(endElement.getName().getLocalPart())) {
-                    deleteSection = false;
-                    logger.trace("NO ACCEPT. Release delete section: {}", event);
-                    return false;
-                }
-            }
-
-            if(deleteSection) {
-                logger.trace("NO ACCEPT. Deleting section: {}", event);
-                return false;
-            }
-
-            if (event.isStartElement()) {
-                StartElement startElement = event.asStartElement();
-                String localPartOfName = startElement.getName().getLocalPart();
-
-                if(doesMatch(localPartOfName)) {
-                    deleteSection = true;
-                    return false;
-                }
-            }
-
-            logger.trace("Accept: {}", event);
-            return true;
-        }
-
-        private boolean doesMatch(String localPartOfName) {
-            return ignoreTypes.contains(localPartOfName);
-        }
-    }
-
     public static final StopPlace POISON_STOP_PLACE = new StopPlace().withId("-100");
 
     public UnmarshalResult readWithXmlEventReaderAsync(XMLInputFactory xmlInputFactory, InputStream inputStream, Unmarshaller unmarshaller) throws XMLStreamException, JAXBException, InterruptedException, IOException {
