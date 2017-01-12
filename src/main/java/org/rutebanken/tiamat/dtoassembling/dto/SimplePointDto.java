a4ca62e04f99f0eb3e3695272909361de3075fb3
Lasse Tyrihjell <lassetyr@gmail.com>
2017-01-12 09:22:22 +0100

    Adding GraphQL-annotations

--- src/main/java/org/rutebanken/tiamat/dtoassembling/dto/SimplePointDto.java
+++ src/main/java/org/rutebanken/tiamat/dtoassembling/dto/SimplePointDto.java
@@ -1,7 +1,13 @@
 package org.rutebanken.tiamat.dtoassembling.dto;
 
+
+import graphql.annotations.GraphQLField;
+import graphql.annotations.GraphQLType;
+
+@GraphQLType
 public class SimplePointDto {
 
 
+    @GraphQLField
     public LocationDto location;
 }
