8234b5c07537611f8a4ee67f58671ac37a878518
Lasse Tyrihjell <lassetyr@gmail.com>
2017-01-12 09:25:37 +0100

    Adding GraphQL-capabilities

---
+++ src/main/java/org/rutebanken/tiamat/rest/dto/StopPlaceFetcher.java
@@ -0,0 +1,103 @@
+package org.rutebanken.tiamat.rest.dto;
+
+import com.google.common.base.Functions;
+import com.google.common.collect.Lists;
+import graphql.language.Field;
+import graphql.language.Selection;
+import graphql.language.SelectionSet;
+import graphql.schema.DataFetcher;
+import graphql.schema.DataFetchingEnvironment;
+import org.rutebanken.tiamat.dtoassembling.assembler.StopPlaceAssembler;
+import org.rutebanken.tiamat.dtoassembling.dto.BoundingBoxDto;
+import org.rutebanken.tiamat.model.StopPlace;
+import org.rutebanken.tiamat.repository.StopPlaceRepository;
+import org.rutebanken.tiamat.repository.StopPlaceSearch;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+@Transactional
+class StopPlaceFetcher implements DataFetcher {
+
+    @Autowired
+    private StopPlaceRepository stopPlaceRepository;
+
+    @Autowired
+    private StopPlaceAssembler stopPlaceAssembler;
+
+    @Override
+    public Object get(DataFetchingEnvironment environment) {
+        StopPlaceSearch.Builder stopPlaceSearchBuilder = new StopPlaceSearch.Builder();
+        stopPlaceSearchBuilder.setIdList(environment.getArgument("id"));
+        stopPlaceSearchBuilder.setStopTypeEnumerations(environment.getArgument("stopPlaceType"));
+
+        if (environment.getArgument("countyReference") != null) {
+            stopPlaceSearchBuilder.setCountyIds(
+                    Lists.transform(environment.getArgument("countyReference"), Functions.toStringFunction())
+            );
+        }
+
+        if (environment.getArgument("municipalityReference") != null) {
+            stopPlaceSearchBuilder.setMunicipalityIds(
+                    Lists.transform(environment.getArgument("municipalityReference"), Functions.toStringFunction())
+            );
+        }
+
+        stopPlaceSearchBuilder.setQuery(environment.getArgument("query"));
+
+        PageRequest pageable = new PageRequest(environment.getArgument("page"), environment.getArgument("size"));
+        stopPlaceSearchBuilder.setPageable(pageable);
+
+        StopPlaceSearch stopPlaceSearch = stopPlaceSearchBuilder.build();
+
+
+        boolean quaysRequested = isQuaysRequested(environment);
+        Page<StopPlace> stopPlaces;
+        if (environment.getArgument("xMin") != null) {
+            BoundingBoxDto boundingBox = new BoundingBoxDto();
+
+            try {
+                boundingBox.xMin = ((BigDecimal) environment.getArgument("xMin")).doubleValue();
+                boundingBox.yMin = ((BigDecimal) environment.getArgument("yMin")).doubleValue();
+                boundingBox.xMax = ((BigDecimal) environment.getArgument("xMax")).doubleValue();
+                boundingBox.yMax = ((BigDecimal) environment.getArgument("yMax")).doubleValue();
+            } catch (NullPointerException npe) {
+                RuntimeException rte = new RuntimeException("xMin, yMin, xMax and yMax must all be set when searching within bounding box");
+                rte.setStackTrace(new StackTraceElement[0]);
+                throw rte;
+            }
+
+            Long ignoreStopPlaceId = environment.getArgument("ignoreStopPlaceId");
+
+            stopPlaces = stopPlaceRepository.findStopPlacesWithin(boundingBox.xMin, boundingBox.yMin, boundingBox.xMax,
+                    boundingBox.yMax, ignoreStopPlaceId, pageable);
+        } else {
+            stopPlaces = stopPlaceRepository.findStopPlace(stopPlaceSearch);
+        }
+
+        return stopPlaceAssembler.assemble(stopPlaces, quaysRequested);
+
+    }
+
+    private boolean isQuaysRequested(DataFetchingEnvironment environment) {
+        boolean quaysRequested = false;
+        List<Field> fields = environment.getFields();
+        for (Field field : fields) {
+            SelectionSet selectionSet = field.getSelectionSet();
+            List<Selection> selections = selectionSet.getSelections();
+            for (Selection selection : selections) {
+                Field selectedField = (Field) selection;
+                if ("quays".equals(selectedField.getName())) {
+                    quaysRequested = true;
+                }
+            }
+        }
+        return quaysRequested;
+    }
+}
--- src/main/java/org/rutebanken/tiamat/rest/dto/StopPlaceRegisterGraphQLSchema.java
+++ src/main/java/org/rutebanken/tiamat/rest/dto/StopPlaceRegisterGraphQLSchema.java
@@ -1,158 +1,108 @@
 package org.rutebanken.tiamat.rest.dto;
 
-import static graphql.Scalars.GraphQLBigDecimal;
-import static graphql.Scalars.GraphQLBoolean;
-import static graphql.Scalars.GraphQLLong;
-import static graphql.Scalars.GraphQLString;
+import graphql.Scalars;
+import graphql.annotations.GraphQLAnnotations;
+import graphql.schema.*;
+import org.rutebanken.tiamat.dtoassembling.dto.StopPlaceDto;
+import org.rutebanken.tiamat.model.StopTypeEnumeration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
 import static graphql.schema.GraphQLFieldDefinition.newFieldDefinition;
 import static graphql.schema.GraphQLObjectType.newObject;
 
-import org.rutebanken.tiamat.model.Site_VersionStructure;
-import org.rutebanken.tiamat.model.StopPlace;
-import org.rutebanken.tiamat.model.StopTypeEnumeration;
-import org.rutebanken.tiamat.repository.StopPlaceRepository;
-
-import graphql.Scalars;
-import graphql.schema.DataFetcher;
-import graphql.schema.DataFetchingEnvironment;
-import graphql.schema.GraphQLArgument;
-import graphql.schema.GraphQLEnumType;
-import graphql.schema.GraphQLList;
-import graphql.schema.GraphQLObjectType;
-import graphql.schema.GraphQLSchema;
-
+@Component
 public class StopPlaceRegisterGraphQLSchema {
 
     public GraphQLSchema stopPlaceRegisterSchema;
 
-    public StopPlaceRegisterGraphQLSchema(StopPlaceRepository stopPlaceRepository) {
-		super();
-		
-	    DataFetcher stopPlaceFetcher = new DataFetcher() {
-	        @Override
-			public
-	        Object get(DataFetchingEnvironment environment) {
-	            Long id = environment.getArgument("id"); 
-	        	if(id != null) {
-	        		return new Object[] {stopPlaceRepository.findOne(id)};
-	        	} else {
-	        		Iterable<StopPlace> findAll = stopPlaceRepository.findAll();
-	        		return findAll;
-	        	}
-	        }};
+    @Autowired
+    DataFetcher stopPlaceFetcher;
 
-        
-	          GraphQLEnumType stopPlaceTypeEnum = GraphQLEnumType.newEnum()
-	                .name("stopPlaceType")
-	                
-	                .value("onstreetBus",StopTypeEnumeration.ONSTREET_BUS)
-	                .value("onstreetTram",StopTypeEnumeration.ONSTREET_TRAM)
-	                .value("airport",StopTypeEnumeration.AIRPORT)
-	                .value("railStation",StopTypeEnumeration.RAIL_STATION)
-	                .value("metroStation",StopTypeEnumeration.METRO_STATION)
-	                .value("busStation",StopTypeEnumeration.BUS_STATION)
-	                .value("coachStation",StopTypeEnumeration.COACH_STATION)
-	                .value("tramStation",StopTypeEnumeration.TRAM_STATION)
-	                .value("harbourPort",StopTypeEnumeration.HARBOUR_PORT)
-	                .value("ferryPort",StopTypeEnumeration.FERRY_PORT)
-	                .value("ferryStop",StopTypeEnumeration.FERRY_STOP)
-	                .value("liftStation",StopTypeEnumeration.LIFT_STATION)
-	                .value("vehicleRailInterchange",StopTypeEnumeration.VEHICLE_RAIL_INTERCHANGE)
-	                .value("other",StopTypeEnumeration.OTHER)
-	                .build();
-	        
-		    GraphQLObjectType location = newObject()
-		            .name("location")
-		            .field(newFieldDefinition()
-		                    .type(GraphQLBigDecimal)
-		                    .name("longitude"))
-		            .field(newFieldDefinition()
-		                    .type(GraphQLBigDecimal)
-		                    .name("latitude"))
-		            .build();
+    @PostConstruct
+    public void init() {
 
-		    GraphQLObjectType centroid = newObject()
-		            .name("centroid")
-		            .field(newFieldDefinition()
-		                    .type(location)
-		                    .name("location"))
-		            .build();
+        GraphQLEnumType stopPlaceTypeEnum = GraphQLEnumType.newEnum()
+                .name("stopPlaceType")
+                .value("onstreetBus", StopTypeEnumeration.ONSTREET_BUS)
+                .value("onstreetTram", StopTypeEnumeration.ONSTREET_TRAM)
+                .value("airport", StopTypeEnumeration.AIRPORT)
+                .value("railStation", StopTypeEnumeration.RAIL_STATION)
+                .value("metroStation", StopTypeEnumeration.METRO_STATION)
+                .value("busStation", StopTypeEnumeration.BUS_STATION)
+                .value("coachStation", StopTypeEnumeration.COACH_STATION)
+                .value("tramStation", StopTypeEnumeration.TRAM_STATION)
+                .value("harbourPort", StopTypeEnumeration.HARBOUR_PORT)
+                .value("ferryPort", StopTypeEnumeration.FERRY_PORT)
+                .value("ferryStop", StopTypeEnumeration.FERRY_STOP)
+                .value("liftStation", StopTypeEnumeration.LIFT_STATION)
+                .value("vehicleRailInterchange", StopTypeEnumeration.VEHICLE_RAIL_INTERCHANGE)
+                .value("other", StopTypeEnumeration.OTHER)
+                .build();
 
-		    GraphQLObjectType multiLingualString = newObject()
-		            .name("multiLingualString")
-		            .field(newFieldDefinition()
-		                    .type(GraphQLString)
-		                    .name("lang"))
-		            .field(newFieldDefinition()
-		                    .type(GraphQLString)
-		                    .name("value"))
-		            .build();
+        GraphQLObjectType stopPlaceDtoObjectType;
+        try {
+            stopPlaceDtoObjectType = GraphQLAnnotations.object(StopPlaceDto.class);
+        } catch (Throwable e) {
+            throw new RuntimeException("Unable to start.", e);
+        }
 
-		    GraphQLObjectType quayType = newObject()
-		            .name("quay")
-		            .field(newFieldDefinition()
-		                    .type(GraphQLLong)
-		                    .name("id"))
-		            .field(newFieldDefinition()
-		                    .type(multiLingualString)
-		                    .name("name"))
-		            .field(newFieldDefinition()
-		                    .type(GraphQLBoolean)
-		                    .name("allAreasWheelchairAccessible"))
-		            .field(newFieldDefinition()
-		                    .type(centroid)
-		                    .name("centroid"))
-		            .build();
+        GraphQLObjectType queryType = newObject()
+                .name("findStopPlaces")
+                .field(newFieldDefinition()
+                                .type(new GraphQLList(stopPlaceDtoObjectType))
+                                .name("stopPlace")
+                                        //Common
+                                .argument(GraphQLArgument.newArgument()
+                                        .name("page")
+                                        .type(Scalars.GraphQLInt)
+                                        .defaultValue(0))
+                                .argument(GraphQLArgument.newArgument()
+                                        .name("size")
+                                        .type(Scalars.GraphQLInt)
+                                        .defaultValue(20))
+                                        //Search
+                                .argument(GraphQLArgument.newArgument()
+                                        .name("id")
+                                        .type(new GraphQLList(Scalars.GraphQLLong)))
+                                .argument(GraphQLArgument.newArgument()
+                                        .name("stopPlaceType")
+                                        .type(new GraphQLList(stopPlaceTypeEnum)))
+                                .argument(GraphQLArgument.newArgument()
+                                        .name("countyReference")
+                                        .type(new GraphQLList(Scalars.GraphQLInt)))
+                                .argument(GraphQLArgument.newArgument()
+                                        .name("municipalityReference")
+                                        .type(new GraphQLList(Scalars.GraphQLInt)))
+                                .argument(GraphQLArgument.newArgument()
+                                        .name("query")
+                                        .type(Scalars.GraphQLString))
 
-		    GraphQLObjectType stopPlaceType = newObject()
-		            .name("stopPlace")
-		            .field(newFieldDefinition()
-		                    .type(GraphQLLong)
-		                    .name("id"))
-		            .field(newFieldDefinition()
-		                    .type(multiLingualString)
-		                    .name("name"))
-//		            .field(newFieldDefinition()
-//		                    .type(GraphQLString)
-//		                    .name("parentSiteRef")
-//		                    .dataFetcher(e -> ((StopPlace)e.getSource()).getParentSiteRef().getValue())) // TODO solve recursive parents
-		            .field(newFieldDefinition()
-		                    .type(GraphQLBoolean)
-		                    .name("allAreasWheelchairAccessible"))
-		            .field(newFieldDefinition()
-		                    .type(stopPlaceTypeEnum)
-		                    .name("stopPlaceType"))
-		            .field(newFieldDefinition()
-		                    .type(centroid)
-		                    .name("centroid"))
-		            .field(newFieldDefinition()
-		                    .type(new GraphQLList(quayType))
-		                    .name("quays"))
+                                        //BoundingBox
+                                .argument(GraphQLArgument.newArgument()
+                                        .name("xMin")
+                                        .type(Scalars.GraphQLBigDecimal))
+                                .argument(GraphQLArgument.newArgument()
+                                        .name("yMin")
+                                        .type(Scalars.GraphQLBigDecimal))
+                                .argument(GraphQLArgument.newArgument()
+                                        .name("xMax")
+                                        .type(Scalars.GraphQLBigDecimal))
+                                .argument(GraphQLArgument.newArgument()
+                                        .name("yMax")
+                                        .type(Scalars.GraphQLBigDecimal))
+                                .argument(GraphQLArgument.newArgument()
+                                        .name("ignoreStopPlaceId")
+                                        .type(Scalars.GraphQLLong))
+                                .dataFetcher(stopPlaceFetcher)
+                ).build();
 
-		            .build();
-	        
-		    
-	        
-	        
-	    GraphQLObjectType queryType = newObject()
-	            .name("findStopPlaces")
-	            .field(newFieldDefinition()
-	                    .type(new GraphQLList(stopPlaceType))
-	                    .name("stopPlace")
-	                    .argument(GraphQLArgument.newArgument()
-	                            .name("id")
-	                            .type(Scalars.GraphQLLong))
-//	                    .argument(GraphQLArgument.newArgument()
-//	                    		.name("stopPlaceType")
-//	                    		.type(stopPlaceTypeEnum))
-	                    .dataFetcher(stopPlaceFetcher)
-	                    )
-	            
-	            .build();
-	    
-	    stopPlaceRegisterSchema = GraphQLSchema.newSchema()
-	            .query(queryType)
-	            .build();
-	}
+        stopPlaceRegisterSchema = GraphQLSchema.newSchema()
+                .query(queryType)
+                .build();
 
+    }
 }
+
