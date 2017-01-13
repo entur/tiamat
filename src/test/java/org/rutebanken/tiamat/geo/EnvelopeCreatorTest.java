639659068050ad898d24a9df4cfb5c42abb694b8
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-01-13 08:54:41 +0100

    NRP-991 Rename test methods

--- src/test/java/org/rutebanken/tiamat/geo/EnvelopeCreatorTest.java
+++ src/test/java/org/rutebanken/tiamat/geo/EnvelopeCreatorTest.java
@@ -24,7 +24,7 @@ public class EnvelopeCreatorTest {
     private EnvelopeCreator envelopeCreator = new EnvelopeCreator();
 
     @Test
-    public void testEnvelope() throws FactoryException, TransformException {
+    public void envelopeIntersectsWithCoordinate() throws FactoryException, TransformException {
         Coordinate coordinate = new Coordinate(59.858690, 10.493860);
         Coordinate coveredCoordinate = new Coordinate(59.858616, 10.493858);
         Point point = geometryFactory.createPoint(coordinate);
@@ -43,7 +43,7 @@ public class EnvelopeCreatorTest {
      * Test that a coordinate does not intersect with the created envelope.
      */
     @Test
-    public void testEnvelopeNoIntersects() throws FactoryException, TransformException {
+    public void envelopeDoesNotIntersectWithCoordinate() throws FactoryException, TransformException {
         Coordinate coordinate = new Coordinate(59.858690, 10.493860);
         Coordinate notCoveredCoordinate = new Coordinate(59.858684, 10.493682);
         Point point = geometryFactory.createPoint(coordinate);
