60146078374877cbb6123c65a6f21e8fd653875b
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-01-17 10:56:33 +0100

    NRP-1050 Implement tests for specific cases in Stavanger. Stop places with name "Stavanger hpl. 13" and "Stavanger spor2".

--- src/test/java/org/rutebanken/tiamat/rest/netex/publicationdelivery/StavangerImportTest.java
+++ src/test/java/org/rutebanken/tiamat/rest/netex/publicationdelivery/StavangerImportTest.java
@@ -14,6 +14,9 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * Import tests for different cases in Stavanger.
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = TiamatApplication.class)
 @ActiveProfiles("geodb")
@@ -23,25 +26,26 @@ public class StavangerImportTest {
     private PublicationDeliveryTestHelper publicationDeliveryTestHelper;
 
 
+    /**
+     * Import stop place with hpl. and number in the name.
+     * The part "hpl. [number]" should be moved to the Quay name.
+     */
     @Test
-    public void importStavanger() throws Exception {
+    public void importStavangerWithHplNumbering() throws Exception {
 
         StopPlace stopPlace = new StopPlace()
-                .withId("MOR:StopArea:15485753")
+                .withId("KOL:StopArea:11032650")
                 .withVersion("1")
-                .withName(new MultilingualString().withValue("Skaret").withLang("no"))
+                .withName(new MultilingualString().withValue("Stavanger hpl. 12").withLang("no"))
+                .withStopPlaceType(StopTypeEnumeration.ONSTREET_BUS)
                 .withQuays(new Quays_RelStructure()
                         .withQuayRefOrQuay(new Quay()
                                         .withVersion("1")
-                                        .withId("MOR:StopArea:1548612801")
-                                        .withName(new MultilingualString().withValue("Skaret").withLang("no"))
+                                        .withId("KOL:StopArea:1103265001")
+                                        .withName(new MultilingualString().withValue("Stavanger hpl. 12").withLang("no"))
                                         .withCentroid(new SimplePoint_VersionStructure().withLocation(new LocationStructure()
-                                                .withLatitude(new BigDecimal("62.799557598196465"))
-                                                .withLongitude(new BigDecimal("7.328336965528884")))),
-                                new Quay()
-                                        .withId("MOR:StopArea:1548575301")
-                                        .withVersion("1")
-                                        .withName(new MultilingualString().withValue("Skaret").withLang("no"))));
+                                                .withLatitude(new BigDecimal("58.966910"))
+                                                .withLongitude(new BigDecimal("5.732949"))))));
 
         PublicationDeliveryStructure publicationDelivery = publicationDeliveryTestHelper.createPublicationDeliveryWithStopPlace(stopPlace);
 
@@ -50,7 +54,49 @@ public class StavangerImportTest {
         // Exception should not have been thrown
         StopPlace actualStopPlace = publicationDeliveryTestHelper.findFirstStopPlace(response);
 
+        assertThat(actualStopPlace.getName().getValue()).isEqualTo("Stavanger");
+
         List<Quay> actualQuays = publicationDeliveryTestHelper.extractQuays(actualStopPlace);
         assertThat(actualQuays).isNotNull().as("quays should not be null");
+        assertThat(actualQuays.get(0).getName()).describedAs("Quay name should not be null").isNotNull();
+        assertThat(actualQuays.get(0).getName().getValue()).isEqualTo("hpl. 12");
+    }
+
+    /**
+     * Import stop place with spor and number in the name.
+     * The part "spor [number]" should be moved to the Quay name.
+     */
+    @Test
+    public void importStopWithSporAndNumbering() throws Exception {
+
+        final String originalStopPlaceName = "Stavanger spor 2";
+
+        StopPlace stopPlace = new StopPlace()
+                .withId("KOL:StopArea:11032650")
+                .withVersion("1")
+                .withName(new MultilingualString().withValue(originalStopPlaceName).withLang("no"))
+                .withStopPlaceType(StopTypeEnumeration.ONSTREET_BUS)
+                .withQuays(new Quays_RelStructure()
+                        .withQuayRefOrQuay(new Quay()
+                                .withVersion("1")
+                                .withId("KOL:StopArea:1103265001")
+                                .withName(new MultilingualString().withValue(originalStopPlaceName).withLang("no"))
+                                .withCentroid(new SimplePoint_VersionStructure().withLocation(new LocationStructure()
+                                        .withLatitude(new BigDecimal("58.966910"))
+                                        .withLongitude(new BigDecimal("5.732949"))))));
+
+        PublicationDeliveryStructure publicationDelivery = publicationDeliveryTestHelper.createPublicationDeliveryWithStopPlace(stopPlace);
+
+        PublicationDeliveryStructure response = publicationDeliveryTestHelper.postAndReturnPublicationDelivery(publicationDelivery);
+
+        // Exception should not have been thrown
+        StopPlace actualStopPlace = publicationDeliveryTestHelper.findFirstStopPlace(response);
+
+        assertThat(actualStopPlace.getName().getValue()).describedAs("Stop place name").isEqualTo("Stavanger");
+
+        List<Quay> actualQuays = publicationDeliveryTestHelper.extractQuays(actualStopPlace);
+        assertThat(actualQuays).isNotNull().as("quays should not be null");
+        assertThat(actualQuays.get(0).getName()).describedAs("Quay name should not be null").isNotNull();
+        assertThat(actualQuays.get(0).getName().getValue()).isEqualTo("spor 2");
     }
 }
