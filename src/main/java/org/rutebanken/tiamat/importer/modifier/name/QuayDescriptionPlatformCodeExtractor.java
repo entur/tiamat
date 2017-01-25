a702ae78a05241badef86291941479994d0b956a
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-01-25 13:10:28 +0100

    NRP-1119 Pick up platform code from quay description

---
+++ src/main/java/org/rutebanken/tiamat/importer/modifier/name/QuayDescriptionPlatformCodeExtractor.java
@@ -0,0 +1,86 @@
+package org.rutebanken.tiamat.importer.modifier.name;
+
+import org.rutebanken.tiamat.model.EmbeddableMultilingualString;
+import org.rutebanken.tiamat.model.Quay;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Component
+public class QuayDescriptionPlatformCodeExtractor {
+
+    private static final Logger logger = LoggerFactory.getLogger(QuayDescriptionPlatformCodeExtractor.class);
+
+    private final Pattern pattern;
+
+    @Autowired
+    public QuayDescriptionPlatformCodeExtractor(@Value("${QuayDescriptionPlatformCodeExtractor.terms:hpl,spor,plattform,pl,plf}") String[] terms) {
+        String termsPart = String.join("|", terms);
+        pattern = Pattern.compile("(" + termsPart + "\\.?)\\s([\\d\\w]+)[\\s\\-.]*(.*)", Pattern.CASE_INSENSITIVE);
+        logger.info("Terms: {}. Pattern: {}", terms, pattern);
+    }
+
+    public QuayDescriptionPlatformCodeExtractor() {
+        this(new String[]{"hpl", "spor", "plattform", "pl", "plf"});
+    }
+
+
+    public void extractPlatformCode(Quay quay) {
+
+        if (quay.getDescription() == null) {
+            logger.debug("Description is not set for Quay {}, ignoring.", quay);
+            return;
+        } else if (quay.getName() != null) {
+            if (!quay.getName().getValue().isEmpty() && !quay.getName().getValue().equals(quay.getDescription().getValue())) {
+                logger.info("Ignoring quay as it does already have a name, and it is not equal to it's description: '{}', {}",
+                        quay.getDescription().getValue(), quay);
+                return;
+            }
+        }
+
+        final String description = quay.getDescription().getValue();
+
+        Matcher matcher = pattern.matcher(description);
+
+        if (matcher.matches()) {
+
+            debugLog(matcher);
+
+            if (matcher.groupCount() >= 2) {
+                String platformCode = matcher.group(2);
+                logger.info("Setting quay name to {}: {}", platformCode, quay);
+                quay.setName(new EmbeddableMultilingualString(platformCode));
+                if (matcher.groupCount() == 3) {
+                    String newDescription = matcher.group(3).trim();
+                    if (newDescription.isEmpty()) {
+                        quay.setDescription(null);
+                    } else {
+                        logger.info("Setting quay description to {} for quay {}", newDescription, quay);
+                        quay.setDescription(new EmbeddableMultilingualString(newDescription));
+                    }
+
+                } else {
+                    quay.setDescription(null);
+                }
+            } else {
+                logger.info("Description: '{}' matches but group count is not as expected. ", description);
+            }
+
+        } else {
+            logger.debug("No match in description {}", description);
+        }
+    }
+
+    private void debugLog(Matcher matcher) {
+        if (logger.isDebugEnabled()) {
+            for (int groupId = 0; groupId <= matcher.groupCount(); groupId++) {
+                logger.debug("Group ID: {}: {}", groupId, matcher.group(groupId));
+            }
+        }
+    }
+}
