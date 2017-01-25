65fbb1645fad00590057b88c536ca3c6f7d22f2c
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-01-25 13:55:17 +0100

    NRP-1120 Add ferryStop to default list of stop types to ignore compass bearings for

--- src/main/java/org/rutebanken/tiamat/importer/modifier/CompassBearingRemover.java
+++ src/main/java/org/rutebanken/tiamat/importer/modifier/CompassBearingRemover.java
@@ -20,7 +20,7 @@ public class CompassBearingRemover {
 
     private final Set<StopTypeEnumeration> stopTypes = new HashSet<>();
 
-    public CompassBearingRemover(@Value("${CompassBearingRemover.types:airport,railStation,liftStation}") String[] removeBearingForTypes) {
+    public CompassBearingRemover(@Value("${CompassBearingRemover.types:airport,railStation,liftStation,ferryStop}") String[] removeBearingForTypes) {
         for (String type : removeBearingForTypes) {
             StopTypeEnumeration stopTypeEnumeration = StopTypeEnumeration.fromValue(type);
             stopTypes.add(stopTypeEnumeration);
