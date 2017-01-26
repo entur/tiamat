fd4bf63210260eeeb46eeb438f36e4712f8e0619
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-01-26 17:52:44 +0100

    NRP-1079 Configure hazelcast in test property file. Add Cache annotation on StopPlace. Add dependency to hazelcast-hibernate5.

--- src/main/java/org/rutebanken/tiamat/model/StopPlace.java
+++ src/main/java/org/rutebanken/tiamat/model/StopPlace.java
@@ -2,11 +2,14 @@ package org.rutebanken.tiamat.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.google.common.base.MoreObjects;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.*;
 
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 @Entity
 @Table(indexes = {@Index(name = "name_value_index", columnList = "name_value"),
         @Index(name="topographic_place_ref_index", columnList = "topographic_place_ref"),
