956a6d71629f8a013e0e728079e5580270de3936
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-01-26 13:13:39 +0100

    NRP-993 Add javadoc to stop place name cleaner.

--- src/main/java/org/rutebanken/tiamat/importer/modifier/name/StopPlaceNameCleaner.java
+++ src/main/java/org/rutebanken/tiamat/importer/modifier/name/StopPlaceNameCleaner.java
@@ -7,6 +7,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+/**
+ * Clean stop place and child quay names.
+ */
 @Component
 public class StopPlaceNameCleaner {
 
