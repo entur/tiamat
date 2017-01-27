af4a1c703f180b74cd456f732746cd23a88c7216
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-01-27 15:20:16 +0100

    NRP-1079 Implement the TiamatHazelcastCacheRegionFactory wich uses rutebanken's hazelcast configuration

--- src/main/java/org/rutebanken/tiamat/hazelcast/ExtendedHazelcastService.java
+++ src/main/java/org/rutebanken/tiamat/hazelcast/ExtendedHazelcastService.java
@@ -5,7 +5,6 @@ import com.hazelcast.core.*;
 
 import org.rutebanken.hazelcasthelper.service.HazelCastService;
 import org.rutebanken.hazelcasthelper.service.KubernetesService;
-import org.rutebanken.tiamat.config.HazelcastConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,18 +17,15 @@ import java.time.Instant;
 import java.util.Collection;
 import java.util.Set;
 
-@Service
 public class ExtendedHazelcastService extends HazelCastService {
 
     private static final Logger logger = LoggerFactory.getLogger(ExtendedHazelcastService.class);
 
-    @Autowired
-    public ExtendedHazelcastService(ExtendedKubernetesService extendedKubernetesService, HazelcastConfiguration hazelcastConfiguration) {
-        super(extendedKubernetesService, hazelcastConfiguration.getHazelcastManagementUrl());
+    public ExtendedHazelcastService(KubernetesService kubernetesService, String hazelcastManagementUrl) {
+        super(kubernetesService, hazelcastManagementUrl);
     }
 
-    @Bean
-    public Config getConfig() {
-        return hazelcast.getConfig();
+    public HazelcastInstance getHazelcastInstance() {
+        return hazelcast;
     }
 }
