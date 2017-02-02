7068c1a3017c7fa0b2727d903b6c33ce6b520784
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-02-02 09:25:17 +0100

    NRP-707 Rename AddressablePlace_VersionStructure and SiteElement_VersionStructure

--- src/main/java/org/rutebanken/tiamat/model/AddressablePlace.java
+++ src/main/java/org/rutebanken/tiamat/model/AddressablePlace.java
@@ -1,7 +1,63 @@
 package org.rutebanken.tiamat.model;
 
-public class AddressablePlace
-        extends AddressablePlace_VersionStructure {
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Transient;
 
+
+@MappedSuperclass
+public class AddressablePlace
+        extends Place {
+
+    @Transient
+    protected String url;
+
+    @Transient
+    protected String image;
+
+    @Transient
+    protected PostalAddress postalAddress;
+
+
+    @Transient
+    protected RoadAddress roadAddress;
+
+    public AddressablePlace(EmbeddableMultilingualString name) {
+        super(name);
+    }
+
+    public AddressablePlace() {
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String value) {
+        this.url = value;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String value) {
+        this.image = value;
+    }
+
+    public PostalAddress getPostalAddress() {
+        return postalAddress;
+    }
+
+    public void setPostalAddress(PostalAddress value) {
+        this.postalAddress = value;
+    }
+
+    public RoadAddress getRoadAddress() {
+        return roadAddress;
+    }
+
+    public void setRoadAddress(RoadAddress value) {
+        this.roadAddress = value;
+    }
 
 }
