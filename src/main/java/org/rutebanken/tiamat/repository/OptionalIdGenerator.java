c046002bf1d5e17c0389b3cdaa9ed7e4721fb273
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-02-08 09:35:10 +0100

    NRP-1171 Tidy up and add some comments to the OptionalIdGenerator

--- src/main/java/org/rutebanken/tiamat/repository/OptionalIdGenerator.java
+++ src/main/java/org/rutebanken/tiamat/repository/OptionalIdGenerator.java
@@ -5,27 +5,24 @@ import org.hibernate.FlushMode;
 import org.hibernate.HibernateException;
 import org.hibernate.SQLQuery;
 import org.hibernate.engine.spi.SessionImplementor;
-import org.hibernate.id.IntegralDataTypeHolder;
 import org.hibernate.id.enhanced.SequenceStyleGenerator;
 import org.hibernate.internal.SessionImpl;
 import org.hibernate.persister.entity.AbstractEntityPersister;
-import org.hibernate.persister.entity.SingleTableEntityPersister;
 import org.hibernate.type.LongType;
 import org.rutebanken.tiamat.model.EntityStructure;
 import org.rutebanken.tiamat.model.Quay;
 import org.rutebanken.tiamat.model.StopPlace;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.Serializable;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.Semaphore;
 import java.util.concurrent.locks.Lock;
 
 
@@ -41,7 +38,7 @@ public class OptionalIdGenerator extends SequenceStyleGenerator {
 
     private static final ConcurrentHashMap<String, ConcurrentLinkedQueue<Long>> availableIdsPerTable = new ConcurrentHashMap<>();
     private static final ConcurrentHashMap<String, Long> lastIdsPerTable = new ConcurrentHashMap<>();
-    private static final Striped<Lock> stripedSemaphores = Striped.lock(1024);
+    private static final Striped<Lock> locks = Striped.lock(1024);
 
     private static Boolean isH2 = null;
 
@@ -53,7 +50,7 @@ public class OptionalIdGenerator extends SequenceStyleGenerator {
         if (object == null) throw new HibernateException(new NullPointerException());
 
         Optional<EntityStructure> optionalEntityStructure = entityStructure(object);
-        if(optionalEntityStructure.isPresent()) {
+        if (optionalEntityStructure.isPresent()) {
 
             EntityStructure entityStructure = optionalEntityStructure.get();
 
@@ -81,7 +78,7 @@ public class OptionalIdGenerator extends SequenceStyleGenerator {
 
     private long generateId(String tableName, EntityStructure entityStructure, SessionImpl sessionImpl, ConcurrentLinkedQueue<Long> availableIds) {
 
-        if(availableIds.isEmpty()) {
+        if (availableIds.isEmpty()) {
             generateNewIds(tableName, sessionImpl, availableIds);
         }
 
@@ -99,13 +96,27 @@ public class OptionalIdGenerator extends SequenceStyleGenerator {
         return nextId.longValue();
     }
 
+    /**
+     * All previously fetched IDs are taken for this table. Generate new IDs.
+     * Will lock per table to avoid fetching and inserting IDs concurrently from the database.
+     * But there might be a potential issue with inserting reserved IDs during the transaction,
+     * because another transaction would not see those IDs in the database until the first transaction commits/flushes.
+     *
+     * On the other hand, we have these available IDs in a a ConcurrentLinkedQueue. So IDs would be available to the next
+     * transaction/thread when lock is unlocked, even though the transaction is not commited. But what about multiple instances?
+     * One opportunity is to use Hazelcast to share memory fetched IDs amongst instances and use map locks.
+     *
+     * @param tableName table to generate IDs for
+     * @param sessionImpl session to use for fetching and inserting reserved IDs
+     * @param availableIds The (empty) queue of available IDs to fill
+     */
     private void generateNewIds(String tableName, SessionImpl sessionImpl, ConcurrentLinkedQueue<Long> availableIds) {
-        final Lock lock = stripedSemaphores.get(tableName);
+        final Lock lock = locks.get(tableName);
 
         try {
             lock.lock();
 
-            while(availableIds.isEmpty()) {
+            while (availableIds.isEmpty()) {
                 List<Long> retrievedIds = retrieveIds(tableName, sessionImpl);
                 logger.trace("Inserting {} ids", retrievedIds);
                 insertRetrievedIds(tableName, retrievedIds, sessionImpl);
@@ -116,30 +127,33 @@ public class OptionalIdGenerator extends SequenceStyleGenerator {
         }
     }
 
-
     private String determineTableName(SessionImpl sessionImpl, Object object) {
         return ((AbstractEntityPersister) sessionImpl.getSessionFactory().getAllClassMetadata().get(object.getClass().getCanonicalName())).getTableName();
     }
 
     private Optional<EntityStructure> entityStructure(Object object) {
-        if(object instanceof EntityStructure) {
+        if (object instanceof EntityStructure) {
             EntityStructure entityStructure = (EntityStructure) object;
 
-            logger.trace("{} is instance of entity structre",entityStructure);
+            logger.trace("{} is instance of entity structre", entityStructure);
 
-            if((entityStructure instanceof StopPlace||entityStructure instanceof Quay)){
+            if ((entityStructure instanceof StopPlace || entityStructure instanceof Quay)) {
                 return Optional.of(entityStructure);
             }
         }
         return Optional.empty();
     }
 
+    /**
+     * Fetch new IDs when all previously fetched IDs taken.
+     * @return list of available IDs for table.
+     */
     @SuppressWarnings(value = "unchecked")
     private List<Long> retrieveIds(String tableName, SessionImpl sessionImpl) {
         Long lastId = lastIdsPerTable.get(tableName);
 
-        if(isH2(sessionImpl)) {
-            // Because of issues using the query below with H2.
+        if (isH2(sessionImpl)) {
+            // Because of issues using the query below or query with system range with H2.
             return generateNextAvailableH2Ids(tableName, lastId, ID_FETCH_SIZE);
         }
 
@@ -151,7 +165,6 @@ public class OptionalIdGenerator extends SequenceStyleGenerator {
 
         SQLQuery sqlQuery = sessionImpl.createSQLQuery(sql);
         sqlQuery.setFlushMode(FlushMode.COMMIT);
-//        sqlQuery.setFlushMode(FlushMode.MANUAL);
         sqlQuery.addScalar("generated", LongType.INSTANCE);
 
         List list = sqlQuery.list();
@@ -170,13 +183,12 @@ public class OptionalIdGenerator extends SequenceStyleGenerator {
         }
         SQLQuery query = sessionImpl.createSQLQuery(insertUsedIdsSql.toString());
         query.setFlushMode(FlushMode.COMMIT);
-//        query.setFlushMode(FlushMode.MANUAL);
         query.executeUpdate();
     }
 
 
     private boolean isH2(SessionImpl sessionImpl) {
-        if(isH2 == null) {
+        if (isH2 == null) {
             try {
                 isH2 = sessionImpl.getPersistenceContext().getSession().connection().getMetaData().getDatabaseProductName().contains("H2");
                 logger.info("Detected H2: {}", isH2);
@@ -187,15 +199,17 @@ public class OptionalIdGenerator extends SequenceStyleGenerator {
         return isH2;
     }
 
-
-    public static List<Long> generateNextAvailableH2Ids(String tableName, long lastId, int max)  {
+    /**
+     * Generate new in-memory IDs for H2.
+     */
+    private List<Long> generateNextAvailableH2Ids(String tableName, long lastId, int max) {
         List<Long> availableIds = new ArrayList<>();
 
         usedH2Ids.putIfAbsent(tableName, new ConcurrentLinkedQueue<>());
 
         Long id = lastId;
         Long counter = 0L;
-        while(counter < max) {
+        while (counter < max) {
             while (usedH2Ids.get(tableName).contains(id)) {
                 logger.debug("Looking for next available ID. {} is taken", id);
             }
