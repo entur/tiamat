a1d6fe37460efaf70c37182ea0029c7438c4eabf
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-02-09 13:43:57 +0100

    NRP-707 Make PathLinkTest transaction so changes are rolled back after test execution

--- src/test/java/org/rutebanken/tiamat/model/PathLinkTest.java
+++ src/test/java/org/rutebanken/tiamat/model/PathLinkTest.java
@@ -12,12 +12,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = TiamatApplication.class)
 @ActiveProfiles("geodb")
+@Transactional
 public class PathLinkTest {
 
     @Autowired
