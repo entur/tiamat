52de6be69fe181ef43f6aa0cc73717292715a3c0
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-02-10 10:28:34 +0100

    NRP-1188 Use random port. Clear repos.

--- src/test/java/org/rutebanken/tiamat/CommonSpringBootTest.java
+++ src/test/java/org/rutebanken/tiamat/CommonSpringBootTest.java
@@ -1,16 +1,43 @@
 package org.rutebanken.tiamat;
 
+import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.rutebanken.tiamat.repository.PathLinkRepository;
+import org.rutebanken.tiamat.repository.QuayRepository;
+import org.rutebanken.tiamat.repository.StopPlaceRepository;
+import org.rutebanken.tiamat.repository.TopographicPlaceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.DEFINED_PORT, classes = TiamatTestApplication.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT, classes = TiamatTestApplication.class)
 @ActiveProfiles("geodb")
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 public abstract class CommonSpringBootTest {
+
+    @Autowired
+    private StopPlaceRepository stopPlaceRepository;
+
+    @Autowired
+    private TopographicPlaceRepository topographicPlaceRepository;
+
+    @Autowired
+    private PathLinkRepository pathLinkRepository;
+
+    @Autowired
+    private QuayRepository quayRepository;
+
+    @Before
+    public void clearRepositories() {
+        pathLinkRepository.deleteAll();
+        stopPlaceRepository.deleteAll();
+        quayRepository.deleteAll();
+        topographicPlaceRepository.deleteAll();
+    }
 }
