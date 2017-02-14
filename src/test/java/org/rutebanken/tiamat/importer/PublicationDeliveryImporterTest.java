217f4e6ae82b4ef4eb5422aff668b46e8f3d2529
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-02-14 17:04:50 +0100

    NRP-1194 Use flatMap for commonFrames

---
+++ src/test/java/org/rutebanken/tiamat/importer/PublicationDeliveryImporterTest.java
@@ -0,0 +1,54 @@
+package org.rutebanken.tiamat.importer;
+
+import org.junit.Test;
+import org.rutebanken.netex.model.*;
+import org.rutebanken.tiamat.exporter.PublicationDeliveryExporter;
+import org.rutebanken.tiamat.importer.modifier.StopPlacePreSteps;
+import org.rutebanken.tiamat.netex.mapping.NetexMapper;
+
+import javax.xml.bind.JAXBElement;
+
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.mock;
+
+public class PublicationDeliveryImporterTest {
+
+    PublicationDeliveryImporter publicationDeliveryImporter = new PublicationDeliveryImporter(mock(NetexMapper.class), mock(TransactionalStopPlacesImporter.class), mock(PublicationDeliveryExporter.class), mock(StopPlacePreSteps.class), mock(PathLinksImporter.class));
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void findSiteFrameFromCompositeFrame() {
+        ObjectFactory objectFactory = new ObjectFactory();
+
+        PublicationDeliveryStructure publicationDeliveryStructure = new PublicationDeliveryStructure()
+                .withDataObjects(
+                        new PublicationDeliveryStructure.DataObjects()
+                                .withCompositeFrameOrCommonFrame(
+                                        objectFactory.createCompositeFrame(
+                                                new CompositeFrame()
+                                                        .withFrames(new Frames_RelStructure()
+                                                            .withCommonFrame(objectFactory.createCommonFrame(new SiteFrame()))))));
+
+        SiteFrame siteFrame = publicationDeliveryImporter.findSiteFrame(publicationDeliveryStructure);
+        assertThat(siteFrame).isNotNull();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void findSiteFrameFromCommonFrame() {
+        ObjectFactory objectFactory = new ObjectFactory();
+        PublicationDeliveryImporter publicationDeliveryImporter = new PublicationDeliveryImporter(mock(NetexMapper.class), mock(TransactionalStopPlacesImporter.class), mock(PublicationDeliveryExporter.class), mock(StopPlacePreSteps.class), mock(PathLinksImporter.class));
+
+
+        PublicationDeliveryStructure publicationDeliveryStructure = new PublicationDeliveryStructure()
+                .withDataObjects(
+                        new PublicationDeliveryStructure.DataObjects()
+                                .withCompositeFrameOrCommonFrame(
+                                        objectFactory.createCommonFrame(new SiteFrame())));
+
+        SiteFrame siteFrame = publicationDeliveryImporter.findSiteFrame(publicationDeliveryStructure);
+        assertThat(siteFrame).isNotNull();
+    }
+
+}
