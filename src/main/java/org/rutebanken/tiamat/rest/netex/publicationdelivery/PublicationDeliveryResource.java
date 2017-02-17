fd5cdacc4ef3579c7979c4864bcd6c12d353579b
erlendnils1 <erlendnils1@gmail.com>
2017-02-17 12:58:47 +0100

    Added method to stream contents of export

--- src/main/java/org/rutebanken/tiamat/rest/netex/publicationdelivery/PublicationDeliveryResource.java
+++ src/main/java/org/rutebanken/tiamat/rest/netex/publicationdelivery/PublicationDeliveryResource.java
@@ -104,7 +104,7 @@ public class PublicationDeliveryResource {
 
     @GET
     @Path(ASYNC_JOB_URL+"/{id}")
-    public Response getJobContents(@PathParam(value = "id") long exportJobId) {
+    public Response getJob(@PathParam(value = "id") long exportJobId) {
 
         ExportJob exportJob = asyncPublicationDeliveryExporter.getExportJob(exportJobId);
 
@@ -114,6 +114,21 @@ public class PublicationDeliveryResource {
 
         logger.info("Returning job {}", exportJob);
         return Response.ok(exportJob).build();
+    }
+
+    @GET
+    @Path(ASYNC_JOB_URL+"/{id}/content")
+    public Response getJobContents(@PathParam(value = "id") long exportJobId) {
+
+        ExportJob exportJob = asyncPublicationDeliveryExporter.getExportJob(exportJobId);
+
+        logger.info("Returning result of job {}", exportJob);
+        if(!exportJob.getStatus().equals(JobStatus.FINISHED)) {
+            return Response.accepted("Job status is not FINISHED for job: "+exportJob).build();
+        }
+
+        InputStream inputStream = asyncPublicationDeliveryExporter.getJobFileContent(exportJob);
+        return Response.ok(inputStream).build();
     }
 
     @GET
