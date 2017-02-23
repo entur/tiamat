c4766e5832ba6dfb862b6d354847775bad40341d
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-02-23 15:56:17 +0100

    NRP-707 Load references from path link end (from/to) from repository. Map some fields from incoming path link, if existing one exists.

--- src/main/java/org/rutebanken/tiamat/rest/graphql/PathLinkUpdater.java
+++ src/main/java/org/rutebanken/tiamat/rest/graphql/PathLinkUpdater.java
@@ -1,10 +1,14 @@
 package org.rutebanken.tiamat.rest.graphql;
 
+import com.google.common.collect.Sets;
 import graphql.language.*;
 import graphql.schema.DataFetcher;
 import graphql.schema.DataFetchingEnvironment;
-import org.rutebanken.tiamat.model.PathLink;
+import org.rutebanken.tiamat.model.*;
+import org.rutebanken.tiamat.netex.mapping.mapper.NetexIdMapper;
 import org.rutebanken.tiamat.repository.PathLinkRepository;
+import org.rutebanken.tiamat.repository.QuayRepository;
+import org.rutebanken.tiamat.repository.StopPlaceRepository;
 import org.rutebanken.tiamat.rest.graphql.resolver.PathLinkMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,6 +32,12 @@ class PathLinkUpdater implements DataFetcher {
 
     @Autowired
     private PathLinkMapper pathLinkMapper;
+
+    @Autowired
+    private QuayRepository quayRepository;
+
+    @Autowired
+    private StopPlaceRepository stopPlaceRepository;
 
     @Override
     public Object get(DataFetchingEnvironment environment) {
@@ -63,33 +73,78 @@ class PathLinkUpdater implements DataFetcher {
         return pathLinkRepository.findAll();
     }
 
-    public PathLink createOrUpdatePathLink(PathLink pathLink) {
+    public PathLink createOrUpdatePathLink(PathLink incomingPathLink) {
 
-        if(pathLink.getId() != null) {
+        PathLink resultPathLink;
+
+        if(incomingPathLink.getId() != null) {
             // Update?
 
-            logger.info("Looking for PathLink with ID: {}", pathLink.getId());
+            logger.info("Looking for PathLink with ID: {}", incomingPathLink.getId());
 
-            PathLink existingPathLink = pathLinkRepository.findOne(pathLink.getId());
+            PathLink existingPathLink = pathLinkRepository.findOne(incomingPathLink.getId());
 
             if(existingPathLink == null) {
-                logger.warn("Specified path link with ID: {} does not exist", pathLink.getId());
+                logger.warn("Specified path link with ID: {} does not exist", incomingPathLink.getId());
+                return null;
             }
 
             logger.info("Found existing path link: {}", existingPathLink);
 
+            existingPathLink.setLineString(incomingPathLink.getLineString());
+            existingPathLink.setAllowedUse(incomingPathLink.getAllowedUse());
+            existingPathLink.setTransferDuration(incomingPathLink.getTransferDuration());
 
-
-
+            resultPathLink = existingPathLink;
 
         } else {
-            // new?
-            logger.info("New path link {}", pathLink.getId());
+            logger.info("New path link {}", incomingPathLink.getId());
+            resultPathLink = incomingPathLink;
         }
 
-        return pathLink;
+
+        if(incomingPathLink.getFrom() != null) {
+            PathLinkEnd from = loadPathLinkEndReference(incomingPathLink.getFrom());
+            resultPathLink.setFrom(from);
+        }
+        if(incomingPathLink.getTo() != null) {
+            PathLinkEnd to = loadPathLinkEndReference(incomingPathLink.getTo());
+            resultPathLink.setTo(to);
+        }
+
+        pathLinkRepository.save(resultPathLink);
+
+
+        return resultPathLink;
 
     }
+
+    private PathLinkEnd loadPathLinkEndReference(PathLinkEnd pathLinkEnd) {
+
+        if(pathLinkEnd.getQuay() != null) {
+            // Quay is only mapped with ID. Need to look it up and replace the reference.
+
+            Quay quay = quayRepository.findOne(pathLinkEnd.getQuay().getId());
+            if(quay != null) {
+                logger.info("Found quay {}", quay);
+                pathLinkEnd.setQuay(quay);
+                return pathLinkEnd;
+            }
+        }
+
+        if(pathLinkEnd.getStopPlace() != null) {
+            StopPlace stopPlace = stopPlaceRepository.findOne(pathLinkEnd.getStopPlace().getId());
+            if(stopPlace != null) {
+                logger.info("Found stop place {}", stopPlace);
+                pathLinkEnd.setStopPlace(stopPlace);
+                return pathLinkEnd;
+            }
+        }
+
+        return null;
+        // TODO entrance/level
+    }
+
 
 
 }
