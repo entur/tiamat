314d1b70c8dbd2152aae388256b34f6f4761c5a7
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-02-23 15:17:19 +0100

    NRP-707 Use IdResolver in PathLinkFetcher

--- src/main/java/org/rutebanken/tiamat/rest/graphql/PathLinkFetcher.java
+++ src/main/java/org/rutebanken/tiamat/rest/graphql/PathLinkFetcher.java
@@ -4,6 +4,7 @@ import graphql.schema.DataFetcher;
 import graphql.schema.DataFetchingEnvironment;
 import org.rutebanken.tiamat.netex.mapping.mapper.NetexIdMapper;
 import org.rutebanken.tiamat.repository.PathLinkRepository;
+import org.rutebanken.tiamat.rest.graphql.resolver.IdResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,12 +20,14 @@ class PathLinkFetcher implements DataFetcher {
     @Autowired
     private PathLinkRepository pathLinkRepository;
 
+    @Autowired
+    private IdResolver idResolver;
+
     @Override
     public Object get(DataFetchingEnvironment environment) {
-        if (environment.getArgument(ID) != null) {
-            String netexId = environment.<String>getArgument(ID);
-            Long tiamatId = NetexIdMapper.getTiamatId(netexId);
 
+        Long tiamatId = idResolver.extractIdIfPresent(ID, environment.getArguments());
+        if (tiamatId != null) {
             return Arrays.asList(pathLinkRepository.findOne(tiamatId));
         }
         return pathLinkRepository.findAll();
