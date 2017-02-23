fd132153f7e0e6fc69ae2afc3be72f6e705675ec
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-02-23 18:06:44 +0100

    NRP-707 Implement integration test for creating pathlink between two quays

--- src/test/java/org/rutebanken/tiamat/rest/graphql/GraphQLResourcePathLinkIntegrationTest.java
+++ src/test/java/org/rutebanken/tiamat/rest/graphql/GraphQLResourcePathLinkIntegrationTest.java
@@ -4,6 +4,7 @@ import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.CoordinateSequence;
 import com.vividsolutions.jts.geom.LineString;
 import com.vividsolutions.jts.geom.impl.CoordinateArraySequence;
+import graphql.GraphQL;
 import org.junit.Test;
 import org.rutebanken.tiamat.model.EmbeddableMultilingualString;
 import org.rutebanken.tiamat.model.PathLink;
@@ -14,8 +15,8 @@ import org.rutebanken.tiamat.repository.PathLinkRepository;
 import org.rutebanken.tiamat.repository.QuayRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.hamcrest.Matchers.comparesEqualTo;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.*;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.MUTATE_PATH_LINK;
 
 public class GraphQLResourcePathLinkIntegrationTest extends GraphQLResourceIntegrationTest {
 
@@ -79,6 +80,63 @@ public class GraphQLResourcePathLinkIntegrationTest extends GraphQLResourceInteg
                     .body("quay.description.value", equalTo(firstQuay.getDescription().getValue()))
                 .root("data.pathLink[0].to")
                     .body("id", comparesEqualTo(NetexIdMapper.getNetexId(pathLink.getTo())))
+                    .body("quay.id", equalTo(NetexIdMapper.getNetexId(secondQuay)))
+                    .body("quay.description.value", equalTo(secondQuay.getDescription().getValue()));
+    }
+
+    @Test
+    public void createNewPathLinkBetweenQuays() throws Exception {
+        Quay firstQuay = new Quay();
+        firstQuay.setCentroid(geometryFactory.createPoint(new Coordinate(5, 60)));
+        firstQuay.setDescription(new EmbeddableMultilingualString("This is the first quay"));
+        quayRepository.save(firstQuay);
+
+        Quay secondQuay = new Quay();
+        secondQuay.setCentroid(geometryFactory.createPoint(new Coordinate(5.1, 60.1)));
+        secondQuay.setDescription(new EmbeddableMultilingualString("This is the second quay"));
+        quayRepository.save(secondQuay);
+
+        String graphQlJsonQuery = "{" +
+                "\"query\":\"mutation { " +
+                "  pathLink: " + MUTATE_PATH_LINK + "(PathLink: { " +
+                "       from: {quay: {id: \\\"" + NetexIdMapper.getNetexId(firstQuay) + "\\\"}}, " +
+                "       to: {quay: {id: \\\"" + NetexIdMapper.getNetexId(secondQuay) + "\\\"}}, " +
+                "       geometry: {" +
+                "           type: \\\"LineString\\\", coordinates: [[10.3, 59.9], [10.3, 59.9], [10.3, 59.9], [10.3, 59.9], [10.3, 59.9]] " +
+                "       }" +
+                "   }) {" +
+                "   id " +
+                "   geometry {" +
+                "        type" +
+                "        coordinates" +
+                "       }" +
+                "    from {" +
+                "      id" +
+                "      quay {" +
+                "        id" +
+                "        description {value}" +
+                "      }" +
+                "    }" +
+                "    to {" +
+                "      id" +
+                "      quay {" +
+                "        id" +
+                "        description {value}" +
+                "      }" +
+                "    }" +
+                "  }" +
+                "}\",\"variables\":\"\"}";
+
+        executeGraphQL(graphQlJsonQuery)
+                .root("data.pathLink[0]")
+                    .body("id", notNullValue())
+                    .body("geometry", notNullValue())
+                .root("data.pathLink[0].from")
+                    .body("id", notNullValue())
+                    .body("quay.id", equalTo(NetexIdMapper.getNetexId(firstQuay)))
+                    .body("quay.description.value", equalTo(firstQuay.getDescription().getValue()))
+                .root("data.pathLink[0].to")
+                    .body("id", notNullValue())
                     .body("quay.id", equalTo(NetexIdMapper.getNetexId(secondQuay)))
                     .body("quay.description.value", equalTo(secondQuay.getDescription().getValue()));
     }
