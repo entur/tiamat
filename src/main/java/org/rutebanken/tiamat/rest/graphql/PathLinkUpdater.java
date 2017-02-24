bc21bedb6c3d7a97f5889a16727b0917ff65278e
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-02-24 08:33:01 +0100

    NRP-707 Move PathLinkUpdater logic to PathLinkUpdaterService. For reusability. Reenable exception logging. Delete empty StopPlaceService.

--- src/main/java/org/rutebanken/tiamat/rest/graphql/PathLinkUpdater.java
+++ src/main/java/org/rutebanken/tiamat/rest/graphql/PathLinkUpdater.java
@@ -11,12 +11,14 @@ import org.rutebanken.tiamat.repository.PathLinkRepository;
 import org.rutebanken.tiamat.repository.QuayRepository;
 import org.rutebanken.tiamat.repository.StopPlaceRepository;
 import org.rutebanken.tiamat.rest.graphql.resolver.PathLinkMapper;
+import org.rutebanken.tiamat.service.PathLinkUpdaterService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -36,10 +38,8 @@ class PathLinkUpdater implements DataFetcher {
     private PathLinkMapper pathLinkMapper;
 
     @Autowired
-    private QuayRepository quayRepository;
+    private PathLinkUpdaterService pathLinkUpdaterService;
 
-    @Autowired
-    private StopPlaceRepository stopPlaceRepository;
 
     @Override
     public Object get(DataFetchingEnvironment environment) {
@@ -48,8 +48,7 @@ class PathLinkUpdater implements DataFetcher {
 
         logger.trace("Got fields {}", fields);
 
-//        try {
-
+        try {
             for (Field field : fields) {
                 if (field.getName().equals(MUTATE_PATH_LINK)) {
 
@@ -58,7 +57,8 @@ class PathLinkUpdater implements DataFetcher {
                         PathLink pathLink = pathLinkMapper.map(input);
                         logger.debug("Mapped {}", pathLink);
 
-                        createOrUpdatePathLink(pathLink);
+                        PathLink importedPathLink = pathLinkUpdaterService.createOrUpdatePathLink(pathLink);
+                        return Arrays.asList(importedPathLink);
 
                     } else {
                         logger.warn("Could not find argument {}", OUTPUT_TYPE_PATH_LINK);
@@ -66,92 +66,10 @@ class PathLinkUpdater implements DataFetcher {
 
                 }
             }
-//        } catch (RuntimeException e) {
-//            logger.warn("Caught exception when updating path link", e);
-//            throw e;
-//        }
-
-
+        } catch (RuntimeException e) {
+            logger.warn("Caught exception when updating path link", e);
+            throw e;
+        }
         return pathLinkRepository.findAll();
     }
-
-    public PathLink createOrUpdatePathLink(PathLink incomingPathLink) {
-
-        PathLink resultPathLink;
-
-        boolean updatedExisting;
-
-        if(incomingPathLink.getId() != null) {
-            // Update?
-
-            logger.debug("Looking for PathLink with ID: {}", incomingPathLink.getId());
-
-            PathLink existingPathLink = pathLinkRepository.findOne(incomingPathLink.getId());
-
-            if(existingPathLink == null) {
-                logger.warn("Specified path link with ID: {} does not exist", incomingPathLink.getId());
-                return null;
-            }
-
-            logger.debug("Found existing path link: {}", existingPathLink);
-
-            existingPathLink.setLineString(incomingPathLink.getLineString());
-            existingPathLink.setAllowedUse(incomingPathLink.getAllowedUse());
-            existingPathLink.setTransferDuration(incomingPathLink.getTransferDuration());
-
-            resultPathLink = existingPathLink;
-            updatedExisting = true;
-
-        } else {
-            logger.info("New incoming path link {}", incomingPathLink);
-            resultPathLink = incomingPathLink;
-            updatedExisting = false;
-        }
-
-
-        if(incomingPathLink.getFrom() != null) {
-            PathLinkEnd from = loadPathLinkEndReference(incomingPathLink.getFrom());
-            resultPathLink.setFrom(from);
-        }
-        if(incomingPathLink.getTo() != null) {
-            PathLinkEnd to = loadPathLinkEndReference(incomingPathLink.getTo());
-            resultPathLink.setTo(to);
-        }
-
-        pathLinkRepository.save(resultPathLink);
-
-        logger.info("{} {}", updatedExisting ? "Updated" : "Created", resultPathLink);
-
-        return resultPathLink;
-
-    }
-
-    private PathLinkEnd loadPathLinkEndReference(PathLinkEnd pathLinkEnd) {
-
-        if(pathLinkEnd.getQuay() != null) {
-            // Quay is only mapped with ID. Need to look it up and replace the reference.
-
-            Quay quay = quayRepository.findOne(pathLinkEnd.getQuay().getId());
-            if(quay != null) {
-                logger.debug("Found quay {}", quay);
-                pathLinkEnd.setQuay(quay);
-                return pathLinkEnd;
-            }
-        }
-
-        if(pathLinkEnd.getStopPlace() != null) {
-            StopPlace stopPlace = stopPlaceRepository.findOne(pathLinkEnd.getStopPlace().getId());
-            if(stopPlace != null) {
-                logger.info("Found stop place {}", stopPlace);
-                pathLinkEnd.setStopPlace(stopPlace);
-                return pathLinkEnd;
-            }
-        }
-
-        return null;
-        // TODO entrance/level
-    }
-
-
-
 }
