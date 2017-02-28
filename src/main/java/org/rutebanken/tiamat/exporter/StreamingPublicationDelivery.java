dcda99912ab8dd652288ec8c38db9794517a7951
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-02-28 15:46:10 +0100

    NRP-1230 Remove commented code related to avoiding name spaces for each stop place marshalled

--- src/main/java/org/rutebanken/tiamat/exporter/StreamingPublicationDelivery.java
+++ src/main/java/org/rutebanken/tiamat/exporter/StreamingPublicationDelivery.java
@@ -162,27 +162,4 @@ public class StreamingPublicationDelivery {
             bufferedWriter.write(lineSeparator);
         }
     }
-
-//
-//    public void nonamespacewriter() throws XMLStreamException {
-//
-//        ByteArrayOutputStream stopPlaceOutputStream = new ByteArrayOutputStream();
-//        XMLStreamWriter writer = new XMLOutputFactoryImpl().createXMLStreamWriter(stopPlaceOutputStream);
-//        writer.setNamespaceContext(new NamespaceContext() {
-//            public Iterator getPrefixes(String namespaceURI) {
-//                return null;
-//            }
-//
-//            public String getPrefix(String namespaceURI) {
-//                return "";
-//            }
-//
-//            public String getNamespaceURI(String prefix) {
-//                return null;
-//            }
-//        });
-//        writer.setDefaultNamespace("");
-//
-//    }
-
 }
