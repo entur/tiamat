ed9c194d0bd4db9c455da4c0aea97d62f98a1d83
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-03-08 10:51:13 +0100

    NRP-1237 Enable and fix tests for gapless ID generation

--- src/test/java/org/rutebanken/tiamat/netex/id/GaplessIdGeneratorTest.java
+++ src/test/java/org/rutebanken/tiamat/netex/id/GaplessIdGeneratorTest.java
@@ -5,19 +5,17 @@ import org.hibernate.Session;
 import org.hibernate.engine.spi.SessionImplementor;
 import org.hibernate.jpa.HibernateEntityManagerFactory;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.rutebanken.tiamat.CommonSpringBootTest;
-import org.rutebanken.tiamat.TiamatApplication;
 import org.rutebanken.tiamat.model.EmbeddableMultilingualString;
 import org.rutebanken.tiamat.model.Quay;
 import org.rutebanken.tiamat.model.StopPlace;
+import org.rutebanken.tiamat.netex.mapping.mapper.NetexIdMapper;
+import org.rutebanken.tiamat.repository.QuayRepository;
+import org.rutebanken.tiamat.repository.StopPlaceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.Serializable;
 import java.math.BigInteger;
-import java.util.ArrayList;
 import java.util.List;
 
 import static junit.framework.TestCase.assertEquals;
@@ -25,95 +23,86 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class GaplessIdGeneratorTest extends CommonSpringBootTest {
 
-//    @Autowired
-//    private StopPlaceRepository stopPlaceRepository;
-//
-//    @Autowired
-//    private QuayRepository quayRepository;
-//
-//    @Autowired
-//    private HibernateEntityManagerFactory hibernateEntityManagerFactory;
-//
-//    @Test
-//    public void verifyStopPlaceIsAssignedId() {
-//        StopPlace stopPlace = new StopPlace();
-//        stopPlaceRepository.save(stopPlace);
-//        assertThat(stopPlace.getId()).isNotNull();
-//    }
-//
-//    @Test
-//    public void explicitIdMustBeInserted() {
-//
-//        GaplessOptionalGenerator gaplessOptionalGenerator = new GaplessOptionalGenerator();
-//
-//        Session session = hibernateEntityManagerFactory.getSessionFactory().openSession();
-//
-//        long wantedId = 12L;
-//
-//        Quay quay = new Quay();
-//        quay.setId(wantedId);
-//
-//        Serializable serializable = gaplessOptionalGenerator.generate((SessionImplementor) session, quay);
-//        Long gotId = (Long) serializable;
-//        assertThat(gotId).isNotNull();
-//        assertThat(gotId).isEqualTo(wantedId);
-//
-//        SQLQuery query = session.createSQLQuery("SELECT id_value FROM id_generator WHERE table_name = 'quay' AND id_value = " + wantedId + "");
-//
-//        List list = query.list();
-//        BigInteger actual = (BigInteger) list.get(0);
-//
-//        assertThat(actual.longValue()).describedAs("Expecting to find the ID in the id_generator table").isEqualTo(wantedId);
-//    }
-//
-//    @Test
-//    public void generateIdAfterExplicitIDs() {
-//
-//        GaplessOptionalGenerator gaplessOptionalGenerator = new GaplessOptionalGenerator();
-//
-//        SessionImplementor session = (SessionImplementor) hibernateEntityManagerFactory.getSessionFactory().openSession();
-//
-//        // Use first 500 IDs
-//        for(long explicitId = 1; explicitId <= 600; explicitId ++) {
-//            Quay quay = new Quay();
-//            quay.setId(explicitId);
-//            gaplessOptionalGenerator.generate(session, quay);
-//        }
-//
-//        // Get next ID which should be 501
-//        Serializable serializable = gaplessOptionalGenerator.generate(session, new Quay());
-//        Long gotId = (Long) serializable;
-//        assertThat(gotId).isEqualTo(601);
-//    }
-//
-//    /**
-//     * Was implemented under the supsicion that OptionalIdCreator caused a bug.
-//     * But it was a matter of keeping the attached returned entity from save (in case the entity was merged)
-//     * See NRP-1171
-//     */
-//    @Test
-//    public void testUpdatingStopPlace() {
-//        StopPlace stopPlace = new StopPlace(new EmbeddableMultilingualString("test"));
-//        stopPlace = stopPlaceRepository.save(stopPlace);
-//
-//        assertThat(stopPlace.getId()).isNotNull();
-//        Long id = stopPlace.getId();
-//
-//        Quay quay = new Quay(new EmbeddableMultilingualString("quayTest"));
-//        quay = quayRepository.save(quay);
-//
-//        //Add Quay, and save StopPlace
-//        stopPlace.getQuays().add(quay);
-//        stopPlace = stopPlaceRepository.save(stopPlace);
-//
-//        Quay quay2 = new Quay(new EmbeddableMultilingualString("quay2Test"));
-//        quay2 = quayRepository.save(quay2);
-//
-//        //Add another Quay and save StopPlace
-//        stopPlace.getQuays().add(quay2);
-//        stopPlace = stopPlaceRepository.save(stopPlace);
-//
-//        assertEquals(id, stopPlace.getId());
-//    }
+    @Autowired
+    private StopPlaceRepository stopPlaceRepository;
+
+    @Autowired
+    private QuayRepository quayRepository;
+
+    @Autowired
+    private HibernateEntityManagerFactory hibernateEntityManagerFactory;
+
+    @Test
+    public void verifyNetexIdAssignedToStop() {
+        StopPlace stopPlace = new StopPlace();
+        stopPlaceRepository.save(stopPlace);
+        assertThat(stopPlace.getNetexId()).isNotNull();
+    }
+
+    @Test
+    public void explicitIdMustBeInsertedIntoHelperTable() {
+
+        String wantedId = NetexIdMapper.getNetexId("Quay", "1");
+
+        Quay quay = new Quay();
+        quay.setNetexId(wantedId);
+
+        quayRepository.save(quay);
+
+        Session session = hibernateEntityManagerFactory.getSessionFactory().openSession();
+
+        SQLQuery query = session.createSQLQuery("SELECT id_value FROM id_generator WHERE table_name = '" + Quay.class.getSimpleName() + "' AND id_value = " + wantedId + "");
+
+        List list = query.list();
+        BigInteger actual = (BigInteger) list.get(0);
+
+        assertThat(actual.longValue()).describedAs("Expecting to find the ID in the id_generator table").isEqualTo(wantedId);
+    }
+
+    @Test
+    public void generateIdAfterExplicitIDs() {
+
+        // Use first 500 IDs
+        for(long explicitId = 1; explicitId <= 600; explicitId ++) {
+            Quay quay = new Quay();
+            quay.setNetexId(NetexIdMapper.getNetexId(Quay.class.getSimpleName(), String.valueOf(explicitId)));
+            quayRepository.save(quay);
+        }
+
+        // Get next ID which should be 501
+        Quay quay = new Quay();
+        quayRepository.save(quay);
+        assertThat(NetexIdMapper.getNetexIdPostfix(quay.getNetexId())).isEqualTo(601);
+    }
+
+    /**
+     * Was implemented under the supsicion that OptionalIdCreator caused a bug.
+     * But it was a matter of keeping the attached returned entity from save (in case the entity was merged)
+     * See NRP-1171
+     */
+    @Test
+    public void testUpdatingStopPlace() {
+        StopPlace stopPlace = new StopPlace(new EmbeddableMultilingualString("test"));
+        stopPlace = stopPlaceRepository.save(stopPlace);
+
+        assertThat(stopPlace.getNetexId()).isNotNull();
+        String id = stopPlace.getNetexId();
+
+        Quay quay = new Quay(new EmbeddableMultilingualString("quayTest"));
+        quay = quayRepository.save(quay);
+
+        //Add Quay, and save StopPlace
+        stopPlace.getQuays().add(quay);
+        stopPlace = stopPlaceRepository.save(stopPlace);
+
+        Quay quay2 = new Quay(new EmbeddableMultilingualString("quay2Test"));
+        quay2 = quayRepository.save(quay2);
+
+        //Add another Quay and save StopPlace
+        stopPlace.getQuays().add(quay2);
+        stopPlace = stopPlaceRepository.save(stopPlace);
+
+        assertEquals(id, stopPlace.getNetexId());
+    }
 
 }
