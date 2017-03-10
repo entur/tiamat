fb5704c7e27c76d32ffa3e364fdc69512429711f
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-03-10 12:05:13 +0100

    NRP-1237 Use hazelcast instance for getting map and queues

--- src/main/java/org/rutebanken/tiamat/netex/id/GeneratedIdState.java
+++ src/main/java/org/rutebanken/tiamat/netex/id/GeneratedIdState.java
@@ -1,51 +1,44 @@
 package org.rutebanken.tiamat.netex.id;
 
-import org.eclipse.jetty.util.ConcurrentArrayQueue;
-import org.springframework.stereotype.Component;
+import com.hazelcast.core.HazelcastInstance;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.List;
+import java.util.concurrent.*;
 
 @Service
 public class GeneratedIdState {
 
     public static final int QUEUE_CAPACITY = 100;
+    public static final String LAST_IDS_FOR_ENTITY = "lastIdsForEntities";
+    public static final String CLAIMED_IDS_FOR_ENTITY_PREFIX = "claimedIdsForEntities";
 
+    private final HazelcastInstance hazelcastInstance;
 
-    private final ConcurrentHashMap<String, BlockingQueue<Long>> availableIdsPerEntity = new ConcurrentHashMap<>();
-
-    private final ConcurrentHashMap<String, Long> lastIdsPerEntity = new ConcurrentHashMap<>();
-
-    /**
-     * If an object claims to use a Netex ID. It should be inserted into the helper table.
-     */
-    private final ConcurrentHashMap<String, ConcurrentLinkedQueue<Long>> claimedIds = new ConcurrentHashMap<>();
-
-    public GeneratedIdState () {
+    @Autowired
+    public GeneratedIdState(HazelcastInstance hazelcastInstance) {
+        this.hazelcastInstance = hazelcastInstance;
     }
 
     public void registerEntityTypeName(String entityTypeName, long startLastId) {
-        availableIdsPerEntity.putIfAbsent(entityTypeName, new ArrayBlockingQueue<>(QUEUE_CAPACITY));
-        claimedIds.putIfAbsent(entityTypeName, new ConcurrentLinkedQueue<>());
-        lastIdsPerEntity.putIfAbsent(entityTypeName, startLastId);
+        hazelcastInstance.getMap(LAST_IDS_FOR_ENTITY).putIfAbsent(entityTypeName, startLastId);
     }
 
     public BlockingQueue<Long> getQueueForEntity(String entityTypeName) {
-        return availableIdsPerEntity.get(entityTypeName);
+        return hazelcastInstance.getQueue(entityTypeName);
     }
 
     public void setLastIdForEntity(String entityTypeName, long lastId) {
-        lastIdsPerEntity.put(entityTypeName, lastId);
+        hazelcastInstance.getMap(LAST_IDS_FOR_ENTITY).put(entityTypeName, lastId);
     }
 
     public long getLastIdForEntity(String entityTypeName) {
-        return lastIdsPerEntity.get(entityTypeName);
+        ConcurrentMap<String, Long> lastIdMap = hazelcastInstance.getMap(LAST_IDS_FOR_ENTITY);
+        return lastIdMap.get(entityTypeName);
     }
 
-    public ConcurrentLinkedQueue<Long> getClaimedIdQueueForEntity(String entityTypeName) {
-        return claimedIds.get(entityTypeName);
+    public List<Long> getClaimedIdQueueForEntity(String entityTypeName) {
+        return hazelcastInstance.getList(CLAIMED_IDS_FOR_ENTITY_PREFIX + "-" + entityTypeName);
     }
 }
