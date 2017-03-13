31b4f32b622f54860f9cdc18d00321aca903b3b3
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-03-13 10:22:27 +0100

    NRP-1237 Attempt to reset ID queues after test using a test execution listener.

--- src/test/java/org/rutebanken/tiamat/CommonSpringBootTest.java
+++ src/test/java/org/rutebanken/tiamat/CommonSpringBootTest.java
@@ -3,24 +3,42 @@ package org.rutebanken.tiamat;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.rutebanken.tiamat.repository.PathLinkRepository;
 import org.rutebanken.tiamat.repository.QuayRepository;
 import org.rutebanken.tiamat.repository.StopPlaceRepository;
 import org.rutebanken.tiamat.repository.TopographicPlaceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.RestDocsTestExecutionListener;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockitoTestExecutionListener;
+import org.springframework.boot.test.mock.mockito.ResetMocksTestExecutionListener;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.jdbc.SqlScriptsTestExecutionListener;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextBeforeModesTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.test.context.web.ServletTestExecutionListener;
 import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT, classes = TiamatTestApplication.class)
 @ActiveProfiles("geodb")
-@TestExecutionListeners(TestCleanUpExecutionListener.class)
+@TestExecutionListeners(listeners = {
+        ServletTestExecutionListener.class,
+        DirtiesContextBeforeModesTestExecutionListener.class,
+        TransactionalTestExecutionListener.class,
+        SqlScriptsTestExecutionListener.class,
+        ResetMocksTestExecutionListener.class,
+        RestDocsTestExecutionListener.class,
+        MockitoTestExecutionListener.class,
+        DependencyInjectionTestExecutionListener.class,
+        TestCleanUpExecutionListener.class })
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 public abstract class CommonSpringBootTest {
 
--- src/test/java/org/rutebanken/tiamat/TestCleanUpExecutionListener.java
+++ src/test/java/org/rutebanken/tiamat/TestCleanUpExecutionListener.java
@@ -1,26 +1,32 @@
 package org.rutebanken.tiamat;
 
 
+import com.hazelcast.core.HazelcastInstance;
 import org.rutebanken.tiamat.netex.id.GaplessIdGenerator;
 import org.rutebanken.tiamat.netex.id.GeneratedIdState;
 import org.springframework.test.context.TestContext;
 import org.springframework.test.context.TestExecutionListener;
+import org.springframework.test.context.support.AbstractTestExecutionListener;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
-public class TestCleanUpExecutionListener extends DependencyInjectionTestExecutionListener {
-/*
+public class TestCleanUpExecutionListener extends AbstractTestExecutionListener {
+
     @Override
     public void prepareTestInstance(TestContext testContext) throws Exception {
         super.prepareTestInstance(testContext);
         GeneratedIdState generatedIdState = testContext.getApplicationContext().getBean(GeneratedIdState.class);
         GaplessIdGenerator gaplessIdGenerator = testContext.getApplicationContext().getBean(GaplessIdGenerator.class);
+        HazelcastInstance hazelcastInstance = testContext.getApplicationContext().getBean(HazelcastInstance.class);
+
         gaplessIdGenerator.getEntityTypeNames().forEach(entityTypeName -> {
             generatedIdState.getClaimedIdQueueForEntity(entityTypeName).clear();
             generatedIdState.getLastIdForEntityMap().put(entityTypeName, 1L);
             generatedIdState.getQueueForEntity(entityTypeName).clear();
+            hazelcastInstance.getList("used-h2-ids-by-entity-" + entityTypeName).clear();
+
         });
 
     }
-*
+
 
 }
