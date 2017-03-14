16974c9ee7ccd6dc642d53c97ac5fae5ff6babc7
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-03-14 17:40:05 +0100

    NRP-1237 Remove cleanup method

--- src/test/java/org/rutebanken/tiamat/CommonSpringBootTest.java
+++ src/test/java/org/rutebanken/tiamat/CommonSpringBootTest.java
@@ -2,31 +2,18 @@ package org.rutebanken.tiamat;
 
 import com.hazelcast.core.HazelcastInstance;
 import org.junit.After;
-import org.junit.Before;
 import org.junit.runner.RunWith;
-import org.rutebanken.tiamat.netex.id.GaplessIdGenerator;
 import org.rutebanken.tiamat.netex.id.GeneratedIdState;
 import org.rutebanken.tiamat.repository.PathLinkRepository;
 import org.rutebanken.tiamat.repository.QuayRepository;
 import org.rutebanken.tiamat.repository.StopPlaceRepository;
 import org.rutebanken.tiamat.repository.TopographicPlaceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.RestDocsTestExecutionListener;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockitoTestExecutionListener;
-import org.springframework.boot.test.mock.mockito.ResetMocksTestExecutionListener;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.jdbc.SqlScriptsTestExecutionListener;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.context.support.DirtiesContextBeforeModesTestExecutionListener;
 import org.springframework.test.context.transaction.AfterTransaction;
-import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
-import org.springframework.test.context.web.ServletTestExecutionListener;
-
-import static org.rutebanken.tiamat.netex.id.GaplessIdGeneratorTask.USED_H2_IDS_BY_ENTITY;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT, classes = TiamatTestApplication.class)
@@ -46,15 +33,6 @@ public abstract class CommonSpringBootTest {
     @Autowired
     private QuayRepository quayRepository;
 
-    @Autowired
-    private GeneratedIdState generatedIdState;
-
-    @Autowired
-    private GaplessIdGenerator gaplessIdGenerator;
-
-    @Autowired
-    private HazelcastInstance hazelcastInstance;
-
     @After
     public void clearRepositories() {
 
@@ -69,18 +47,5 @@ public abstract class CommonSpringBootTest {
 
         topographicPlaceRepository.deleteAll();
         topographicPlaceRepository.flush();
-
-
-    }
-
-    @AfterTransaction
-    public void cleanUpGaplessIdGenerator() {
-//        gaplessIdGenerator.getEntityTypeNames().forEach(entityTypeName -> {
-//            generatedIdState.getClaimedIdListForEntity(entityTypeName).clear();
-//            generatedIdState.getLastIdForEntityMap().put(entityTypeName, 1L);
-//            generatedIdState.getQueueForEntity(entityTypeName).clear();
-//            hazelcastInstance.getList(USED_H2_IDS_BY_ENTITY + entityTypeName).clear();
-//
-//        });
     }
 }
