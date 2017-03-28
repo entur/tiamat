22d6bc31d0c5bb8dd7dc31c393ad5856ba9652f9
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-03-28 15:22:13 +0200

    NRP-25 Find all versions of object (if it is instance of EntityInVersionStructure) by netexId

--- src/main/java/org/rutebanken/tiamat/repository/IdentifiedEntityRepository.java
+++ src/main/java/org/rutebanken/tiamat/repository/IdentifiedEntityRepository.java
@@ -5,9 +5,13 @@ import org.rutebanken.tiamat.model.identification.IdentifiedEntity;
 import org.springframework.data.jpa.repository.support.JpaEntityInformation;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 
+import java.util.List;
+
 public interface IdentifiedEntityRepository<T extends IdentifiedEntity> {
 
     T findFirstByNetexIdOrderByVersionDesc(String netexId);
 
     T findFirstByNetexIdAndVersion(String netexId, long version);
+
+    List<T> findByNetexId(String netexId);
 }
