ed969845e5051c3d5405294fc80767075603a7f0
erlendnils1 <erlendnils1@gmail.com>
2017-03-29 22:39:41 +0200

    NRP-16 Map correct response status for access denied

--- src/main/java/org/rutebanken/tiamat/config/JerseyConfig.java
+++ src/main/java/org/rutebanken/tiamat/config/JerseyConfig.java
@@ -5,6 +5,7 @@ import org.rutebanken.tiamat.rest.coordinates.CoordinatesFixerResource;
 import org.rutebanken.tiamat.rest.dto.DtoQuayResource;
 import org.rutebanken.tiamat.rest.dto.DtoStopPlaceResource;
 import org.rutebanken.tiamat.rest.dto.DtoTopographicPlaceResource;
+import org.rutebanken.tiamat.rest.exception.AccessDeniedExceptionMapper;
 import org.rutebanken.tiamat.rest.graphql.GraphQLResource;
 import org.rutebanken.tiamat.rest.netex.publicationdelivery.InitialImportResource;
 import org.rutebanken.tiamat.rest.netex.publicationdelivery.PublicationDeliveryResource;
@@ -25,5 +26,7 @@ public class JerseyConfig extends ResourceConfig {
         register(InitialImportResource.class);
         register(GraphQLResource.class);
         register(StopPlaceTopographicRefUpdaterResource.class);
+
+        register(AccessDeniedExceptionMapper.class);
     }
 }
