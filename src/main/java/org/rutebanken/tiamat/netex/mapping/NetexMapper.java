e432e4698e7316c462359b82b280030d522fded8
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-04-04 19:24:41 +0200

    NRP-1340 AccessibilityAssessment is not a subclass of DataManagedObjectStructure, which is a problem, becase we only support mapping DataManagedObjectStructure with key values and ID. Temporary creating a custom mapper for AccessibilityAssessment.

--- src/main/java/org/rutebanken/tiamat/netex/mapping/NetexMapper.java
+++ src/main/java/org/rutebanken/tiamat/netex/mapping/NetexMapper.java
@@ -3,6 +3,13 @@ package org.rutebanken.tiamat.netex.mapping;
 import ma.glasnost.orika.*;
 import ma.glasnost.orika.impl.DefaultMapperFactory;
 import org.rutebanken.netex.model.*;
+import org.rutebanken.netex.model.AccessibilityAssessment;
+import org.rutebanken.netex.model.DataManagedObjectStructure;
+import org.rutebanken.netex.model.PathLink;
+import org.rutebanken.netex.model.Quay;
+import org.rutebanken.netex.model.SiteFrame;
+import org.rutebanken.netex.model.StopPlace;
+import org.rutebanken.netex.model.TopographicPlace;
 import org.rutebanken.netex.model.DataManagedObjectStructure;
 import org.rutebanken.netex.model.PathLink;
 import org.rutebanken.netex.model.Quay;
@@ -10,10 +17,7 @@ import org.rutebanken.netex.model.SiteFrame;
 import org.rutebanken.netex.model.StopPlace;
 import org.rutebanken.netex.model.TopographicPlace;
 import org.rutebanken.tiamat.netex.mapping.converter.*;
-import org.rutebanken.tiamat.netex.mapping.mapper.DataManagedObjectStructureMapper;
-import org.rutebanken.tiamat.netex.mapping.mapper.KeyListToKeyValuesMapMapper;
-import org.rutebanken.tiamat.netex.mapping.mapper.NetexIdMapper;
-import org.rutebanken.tiamat.netex.mapping.mapper.TopographicPlaceMapper;
+import org.rutebanken.tiamat.netex.mapping.mapper.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -72,6 +76,25 @@ public class NetexMapper {
                 .byDefault()
                 .register();
 
+        NetexIdMapper netexIdMapper = new NetexIdMapper();
+
+        mapperFactory.classMap(AccessibilityAssessment.class, org.rutebanken.tiamat.model.AccessibilityAssessment.class)
+                .customize(new CustomMapper<AccessibilityAssessment, org.rutebanken.tiamat.model.AccessibilityAssessment>() {
+                    @Override
+                    public void mapAtoB(AccessibilityAssessment accessibilityAssessment, org.rutebanken.tiamat.model.AccessibilityAssessment accessibilityAssessment2, MappingContext context) {
+                        super.mapAtoB(accessibilityAssessment, accessibilityAssessment2, context);
+                        netexIdMapper.toTiamatModel(accessibilityAssessment, accessibilityAssessment2);
+                    }
+
+                    @Override
+                    public void mapBtoA(org.rutebanken.tiamat.model.AccessibilityAssessment accessibilityAssessment, AccessibilityAssessment accessibilityAssessment2, MappingContext context) {
+                        super.mapBtoA(accessibilityAssessment, accessibilityAssessment2, context);
+                        netexIdMapper.toNetexModel(accessibilityAssessment, accessibilityAssessment2);
+                    }
+                })
+                .exclude("id")
+                .byDefault()
+                .register();
 
         mapperFactory.classMap(DataManagedObjectStructure.class, org.rutebanken.tiamat.model.DataManagedObjectStructure.class)
                 .fieldBToA("keyValues", "keyList")
