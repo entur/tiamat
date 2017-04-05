2a5768278a2927e4ae7bb36db53aaf15665e736a
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-04-05 18:38:34 +0200

    NRP-1346 Use sequence for primary key of PersitablePolygon. Because the gapless ID generator is quite slow.

--- src/main/java/org/rutebanken/tiamat/model/PersistablePolygon.java
+++ src/main/java/org/rutebanken/tiamat/model/PersistablePolygon.java
@@ -5,9 +5,15 @@ import com.vividsolutions.jts.geom.Polygon;
 import org.rutebanken.tiamat.model.identification.IdentifiedEntity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
-public class PersistablePolygon extends IdentifiedEntity {
+public class PersistablePolygon  {
+
+    @Id
+    @GeneratedValue(generator="sequence_per_table_generator")
+    protected Long id;
 
     private Polygon polygon;
 
