05df98a46815c4b8cb15c0fb1162bd63ef84b957
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-04-06 18:48:34 +0200

    NRP-1346 Initial stop place importer does not have to create topographic places

--- src/main/java/org/rutebanken/tiamat/importer/initial/InitialStopPlaceImporter.java
+++ src/main/java/org/rutebanken/tiamat/importer/initial/InitialStopPlaceImporter.java
@@ -1,19 +1,22 @@
 package org.rutebanken.tiamat.importer.initial;
 
 
-import org.rutebanken.tiamat.importer.TopographicPlaceCreator;
+import org.rutebanken.tiamat.model.Quay;
 import org.rutebanken.tiamat.model.SiteFrame;
 import org.rutebanken.tiamat.model.StopPlace;
 import org.rutebanken.tiamat.netex.mapping.NetexMapper;
 import org.rutebanken.tiamat.repository.QuayRepository;
+import org.rutebanken.tiamat.repository.ReferenceResolver;
 import org.rutebanken.tiamat.repository.StopPlaceRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 /**
  * Import stop place without taking existing data into account.
@@ -30,34 +33,23 @@ public class InitialStopPlaceImporter {
 
     private final NetexMapper netexMapper;
 
-    private final TopographicPlaceCreator topographicPlaceCreator;
-
     @Autowired
-    public InitialStopPlaceImporter(QuayRepository quayRepository, StopPlaceRepository stopPlaceRepository, NetexMapper netexMapper, TopographicPlaceCreator topographicPlaceCreator) {
+    public InitialStopPlaceImporter(QuayRepository quayRepository, StopPlaceRepository stopPlaceRepository, NetexMapper netexMapper) {
         this.quayRepository = quayRepository;
         this.stopPlaceRepository = stopPlaceRepository;
         this.netexMapper = netexMapper;
-        this.topographicPlaceCreator = topographicPlaceCreator;
     }
 
-    public org.rutebanken.netex.model.StopPlace importStopPlace(AtomicInteger stopPlacesImported, AtomicInteger topographicPlacesCreatedCounter, SiteFrame siteFrame, StopPlace stopPlace) throws InterruptedException, ExecutionException {
+    public org.rutebanken.netex.model.StopPlace importStopPlace(AtomicInteger stopPlacesImported, StopPlace stopPlace) throws InterruptedException, ExecutionException {
 
-        if(siteFrame.getTopographicPlaces() != null) {
-            topographicPlaceCreator.setTopographicReference(stopPlace,
-                    siteFrame.getTopographicPlaces().getTopographicPlace(),
-                    topographicPlacesCreatedCounter);
-        } else {
-            logger.warn("Site frame does not contain any topographic places");
+        if (stopPlace.getQuays() != null) {
+            logger.debug("Stop place has {} quays", stopPlace.getQuays().size());
+            Set<Quay> savedQuays = stopPlace.getQuays().stream().map(quay -> {
+                logger.debug("Saving quay ");
+                return quayRepository.save(quay);
+            }).collect(Collectors.toSet());
+            stopPlace.setQuays(savedQuays);
         }
-//
-//        if (stopPlace.getQuays() != null) {
-//            logger.debug("Stop place has {} quays", stopPlace.getQuays().size());
-//            Set<Quay> savedQuays = stopPlace.getQuays().stream().map(quay -> {
-//                logger.debug("Saving quay ");
-//                return quayRepository.save(quay);
-//            }).collect(Collectors.toSet());
-//            stopPlace.setQuays(savedQuays);
-//        }
 
         stopPlaceRepository.save(stopPlace);
         logger.debug("Saving stop place {}, version {}, netex ID: {}", stopPlace.getName(), stopPlace.getVersion(), stopPlace.getNetexId());
