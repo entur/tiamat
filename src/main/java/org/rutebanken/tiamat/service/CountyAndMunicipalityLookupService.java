374fd5d2ff58bf04356bff0d3da4d6c5bd5d3054
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-04-06 19:53:51 +0200

    NRP-1346 Does not have to propagate new transaction

--- src/main/java/org/rutebanken/tiamat/service/CountyAndMunicipalityLookupService.java
+++ src/main/java/org/rutebanken/tiamat/service/CountyAndMunicipalityLookupService.java
@@ -1,29 +1,19 @@
 package org.rutebanken.tiamat.service;
 
 
-import com.google.common.util.concurrent.Striped;
-import com.vividsolutions.jts.geom.Point;
-import org.geotools.console.Option;
-import org.rutebanken.tiamat.model.*;
-import org.rutebanken.tiamat.pelias.PeliasReverseLookupClient;
-import org.rutebanken.tiamat.pelias.model.Properties;
+import org.rutebanken.tiamat.model.Site_VersionStructure;
+import org.rutebanken.tiamat.model.TopographicPlace;
 import org.rutebanken.tiamat.repository.TopographicPlaceRepository;
-import org.rutebanken.tiamat.versioning.VersionIncrementor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
-@Transactional(propagation = Propagation.NOT_SUPPORTED)
 public class CountyAndMunicipalityLookupService {
 
     private static final Logger logger = LoggerFactory.getLogger(CountyAndMunicipalityLookupService.class);
@@ -38,7 +28,7 @@ public class CountyAndMunicipalityLookupService {
         }
 
         List<TopographicPlace> topographicPlaces = topographicPlaceRepository.findByPoint(siteVersionStructure.getCentroid());
-        if(topographicPlaces == null || topographicPlaces.isEmpty()) {
+        if (topographicPlaces == null || topographicPlaces.isEmpty()) {
             logger.warn("Could not find topographic places from site's point: {}", siteVersionStructure.getCentroid());
             return;
         }
@@ -47,7 +37,7 @@ public class CountyAndMunicipalityLookupService {
                 .filter(topographicPlace -> topographicPlace.getParentTopographicPlaceRef() == null)
                 .findAny();
 
-        if(topographicPlaceWithParent.isPresent()) {
+        if (topographicPlaceWithParent.isPresent()) {
             siteVersionStructure.setTopographicPlace(topographicPlaceWithParent.get());
             logger.debug("Found topographic place {} for site {}", siteVersionStructure.getTopographicPlace(), siteVersionStructure);
         } else {
@@ -58,15 +48,11 @@ public class CountyAndMunicipalityLookupService {
                 .filter(topographicPlace -> topographicPlace.getParentTopographicPlaceRef() != null)
                 .findAny();
 
-        if(topographicPlaceWithoutParent.isPresent()) {
+        if (topographicPlaceWithoutParent.isPresent()) {
             siteVersionStructure.setTopographicPlace(topographicPlaceWithoutParent.get());
             logger.warn("Found topographic place without parent: {} for site {}", siteVersionStructure.getTopographicPlace(), siteVersionStructure);
         } else {
             logger.warn("Could not find topographic place without parent for site {}", siteVersionStructure);
         }
-
-
-
-
     }
 }
