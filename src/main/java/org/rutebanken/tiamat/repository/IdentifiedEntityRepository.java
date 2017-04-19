ca466648195d33b5bd2fd21fea9fba0cb95b524b
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-04-19 17:43:49 +0200

    NRP-1392 Add query hints to queries in IdentifiedEntityRepository

--- src/main/java/org/rutebanken/tiamat/repository/IdentifiedEntityRepository.java
+++ src/main/java/org/rutebanken/tiamat/repository/IdentifiedEntityRepository.java
@@ -2,16 +2,21 @@ package org.rutebanken.tiamat.repository;
 
 import org.rutebanken.tiamat.model.StopPlace;
 import org.rutebanken.tiamat.model.identification.IdentifiedEntity;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.jpa.repository.support.JpaEntityInformation;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 
+import javax.persistence.QueryHint;
 import java.util.List;
 
 public interface IdentifiedEntityRepository<T extends IdentifiedEntity> {
 
+    @QueryHints(value = { @QueryHint(name = "org.hibernate.cacheable", value = "true")}, forCounting = false)
     T findFirstByNetexIdOrderByVersionDesc(String netexId);
 
+    @QueryHints(value = { @QueryHint(name = "org.hibernate.cacheable", value = "true")}, forCounting = false)
     T findFirstByNetexIdAndVersion(String netexId, long version);
 
+    @QueryHints(value = { @QueryHint(name = "org.hibernate.cacheable", value = "true")}, forCounting = false)
     List<T> findByNetexId(String netexId);
 }
