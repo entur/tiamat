c42605fb205d5671606735d6419c488f8c0773c3
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-05-10 18:37:47 +0200

    NRP-1529 Import stop places in one transaction per stop place. To avoid hibernate getting slow in long running transactions

--- src/main/java/org/rutebanken/tiamat/importer/MatchingAppendingIdStopPlacesImporter.java
+++ src/main/java/org/rutebanken/tiamat/importer/MatchingAppendingIdStopPlacesImporter.java
@@ -3,9 +3,6 @@ package org.rutebanken.tiamat.importer;
 import org.rutebanken.netex.model.StopPlace;
 import org.rutebanken.tiamat.importer.finder.NearbyStopPlaceFinder;
 import org.rutebanken.tiamat.importer.finder.StopPlaceByIdFinder;
-import org.rutebanken.tiamat.netex.mapping.NetexMapper;
-import org.rutebanken.tiamat.netex.mapping.mapper.NetexIdMapper;
-import org.rutebanken.tiamat.repository.StopPlaceRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +10,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -22,33 +18,15 @@ import java.util.concurrent.atomic.AtomicInteger;
  * Matches nearby existing stop places.
  * If match, the ID and tariffzone ref is appended to the existing stop.
  */
-@Transactional
 @Component
+@Transactional
 public class MatchingAppendingIdStopPlacesImporter {
 
     private static final Logger logger = LoggerFactory.getLogger(MatchingAppendingIdStopPlacesImporter.class);
 
-    private static final boolean ALLOW_OTHER_TYPE_AS_ANY_MATCH = true;
-
-    private static final boolean CREATE_NEW_QUAYS = false;
 
     @Autowired
-    private NearbyStopPlaceFinder nearbyStopPlaceFinder;
-
-    @Autowired
-    private KeyValueListAppender keyValueListAppender;
-
-    @Autowired
-    private StopPlaceRepository stopPlaceRepository;
-
-    @Autowired
-    private QuayMerger quayMerger;
-
-    @Autowired
-    private NetexMapper netexMapper;
-
-    @Autowired
-    private StopPlaceByIdFinder stopPlaceByIdFinder;
+    private TransactionalmatchingAppendingStopPlaceImporter transactionalmatchingAppendingStopPlaceImporter;
 
     public List<StopPlace> importStopPlaces(List<org.rutebanken.tiamat.model.StopPlace> tiamatStops, AtomicInteger stopPlacesCreatedOrUpdated) {
 
@@ -56,51 +34,12 @@ public class MatchingAppendingIdStopPlacesImporter {
 
         tiamatStops.forEach(incomingStopPlace -> {
 
+            transactionalmatchingAppendingStopPlaceImporter.findAppendAndAdd(incomingStopPlace, matchedStopPlaces, stopPlacesCreatedOrUpdated);
 
-            Optional<org.rutebanken.tiamat.model.StopPlace> foundStopPlace = stopPlaceByIdFinder.findStopPlace(incomingStopPlace);
-
-            if(!foundStopPlace.isPresent()) {
-                foundStopPlace = Optional.ofNullable(nearbyStopPlaceFinder.find(incomingStopPlace, ALLOW_OTHER_TYPE_AS_ANY_MATCH));
-            }
-
-            if(!foundStopPlace.isPresent()) {
-                logger.warn("Cannot find stop place: {}", incomingStopPlace);
-            } else {
-
-                org.rutebanken.tiamat.model.StopPlace existingstopPlace = foundStopPlace.get();
-
-                logger.debug("Found matching stop place {}", existingstopPlace);
-                keyValueListAppender.appendToOriginalId(NetexIdMapper.ORIGINAL_ID_KEY, incomingStopPlace, existingstopPlace);
-
-                if(incomingStopPlace.getTariffZones() != null) {
-                    if (existingstopPlace.getTariffZones() == null) {
-                        existingstopPlace.setTariffZones(new HashSet<>());
-                    }
-                    existingstopPlace.getTariffZones().addAll(incomingStopPlace.getTariffZones());
-                }
-
-                quayMerger.appendImportIds(incomingStopPlace, existingstopPlace, CREATE_NEW_QUAYS);
-
-                incomingStopPlace = stopPlaceRepository.save(existingstopPlace);
-                String netexId = incomingStopPlace.getNetexId();
-
-                boolean alreadyAdded = matchedStopPlaces
-                        .stream()
-                        .filter(alreadyAddedStop -> alreadyAddedStop.getId().equals(netexId))
-                        .findAny().isPresent();
-
-                if(!alreadyAdded) {
-                    matchedStopPlaces.add(netexMapper.mapToNetexModel(existingstopPlace));
-                }
-
-                stopPlacesCreatedOrUpdated.incrementAndGet();
-
-                if(stopPlacesCreatedOrUpdated.get() % 100 == 0) {
-                    stopPlaceRepository.flush();
-                }
-            }
         });
 
         return matchedStopPlaces;
     }
+
+
 }
