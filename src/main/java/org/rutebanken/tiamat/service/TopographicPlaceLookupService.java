eea65f7a250c3f9a528205e86c9bebabd73735f4
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-05-15 10:40:40 +0200

    NRP-1524 Change to use our own memoizer, not guava's. To be able to reset it.

--- src/main/java/org/rutebanken/tiamat/service/TopographicPlaceLookupService.java
+++ src/main/java/org/rutebanken/tiamat/service/TopographicPlaceLookupService.java
@@ -1,8 +1,5 @@
 package org.rutebanken.tiamat.service;
 
-
-import com.google.common.base.Supplier;
-import com.google.common.base.Suppliers;
 import com.vividsolutions.jts.geom.Point;
 import com.vividsolutions.jts.geom.Polygon;
 import org.apache.commons.lang3.tuple.ImmutableTriple;
@@ -22,6 +19,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
 
 import static java.util.stream.Collectors.toList;
 
@@ -33,10 +31,7 @@ public class TopographicPlaceLookupService {
 
     private static final List<TopographicPlaceTypeEnumeration> ADMIN_LEVEL_ORDER = Arrays.asList(TopographicPlaceTypeEnumeration.TOWN, TopographicPlaceTypeEnumeration.COUNTY, TopographicPlaceTypeEnumeration.STATE);
 
-
-    private final Supplier<List<ImmutableTriple<String, TopographicPlaceTypeEnumeration, Polygon>>> topographicPlaces = Suppliers.memoizeWithExpiration(getTopographicPlaceSupplier(), 10, TimeUnit.HOURS);
-
-//    private final ResettableMemoizer<List<ImmutableTriple<String, TopographicPlaceTypeEnumeration, Polygon>>> memoizedTopographicPlaces = new ResettableMemoizer<>(getTopographicPlaceSupplier2());
+    private final ResettableMemoizer<List<ImmutableTriple<String, TopographicPlaceTypeEnumeration, Polygon>>> memoizedTopographicPlaces = new ResettableMemoizer<>(getTopographicPlaceSupplier());
 
     @Autowired
     private TopographicPlaceRepository topographicPlaceRepository;
@@ -60,7 +55,7 @@ public class TopographicPlaceLookupService {
     }
 
     public Optional<TopographicPlace> findTopographicPlace(Point point) {
-        return topographicPlaces.get()
+        return memoizedTopographicPlaces.get()
                        .stream()
                        .filter(triple -> point.within(triple.getRight()))
                        .map(triple -> topographicPlaceRepository.findFirstByNetexIdOrderByVersionDesc(triple.getLeft()))
@@ -69,7 +64,7 @@ public class TopographicPlaceLookupService {
     }
 
     public Optional<TopographicPlace> findTopographicPlaceByReference(List<String> topographicPlaceReferences, Point point) {
-        return topographicPlaces.get()
+        return memoizedTopographicPlaces.get()
                 .stream()
                 .filter(triple -> topographicPlaceReferences.contains(triple.getLeft()))
                 .filter(triple -> point.within(triple.getRight()))
@@ -78,18 +73,9 @@ public class TopographicPlaceLookupService {
                 .findAny();
     }
 
-//    private java.util.function.Supplier<List<ImmutableTriple<String, TopographicPlaceTypeEnumeration, Polygon>>> getTopographicPlaceSupplier2() {
-//        return () -> {
-//            logger.info("Fetching topographic places from repository");
-//            return topographicPlaceRepository.findAllMaxVersion()
-//                    .stream()
-//                    .filter(topographicPlace -> topographicPlace.getPolygon() != null)
-//                    .filter(topographicPlace -> ADMIN_LEVEL_ORDER.contains(topographicPlace.getTopographicPlaceType()))
-//                    .sorted(new TopographicPlaceByAdminLevelComparator())
-//                    .map(topographicPlace -> ImmutableTriple.of(topographicPlace.getNetexId(), topographicPlace.getTopographicPlaceType(), topographicPlace.getPolygon()))
-//                    .collect(toList());
-//        };
-//    }
+    public void reset() {
+        memoizedTopographicPlaces.reset();
+    }
 
     private Supplier<List<ImmutableTriple<String, TopographicPlaceTypeEnumeration, Polygon>>> getTopographicPlaceSupplier() {
         return () -> {
