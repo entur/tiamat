9d7c48969f992b4d58bac656c69f86286e9e730a
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-05-19 12:43:47 +0200

    NRP-1554 Handle topographic place reference when restoring stop place

--- src/main/java/org/rutebanken/tiamat/importer/restore/RestoringStopPlaceImporter.java
+++ src/main/java/org/rutebanken/tiamat/importer/restore/RestoringStopPlaceImporter.java
@@ -4,6 +4,7 @@ package org.rutebanken.tiamat.importer.restore;
 import org.rutebanken.tiamat.model.StopPlace;
 import org.rutebanken.tiamat.netex.mapping.NetexMapper;
 import org.rutebanken.tiamat.repository.StopPlaceRepository;
+import org.rutebanken.tiamat.repository.TopographicPlaceRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,15 +24,28 @@ public class RestoringStopPlaceImporter {
 
     private final StopPlaceRepository stopPlaceRepository;
 
+    private final TopographicPlaceRepository topographicPlaceRepository;
+
     private final NetexMapper netexMapper;
 
     @Autowired
-    public RestoringStopPlaceImporter(StopPlaceRepository stopPlaceRepository, NetexMapper netexMapper) {
+    public RestoringStopPlaceImporter(StopPlaceRepository stopPlaceRepository, TopographicPlaceRepository topographicPlaceRepository, NetexMapper netexMapper) {
         this.stopPlaceRepository = stopPlaceRepository;
+        this.topographicPlaceRepository = topographicPlaceRepository;
         this.netexMapper = netexMapper;
     }
 
     public org.rutebanken.netex.model.StopPlace importStopPlace(AtomicInteger stopPlacesImported, StopPlace stopPlace) {
+
+        if(stopPlace.getTopographicPlace() != null) {
+
+            String netexId = stopPlace.getTopographicPlace().getNetexId();
+            Long version = stopPlace.getTopographicPlace().getVersion();
+
+            stopPlace.setTopographicPlace(topographicPlaceRepository.findFirstByNetexIdAndVersion(netexId, version));
+            logger.trace("Resolved topographic place from {}:{} for stop place {}:{}", netexId, version, stopPlace.getNetexId(), stopPlace.getVersion());
+        }
+
         stopPlaceRepository.save(stopPlace);
         logger.debug("Saving stop place {}, version {}, netex ID: {}", stopPlace.getName(), stopPlace.getVersion(), stopPlace.getNetexId());
         stopPlacesImported.incrementAndGet();
