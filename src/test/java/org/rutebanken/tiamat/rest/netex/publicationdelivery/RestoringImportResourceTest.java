6142476b0f827bec9045bd87861e0d6cd4999d33
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-05-19 12:44:04 +0200

    NRP-1554 Do more checks in restoring import test

--- src/test/java/org/rutebanken/tiamat/rest/netex/publicationdelivery/RestoringImportResourceTest.java
+++ src/test/java/org/rutebanken/tiamat/rest/netex/publicationdelivery/RestoringImportResourceTest.java
@@ -4,11 +4,14 @@ import org.junit.Test;
 import org.rutebanken.netex.model.PublicationDeliveryStructure;
 import org.rutebanken.netex.model.TopographicPlace;
 import org.rutebanken.tiamat.TiamatIntegrationTest;
+import org.rutebanken.tiamat.model.StopPlace;
+import org.rutebanken.tiamat.netex.id.NetexIdHelper;
 import org.rutebanken.tiamat.repository.ParkingRepository;
 import org.rutebanken.tiamat.repository.StopPlaceRepository;
 import org.rutebanken.tiamat.repository.TopographicPlaceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.transaction.annotation.Transactional;
 import org.xml.sax.SAXException;
 
 import javax.ws.rs.core.Response;
@@ -23,6 +26,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Transactional
 public class RestoringImportResourceTest extends TiamatIntegrationTest {
 
     @Autowired
@@ -46,10 +50,24 @@ public class RestoringImportResourceTest extends TiamatIntegrationTest {
         assertThat(topographicPlaceRepository.findAll()).isEmpty();
 
         restoringImportResource.importPublicationDeliveryOnEmptyDatabase(new FileInputStream(file));
-        assertThat(topographicPlaceRepository.findAllMaxVersion()).isNotEmpty();
+        List<org.rutebanken.tiamat.model.TopographicPlace> allMaxVersion = topographicPlaceRepository.findAllMaxVersion();
+        assertThat(allMaxVersion).isNotEmpty();
+        assertThat(allMaxVersion).hasSize(2);
+        assertThat(allMaxVersion)
+                .extracting(org.rutebanken.tiamat.model.TopographicPlace::getNetexId)
+                .contains("KVE:TopographicPlace:20", "KVE:TopographicPlace:2002");
 
-        assertThat(stopPlaceRepository.findAll()).isNotEmpty();
-        assertThat(parkingRepository.findAll()).isNotEmpty();
+        List<StopPlace> importedStops = stopPlaceRepository.findAll();
+        assertThat(importedStops).as("imported stops in repository").isNotEmpty();
+
+
+        importedStops.forEach(stopPlace -> {
+            org.rutebanken.tiamat.model.TopographicPlace topographicPlace = stopPlace.getTopographicPlace();
+            assertThat(topographicPlace).as("stop place's topographic place "+stopPlace.getNetexId()).isNotNull();
+            assertThat(NetexIdHelper.isNetexId(topographicPlace.getNetexId())).as("Topographic place has valid netexID").isTrue();
+        });
+
+        assertThat(parkingRepository.findAll()).as("imported parkings in repository").isNotEmpty();
     }
 
 }
