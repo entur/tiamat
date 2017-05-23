05bb12b169982c8a7b4129475fbf256626dae968
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-05-23 09:09:18 +0200

    NRP-1568 Add constructor and equals method for PrivateCodeStructure

--- src/main/java/org/rutebanken/tiamat/model/PrivateCodeStructure.java
+++ src/main/java/org/rutebanken/tiamat/model/PrivateCodeStructure.java
@@ -1,13 +1,24 @@
 package org.rutebanken.tiamat.model;
 
+import org.rutebanken.tiamat.netex.mapping.mapper.NetexIdMapper;
+
 import javax.persistence.Embeddable;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Embeddable
 public class PrivateCodeStructure implements Serializable {
 
     protected String value;
     protected String type;
+
+    public PrivateCodeStructure() {
+    }
+
+    public PrivateCodeStructure(String value, String type) {
+        this.value = value;
+        this.type = type;
+    }
 
     public String getValue() {
         return value;
@@ -25,4 +36,17 @@ public class PrivateCodeStructure implements Serializable {
         this.type = value;
     }
 
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        } else if (!(object instanceof PrivateCodeStructure)) {
+            return false;
+        }
+
+        PrivateCodeStructure other = (PrivateCodeStructure) object;
+
+        return Objects.equals(this.value, other.value)
+                && Objects.equals(this.type, other.type);
+    }
 }
