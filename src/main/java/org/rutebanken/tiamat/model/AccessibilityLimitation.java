41ab2c2dabbdc715bab9e6034e80579c7f1cdb4d
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-05-29 16:41:27 +0200

    NRP-1595 Add toString for AccessibilityLimitation

--- src/main/java/org/rutebanken/tiamat/model/AccessibilityLimitation.java
+++ src/main/java/org/rutebanken/tiamat/model/AccessibilityLimitation.java
@@ -1,11 +1,30 @@
 package org.rutebanken.tiamat.model;
 
+import com.google.common.base.MoreObjects;
+
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 
 
 @Entity
 public class AccessibilityLimitation
         extends AccessibilityLimitation_VersionedChildStructure {
 
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .omitNullValues()
+                .add("netexId", netexId)
+                .add("version", version)
+                .add("wheelchairAccess", wheelchairAccess)
+                .add("stepFreeAccess", stepFreeAccess)
+                .add("escalatorFreeAccess", escalatorFreeAccess)
+                .add("liftFreeAccess", liftFreeAccess)
+                .add("audibleSignalsAvailable", audibleSignalsAvailable)
+                .add("visualSignsAvailable", visualSignsAvailable)
+                .toString();
+    }
 
 }
