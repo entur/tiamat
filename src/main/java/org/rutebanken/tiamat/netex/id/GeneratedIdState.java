62c5695b1950a9928cb1a14a31a667c59b315370
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-06-01 13:15:39 +0200

    NRP-1633 Keep entities registered for generated IDs in a hazelcast Set.

--- src/main/java/org/rutebanken/tiamat/netex/id/GeneratedIdState.java
+++ src/main/java/org/rutebanken/tiamat/netex/id/GeneratedIdState.java
@@ -1,13 +1,13 @@
 package org.rutebanken.tiamat.netex.id;
 
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.IMap;
 import com.hazelcast.core.IQueue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.*;
 
 import static org.rutebanken.tiamat.netex.id.GaplessIdGeneratorService.INITIAL_LAST_ID;
@@ -17,6 +17,7 @@ public class GeneratedIdState implements Serializable{
 
     public static final String LAST_IDS_FOR_ENTITY = "lastIdsForEntities";
     public static final String CLAIMED_IDS_FOR_ENTITY_PREFIX = "claimedIdsForEntities";
+    public static final String ENTITY_NAMES_REGISTERED = "entityNamesRegistered";
 
     private final HazelcastInstance hazelcastInstance;
 
@@ -27,11 +28,16 @@ public class GeneratedIdState implements Serializable{
 
 
     public IQueue<Long> getQueueForEntity(String entityTypeName) {
+        hazelcastInstance.getSet(ENTITY_NAMES_REGISTERED).add(entityTypeName);
         return hazelcastInstance.getQueue(entityTypeName);
     }
 
     public void setLastIdForEntity(String entityTypeName, long lastId) {
         hazelcastInstance.getMap(LAST_IDS_FOR_ENTITY).put(entityTypeName, lastId);
+    }
+
+    public Set<String> getRegisteredEntityNames() {
+        return hazelcastInstance.getSet(ENTITY_NAMES_REGISTERED);
     }
 
     /**
