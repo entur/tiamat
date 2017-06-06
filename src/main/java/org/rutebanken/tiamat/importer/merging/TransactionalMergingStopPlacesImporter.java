523c6f1163db6f0a5d4329e1ee4cf1c0363d6379
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-06-06 14:03:31 +0200

    NRP-1586 Move to merging package

--- src/main/java/org/rutebanken/tiamat/importer/merging/TransactionalMergingStopPlacesImporter.java
+++ src/main/java/org/rutebanken/tiamat/importer/merging/TransactionalMergingStopPlacesImporter.java
@@ -1,5 +1,7 @@
-package org.rutebanken.tiamat.importer;
+package org.rutebanken.tiamat.importer.merging;
 
+import org.rutebanken.tiamat.importer.MergingStopPlaceImporter;
+import org.rutebanken.tiamat.importer.StopPlaceTopographicPlaceReferenceUpdater;
 import org.rutebanken.tiamat.model.StopPlace;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
