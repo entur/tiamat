5facd06ebd5bfa6cbf142ee02e9a5d5ebf27d5b5
Lasse Tyrihjell <lassetyr@gmail.com>
2017-06-23 10:12:45 +0200

    Merge-operations are now union-merge - Added optional dryrun-parameter

---
+++ src/main/java/org/rutebanken/tiamat/rest/graphql/helpers/ObjectMerger.java
@@ -0,0 +1,92 @@
+package org.rutebanken.tiamat.rest.graphql.helpers;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.FatalBeanException;
+import org.springframework.util.Assert;
+import org.springframework.util.ClassUtils;
+
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+/*
+  * "Borrowed" from https://stackoverflow.com/questions/41602260/how-to-merge-two-java-objects-into-one
+ */
+public class ObjectMerger extends BeanUtils {
+
+
+    private static void setAccessible(Method method) {
+        if (!Modifier.isPublic(method.getDeclaringClass().getModifiers())) {
+            method.setAccessible(true);
+        }
+    }
+
+    /**
+     * Copy the not null property values of the given source bean into the given target bean.
+     * <p>Note: The source and target classes do not have to match or even be derived
+     * from each other, as long as the properties match. Any bean properties that the
+     * source bean exposes but the target bean does not will silently be ignored.
+     * @param source the source bean
+     * @param target the target bean
+     * @param ignoreProperties array of property names to ignore
+     * @throws BeansException if the copying failed
+     * @see BeanWrapper
+     */
+    public static void copyPropertiesNotNull(Object source, Object target, String... ignoreProperties)
+            throws BeansException {
+
+        Assert.notNull(source, "Source must not be null");
+        Assert.notNull(target, "Target must not be null");
+
+        Class<?> actualEditable = target.getClass();
+        PropertyDescriptor[] targetPds = getPropertyDescriptors(actualEditable);
+        List<String> ignoreList = (ignoreProperties != null ? Arrays.asList(ignoreProperties) : null);
+
+        for (PropertyDescriptor targetPropertyDescriptor : targetPds) {
+            Method targetWriteMethod = targetPropertyDescriptor.getWriteMethod();
+            if (targetWriteMethod != null
+                    && (ignoreList == null || !ignoreList.contains(targetPropertyDescriptor.getName()))) {
+                PropertyDescriptor sourcePropertyDescriptor =
+                        getPropertyDescriptor(source.getClass(), targetPropertyDescriptor.getName());
+                if (sourcePropertyDescriptor != null) {
+                    Method sourceReadMethod = sourcePropertyDescriptor.getReadMethod();
+                    if (sourceReadMethod != null &&
+                            ClassUtils.isAssignable(
+                                    targetWriteMethod.getParameterTypes()[0], sourceReadMethod.getReturnType())) {
+                        try {
+                            Method targetReadMethod = targetPropertyDescriptor.getReadMethod();
+                            setAccessible(sourceReadMethod);
+                            setAccessible(targetWriteMethod);
+                            Object sourceValue = sourceReadMethod.invoke(source);
+
+                            if (sourceValue != null && targetReadMethod != null) {
+                                setAccessible(targetReadMethod);
+                                Object targetValue = targetReadMethod.invoke(target);
+                                if (targetValue == null) {
+                                    targetWriteMethod.invoke(target, sourceValue);
+                                } else if(targetValue instanceof Collection<?>) {
+                                    ((Collection) targetValue).addAll((Collection) sourceValue);
+                                } else if (targetValue instanceof Map<?,?>) {
+                                    ((Map) targetValue).putAll((Map) sourceValue);
+                                } else {
+                                    copyPropertiesNotNull(sourceValue, targetValue, ignoreProperties);
+                                }
+                            }
+                        }
+                        catch (Throwable ex) {
+                            throw new FatalBeanException(
+                                    "Could not copy property '" + targetPropertyDescriptor.getName() +
+                                    "' from source to target", ex);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- src/test/java/org/rutebanken/tiamat/rest/graphql/StopPlaceQuayMergerTest.java
+++ src/test/java/org/rutebanken/tiamat/rest/graphql/StopPlaceQuayMergerTest.java
@@ -3,12 +3,13 @@ package org.rutebanken.tiamat.rest.graphql;
 import com.vividsolutions.jts.geom.Coordinate;
 import org.junit.Test;
 import org.rutebanken.tiamat.model.*;
+import org.rutebanken.tiamat.netex.mapping.mapper.NetexIdMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.transaction.Transactional;
+import java.math.BigInteger;
 import java.time.Instant;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
@@ -29,6 +30,18 @@ public class StopPlaceQuayMergerTest extends AbstractGraphQLResourceIntegrationT
         fromStopPlace.getOriginalIds().add("TEST:StopPlace:1234");
         fromStopPlace.getOriginalIds().add("TEST:StopPlace:5678");
 
+
+        PlaceEquipment fromPlaceEquipment = new PlaceEquipment();
+        GeneralSign generalSign = new GeneralSign();
+        generalSign.setSignContentType(SignContentEnumeration.TRANSPORT_MODE);
+        generalSign.setPublicCode(new PrivateCodeStructure("111", "111111"));
+        fromPlaceEquipment.getInstalledEquipment().add(generalSign);
+        fromStopPlace.setPlaceEquipments(fromPlaceEquipment);
+
+        String testKey = "testKey";
+        String testValue = "testValue";
+        fromStopPlace.getKeyValues().put(testKey, new Value(testValue));
+
         Quay fromQuay = new Quay();
         fromQuay.setCompassBearing(new Float(90));
         fromQuay.setCentroid(geometryFactory.createPoint(new Coordinate(11.2, 60.2)));
@@ -36,6 +49,11 @@ public class StopPlaceQuayMergerTest extends AbstractGraphQLResourceIntegrationT
         fromQuay.getOriginalIds().add("TEST:Quay:567801");
 
         fromStopPlace.getQuays().add(fromQuay);
+
+        String oldVersionComment = "Old version deleted";
+        fromStopPlace.setVersionComment(oldVersionComment);
+
+        fromStopPlace.setTransportMode(VehicleModeEnumeration.BUS);
 
         stopPlaceVersionedSaverService.saveNewVersion(fromStopPlace);
 
@@ -55,17 +73,33 @@ public class StopPlaceQuayMergerTest extends AbstractGraphQLResourceIntegrationT
 
         stopPlaceVersionedSaverService.saveNewVersion(toStopPlace);
 
-        StopPlace mergedStopPlace = stopPlaceQuayMerger.mergeStopPlaces(fromStopPlace.getNetexId(), toStopPlace.getNetexId(), null, null);
+        StopPlace mergedStopPlace = stopPlaceQuayMerger.mergeStopPlaces(fromStopPlace.getNetexId(), toStopPlace.getNetexId(), null, null, false);
 
         assertThat(mergedStopPlace).isNotNull();
 
-        assertThat(toStopPlace.getOriginalIds()).isNotEmpty();
         assertThat(fromStopPlace.getOriginalIds()).isNotEmpty();
+        assertThat(toStopPlace.getOriginalIds()).isNotEmpty();
 
         assertThat(mergedStopPlace.getOriginalIds()).hasSize(fromStopPlace.getOriginalIds().size() + toStopPlace.getOriginalIds().size());
+
         assertThat(mergedStopPlace.getOriginalIds().containsAll(fromStopPlace.getOriginalIds()));
 
+        assertThat(mergedStopPlace.getKeyValues().get(testKey)).isNotNull();
+        assertThat(mergedStopPlace.getKeyValues().get(testKey).getItems()).hasSize(1);
+        assertThat(mergedStopPlace.getKeyValues().get(testKey).getItems()).contains(testValue);
+
         assertThat(mergedStopPlace.getName().getValue()).matches(toStopPlace.getName().getValue());
+
+        assertThat(mergedStopPlace.getPlaceEquipments()).isNotNull();
+        assertThat(mergedStopPlace.getVersionComment()).isNull();
+        assertThat(mergedStopPlace.getTransportMode()).isEqualTo(VehicleModeEnumeration.BUS);
+
+        PlaceEquipment placeEquipment = mergedStopPlace.getPlaceEquipments();
+        List<InstalledEquipment_VersionStructure> equipment = placeEquipment.getInstalledEquipment();
+        assertThat(equipment).hasSize(1);
+        assertThat(equipment).doesNotContain(generalSign); // Result from merge does not contain same object
+        assertThat(equipment.get(0)).isInstanceOf(GeneralSign.class);
+        assertThat(((GeneralSign)equipment.get(0)).getSignContentType()).isEqualTo(SignContentEnumeration.TRANSPORT_MODE);
 
         // assertQuays
         assertThat(mergedStopPlace.getQuays()).hasSize(2);
@@ -85,6 +119,61 @@ public class StopPlaceQuayMergerTest extends AbstractGraphQLResourceIntegrationT
                 fail("Unknown Quay has been added");
             }
         });
+    }
+
+    @Test
+    public void testMergeKeyValues() {
+        String fromOriginalId = "1234";
+        String toOriginalId = "4321";
+        String testKey = "test-key";
+        String otherTestKey = "other-test-key";
+        String testValue = "test";
+
+        Map<String, Value> fromKeyValues = new HashMap<>();
+
+        fromKeyValues.put(NetexIdMapper.ORIGINAL_ID_KEY, new Value(fromOriginalId));
+        fromKeyValues.put(testKey, new Value(testValue));
+
+        Map<String, Value> toKeyValues = new HashMap<>();
+        toKeyValues.put(NetexIdMapper.ORIGINAL_ID_KEY, new Value(fromOriginalId, toOriginalId));
+        toKeyValues.put(otherTestKey, new Value(testValue));
+
+        stopPlaceQuayMerger.mergeKeyValues(fromKeyValues, toKeyValues);
+
+        assertThat(toKeyValues).hasSize(3);
+        assertThat(toKeyValues.get(NetexIdMapper.ORIGINAL_ID_KEY)).isNotNull();
+        assertThat(toKeyValues.get(NetexIdMapper.ORIGINAL_ID_KEY).getItems()).hasSize(2);
+        assertThat(toKeyValues.get(NetexIdMapper.ORIGINAL_ID_KEY).getItems()).contains(fromOriginalId, toOriginalId);
+        assertThat(toKeyValues.get(testKey).getItems()).contains(testValue);
+        assertThat(toKeyValues.get(otherTestKey).getItems()).contains(testValue);
+
+    }
+
+    @Test
+    public void testMergePlaceEquipment() {
+
+
+        PlaceEquipment fromPlaceEquipment = new PlaceEquipment();
+        GeneralSign generalSign = new GeneralSign();
+        generalSign.setSignContentType(SignContentEnumeration.TRANSPORT_MODE);
+        generalSign.setPublicCode(new PrivateCodeStructure("111", "111111"));
+        fromPlaceEquipment.getInstalledEquipment().add(generalSign);
+
+
+        PlaceEquipment toPlaceEquipment = new PlaceEquipment();
+        GeneralSign generalSign2 = new GeneralSign();
+        generalSign2.setSignContentType(SignContentEnumeration.TRANSPORT_MODE);
+        generalSign2.setPublicCode(new PrivateCodeStructure("222", "222222"));
+        toPlaceEquipment.getInstalledEquipment().add(generalSign2);
+
+
+        stopPlaceQuayMerger.mergePlaceEquipments(fromPlaceEquipment, toPlaceEquipment);
+
+        List<InstalledEquipment_VersionStructure> equipment = toPlaceEquipment.getInstalledEquipment();
+        assertThat(equipment).hasSize(2);
+        assertThat(equipment).contains(generalSign2);
+        assertThat(equipment).doesNotContain(generalSign);
+
     }
 
     @Test
@@ -188,9 +277,18 @@ public class StopPlaceQuayMergerTest extends AbstractGraphQLResourceIntegrationT
         fromQuay.getOriginalIds().add("TEST:Quay:123401");
         fromQuay.getOriginalIds().add("TEST:Quay:567801");
 
+        String testKey = "testKey";
+        String testValue = "testValue";
+        fromQuay.getKeyValues().put(testKey, new Value(testValue));
+
+        PlaceEquipment equipment = new PlaceEquipment();
+        ShelterEquipment shelter = new ShelterEquipment();
+        shelter.setSeats(BigInteger.ONE);
+        shelter.setAirConditioned(false);
+        equipment.getInstalledEquipment().add(shelter);
+        fromQuay.setPlaceEquipments(equipment);
 
         Quay toQuay = new Quay();
-        toQuay.setCompassBearing(new Float(90));
         toQuay.setCentroid(geometryFactory.createPoint(new Coordinate(11.21, 60.21)));
         toQuay.getOriginalIds().add("TEST:Quay:432101");
         toQuay.getOriginalIds().add("TEST:Quay:876501");
@@ -206,17 +304,27 @@ public class StopPlaceQuayMergerTest extends AbstractGraphQLResourceIntegrationT
 
         stopPlaceVersionedSaverService.saveNewVersion(fromStopPlace);
 
-        StopPlace mergedStopPlace = stopPlaceQuayMerger.mergeQuays(fromStopPlace.getNetexId(), fromQuay.getNetexId(), toQuay.getNetexId(), null);
+        StopPlace stopPlaceWithMergedQuays = stopPlaceQuayMerger.mergeQuays(fromStopPlace.getNetexId(), fromQuay.getNetexId(), toQuay.getNetexId(), null, false);
 
-        assertThat(mergedStopPlace).isNotNull();
-        assertThat(mergedStopPlace.getOriginalIds()).isNotEmpty();
+        assertThat(stopPlaceWithMergedQuays).isNotNull();
+        assertThat(stopPlaceWithMergedQuays.getOriginalIds()).isNotEmpty();
 
         // assertQuays
-        assertThat(mergedStopPlace.getQuays()).hasSize(2);
-        mergedStopPlace.getQuays().forEach(quay -> {
+        assertThat(stopPlaceWithMergedQuays.getQuays()).hasSize(2);
+        stopPlaceWithMergedQuays.getQuays().forEach(quay -> {
             if (quay.getNetexId().equals(toQuay.getNetexId())){
 
                 assertThat(quay.getVersion()).isEqualTo(1 + toQuay.getVersion());
+
+                assertThat(quay.getCompassBearing()).isEqualTo(fromQuay.getCompassBearing());
+                assertThat(quay.getKeyValues().get(testKey)).isNotNull();
+                assertThat(quay.getKeyValues().get(testKey).getItems()).contains(testValue);
+
+                assertThat(quay.getPlaceEquipments()).isNotNull();
+                assertThat(quay.getPlaceEquipments().getInstalledEquipment()).isNotNull();
+                assertThat(quay.getPlaceEquipments().getInstalledEquipment()).hasSize(1);
+                assertThat(quay.getPlaceEquipments().getInstalledEquipment().get(0)).isInstanceOf(ShelterEquipment.class);
+
                 assertThat(!quay.equals(toQuay));
                 assertThat(!quay.equals(fromQuay));
 
