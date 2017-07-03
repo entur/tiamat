4c4f14ecb21f1844aac4d9f33624806a214fb7d4
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-07-03 16:15:34 +0200

    NRP-1833 Build role assignments for test with entity type classification

--- src/test/java/org/rutebanken/tiamat/auth/RoleAssignmentListBuilder.java
+++ src/test/java/org/rutebanken/tiamat/auth/RoleAssignmentListBuilder.java
@@ -2,7 +2,9 @@ package org.rutebanken.tiamat.auth;
 
 import org.rutebanken.helper.organisation.AuthorizationConstants;
 import org.rutebanken.helper.organisation.RoleAssignment;
+import org.rutebanken.tiamat.model.Parking;
 import org.rutebanken.tiamat.model.ParkingTypeEnumeration;
+import org.rutebanken.tiamat.model.StopPlace;
 import org.rutebanken.tiamat.model.StopTypeEnumeration;
 
 import java.util.ArrayList;
@@ -10,47 +12,51 @@ import java.util.List;
 
 public class RoleAssignmentListBuilder {
 
-	private List<RoleAssignment> roleAssignments = new ArrayList<>();
+    private List<RoleAssignment> roleAssignments = new ArrayList<>();
 
-	public static RoleAssignmentListBuilder builder() {
-		return new RoleAssignmentListBuilder();
-	}
+    public static RoleAssignmentListBuilder builder() {
+        return new RoleAssignmentListBuilder();
+    }
 
-	public List<RoleAssignment> build() {
-		return roleAssignments;
-	}
+    public List<RoleAssignment> build() {
+        return roleAssignments;
+    }
 
-	public RoleAssignmentListBuilder withAccessAllAreas() {
-		return withStopPlaceOfType(AuthorizationConstants.ENTITY_CLASSIFIER_ALL_TYPES)
-				.withParkingOfType(AuthorizationConstants.ENTITY_CLASSIFIER_ALL_TYPES);
-	}
+    public RoleAssignmentListBuilder withAccessAllAreas() {
+        return withStopPlaceOfType(AuthorizationConstants.ENTITY_CLASSIFIER_ALL_TYPES)
+                .withParkingOfType(AuthorizationConstants.ENTITY_CLASSIFIER_ALL_TYPES);
+    }
 
 
-	public RoleAssignmentListBuilder withStopPlaceOfType(StopTypeEnumeration type) {
-		return withStopPlaceOfType(type.value());
-	}
+    public RoleAssignmentListBuilder withStopPlaceOfType(StopTypeEnumeration type) {
+        return withStopPlaceOfType(type.value());
+    }
 
-	public RoleAssignmentListBuilder withStopPlaceOfType(String type) {
-		RoleAssignment allStopPlaceAccess = RoleAssignment.builder().withRole(AuthorizationConstants.ROLE_EDIT_STOPS)
-				                                    .withOrganisation("NOT_YET_CHECKED")
-				                                    .withEntityClassification("StopPlace", type).build();
+    public RoleAssignmentListBuilder withStopPlaceOfType(String type) {
+        RoleAssignment allStopPlaceAccess = RoleAssignment.builder().withRole(AuthorizationConstants.ROLE_EDIT_STOPS)
+                .withOrganisation("NOT_YET_CHECKED")
+                .withEntityClassification(AuthorizationConstants.ENTITY_TYPE, StopPlace.class.getSimpleName())
+                .withEntityClassification("StopPlaceType", type)
+                .build();
 
-		roleAssignments.add(allStopPlaceAccess);
-		return this;
-	}
+        roleAssignments.add(allStopPlaceAccess);
+        return this;
+    }
 
-	public RoleAssignmentListBuilder withParkingOfType(ParkingTypeEnumeration type) {
-		return withParkingOfType(type.value());
-	}
+    public RoleAssignmentListBuilder withParkingOfType(ParkingTypeEnumeration type) {
+        return withParkingOfType(type.value());
+    }
 
-	public RoleAssignmentListBuilder withParkingOfType(String type) {
-		RoleAssignment allParkings = RoleAssignment.builder().withRole(AuthorizationConstants.ROLE_EDIT_STOPS)
-				                                    .withOrganisation("NOT_YET_CHECKED")
-				                                    .withEntityClassification("Parking", type).build();
+    public RoleAssignmentListBuilder withParkingOfType(String type) {
+        RoleAssignment allParkings = RoleAssignment.builder().withRole(AuthorizationConstants.ROLE_EDIT_STOPS)
+                .withOrganisation("NOT_YET_CHECKED")
+                .withEntityClassification(AuthorizationConstants.ENTITY_TYPE, Parking.class.getSimpleName())
+                .withEntityClassification("parkingType", type)
+                .build();
 
-		roleAssignments.add(allParkings);
-		return this;
-	}
+        roleAssignments.add(allParkings);
+        return this;
+    }
 
 
 }
