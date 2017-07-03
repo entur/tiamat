a6b80b5b986311b0e7f9f5e7874fedbb4d5cff6c
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-07-03 14:34:49 +0200

    NRP-1833 Test authorized to edit stop place from tiamat perspective

--- src/test/java/org/rutebanken/tiamat/auth/TiamatAuthorizationServiceTest.java
+++ src/test/java/org/rutebanken/tiamat/auth/TiamatAuthorizationServiceTest.java
@@ -1,20 +1,42 @@
 package org.rutebanken.tiamat.auth;
 
 import org.junit.Test;
+import org.rutebanken.helper.organisation.RoleAssignment;
 import org.rutebanken.tiamat.TiamatIntegrationTest;
+import org.rutebanken.tiamat.model.StopPlace;
+import org.rutebanken.tiamat.model.StopTypeEnumeration;
+import org.rutebanken.tiamat.model.TopographicPlace;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
+import static org.rutebanken.helper.organisation.AuthorizationConstants.ENTITY_TYPE;
 
 public class TiamatAuthorizationServiceTest extends TiamatIntegrationTest {
 
+    @Autowired
+    private TiamatAuthorizationService tiamatAuthorizationService;
 
 
     @Test
-    public void StopPlaceAuthorizedWhenInPolygon() {
+    public void authorizedForLegalStopPlaceTypesWhenOthersBlacklisted() {
 
 
+        RoleAssignment roleAssignment = RoleAssignment.builder()
+                .withRole("editStops")
+//                .withAdministrativeZone("01")
+                .withOrganisation("OST")
+                .withEntityClassification(ENTITY_TYPE, "StopPlace")
+                .withEntityClassification("StopPlaceType", "!airport")
+                .withEntityClassification("StopPlaceType", "!railStation")
+                .build();
 
+        StopPlace stopPlace = new StopPlace();
+        stopPlace.setStopPlaceType(StopTypeEnumeration.ONSTREET_BUS);
 
+        boolean authorized = tiamatAuthorizationService.authorized(roleAssignment, stopPlace, roleAssignment.r);
+        assertThat(authorized, is(true));
     }
 
 }
