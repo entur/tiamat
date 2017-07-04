5b950ebf2c0e6a51afd74f8a414ea664e45ba2c5
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-07-04 16:04:47 +0200

    NRP-1833 Apply field mappings for the AuthorizationService so that it's possible to apply classifiers for 'Submode' for multiple fields in the model; "airSubmode", "busSubmode", "coachSubmode" ...

--- src/main/java/org/rutebanken/tiamat/config/AuthorizationServiceConfig.java
+++ src/main/java/org/rutebanken/tiamat/config/AuthorizationServiceConfig.java
@@ -5,9 +5,19 @@ import org.rutebanken.helper.organisation.RoleAssignmentExtractor;
 import org.rutebanken.tiamat.auth.check.TiamatOriganisationChecker;
 import org.rutebanken.tiamat.auth.TiamatEntityResolver;
 import org.rutebanken.tiamat.auth.check.TopographicPlaceChecker;
+import org.rutebanken.tiamat.model.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.SUBMODE;
 
 @Configuration
 public class AuthorizationServiceConfig {
@@ -19,7 +29,13 @@ public class AuthorizationServiceConfig {
                                                                   TiamatOriganisationChecker tiamatOriganisationChecker,
                                                                   TopographicPlaceChecker topographicPlaceChecker,
                                                                   TiamatEntityResolver tiamatEntityResolver) {
-        return new ReflectionAuthorizationService(roleAssignmentExtractor, authorizationEnabled, tiamatOriganisationChecker, topographicPlaceChecker, tiamatEntityResolver);
+
+        // Should be made configurable
+        Map<String, List<String>> fieldMappings = new HashMap<>();
+        fieldMappings.put(SUBMODE, Arrays.asList("airSubmode", "busSubmode", "coachSubmode", "funicularSubmode", "metroSubmode", "tramSubmode", "telecabinSubmode", "railSubmode", "waterSubmode"));
+
+
+        return new ReflectionAuthorizationService(roleAssignmentExtractor, authorizationEnabled, tiamatOriganisationChecker, topographicPlaceChecker, tiamatEntityResolver, fieldMappings);
     }
 
 }
--- src/test/java/org/rutebanken/tiamat/auth/TiamatAuthorizationServiceTest.java
+++ src/test/java/org/rutebanken/tiamat/auth/TiamatAuthorizationServiceTest.java
@@ -4,16 +4,19 @@ import org.junit.Test;
 import org.rutebanken.helper.organisation.ReflectionAuthorizationService;
 import org.rutebanken.helper.organisation.RoleAssignment;
 import org.rutebanken.tiamat.TiamatIntegrationTest;
+import org.rutebanken.tiamat.model.BusSubmodeEnumeration;
 import org.rutebanken.tiamat.model.Quay;
 import org.rutebanken.tiamat.model.StopPlace;
 import org.rutebanken.tiamat.model.StopTypeEnumeration;
 import org.rutebanken.tiamat.repository.StopPlaceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.rutebanken.helper.organisation.AuthorizationConstants.ENTITY_TYPE;
 
+@Transactional // Because of the authorization service logs entities which could read lazy loaded fields
 public class TiamatAuthorizationServiceTest extends TiamatIntegrationTest {
 
     @Autowired
@@ -81,4 +84,42 @@ public class TiamatAuthorizationServiceTest extends TiamatIntegrationTest {
         assertThat(authorized, is(false));
     }
 
+    /**
+     * EntityType=StopPlace, StopPlaceType=!railStation,!airport, Submode=!railReplacementBus
+     */
+    @Test
+    public void notAuthorizedWithSubmodeAndType() {
+        RoleAssignment roleAssignment = RoleAssignment.builder()
+                .withRole("editStops")
+                .withOrganisation("OST")
+                .withEntityClassification(ENTITY_TYPE, "StopPlace")
+                .withEntityClassification("StopPlaceType", "!airport")
+                .withEntityClassification("Submode", "!railReplacementBus")
+                .build();
+
+        StopPlace stopPlace = new StopPlace();
+        stopPlace.setStopPlaceType(StopTypeEnumeration.AIRPORT);
+        stopPlace.setBusSubmode(BusSubmodeEnumeration.RAIL_REPLACEMENT_BUS);
+
+        boolean authorized = reflectionAuthorizationService.authorized(roleAssignment, stopPlace, roleAssignment.r);
+        assertThat("Should not be authorized as both type and subode does not match", authorized, is(false));
+    }
+
+    @Test
+    public void authorizedWithSubmodeAndType() {
+        RoleAssignment roleAssignment = RoleAssignment.builder()
+                .withRole("editStops")
+                .withOrganisation("OST")
+                .withEntityClassification(ENTITY_TYPE, "StopPlace")
+                .withEntityClassification("StopPlaceType", "!airport")
+                .withEntityClassification("Submode", "!railReplacementBus")
+                .build();
+
+        StopPlace stopPlace = new StopPlace();
+        stopPlace.setStopPlaceType(StopTypeEnumeration.BUS_STATION);
+        stopPlace.setBusSubmode(BusSubmodeEnumeration.REGIONAL_BUS);
+
+        boolean authorized = reflectionAuthorizationService.authorized(roleAssignment, stopPlace, roleAssignment.r);
+        assertThat("Should be authorized as both type and subode does not match", authorized, is(true));
+    }
 }
