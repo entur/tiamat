cfe01ca3aa68b24687a9aed26ac9241ae61c4a2e
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-08-03 09:23:44 +0200

    NRP-1297 Improve test assert statements for conversion of line string

--- src/test/java/org/rutebanken/tiamat/netex/mapping/converter/LineStringConverterTest.java
+++ src/test/java/org/rutebanken/tiamat/netex/mapping/converter/LineStringConverterTest.java
@@ -38,7 +38,14 @@ public class LineStringConverterTest {
         assertThat(gisLineString.getPosList().getCount().intValue()).isEqualTo(4);
         assertThat(gisLineString.getPosList().getSrsDimension().intValue()).isEqualTo(geometryFactory.getSRID());
         assertThat(gisLineString.getId()).isNotEmpty();
-        // assertThat(gisLineString.getPosList().getSrsName()).isEqualTo("WGS84");
+        // Check that the format is Y,X
+        assertThat(gisLineString.getPosList().getValue().get(0)).isEqualTo(coordinates[0].y);
+        assertThat(gisLineString.getPosList().getValue().get(1)).isEqualTo(coordinates[0].x);
+        assertThat(gisLineString.getPosList().getValue().get(2)).isEqualTo(coordinates[1].y);
+        assertThat(gisLineString.getPosList().getValue().get(3)).isEqualTo(coordinates[1].x);
+
+
+//         assertThat(gisLineString.getPosList().getSrsName()).isEqualTo("WGS84");
     }
 
     @Test
@@ -48,11 +55,15 @@ public class LineStringConverterTest {
                 .withId("LineString")
                 .withPosList(new DirectPositionListType()
                         .withSrsDimension(BigInteger.valueOf(geometryFactory.getSRID()))
-                        .withValue(9.1, 71.1, 9.5, 4.1));
+                        .withValue(71.1, 9.1, 4.1, 9.5));
 
         LineString lineString = lineStringConverter.convertTo(lineStringType, new TypeBuilder<LineString>(){}.build());
         assertThat(lineString).isNotNull();
         assertThat(lineString.getCoordinates()).hasSize(2);
+        assertThat(lineString.getCoordinates()[0].x).isEqualTo(lineStringType.getPosList().getValue().get(1));
+        assertThat(lineString.getCoordinates()[0].y).isEqualTo(lineStringType.getPosList().getValue().get(0));
+        assertThat(lineString.getCoordinates()[1].x).isEqualTo(lineStringType.getPosList().getValue().get(3));
+        assertThat(lineString.getCoordinates()[1].y).isEqualTo(lineStringType.getPosList().getValue().get(2));
 
     }
 }
