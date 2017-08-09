602a9b92184686ff565e28e38b45a6bf1e7b4f03
Lasse Tyrihjell <lassetyr@gmail.com>
2017-08-09 11:05:56 +0200

    @wip Adding processes for mutating multimodal stop places

--- src/main/java/org/rutebanken/tiamat/rest/graphql/GraphQLNames.java
+++ src/main/java/org/rutebanken/tiamat/rest/graphql/GraphQLNames.java
@@ -302,4 +302,8 @@ public class GraphQLNames {
     public static final String REOPEN_STOP_PLACE = "reopenStopPlace";
     public static final String DELETE_STOP_PLACE = "deleteStopPlace";
     public static final String DELETE_QUAY_FROM_STOP_PLACE = "deleteQuay";
+
+    public static final String CREATE_MULTIMODAL_STOPPLACE = "createMultiModalStopPlace";
+    public static final String ADD_TO_MULTIMODAL_STOPPLACE = "addToModalStopPlace";
+    public static final String REMOVE_FROM_MULTIMODAL_STOPPLACE = "removeFromModalStopPlace";
 }
