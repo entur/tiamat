a813b3e51439b4b77013486622162a762444c493
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-08-09 17:05:21 +0200

    NRP-1912 Get embeddable lingual string from graphql arguments. Remove unused dependency and invalid comments.

--- src/main/java/org/rutebanken/tiamat/rest/graphql/operations/MultiModalityOperationsBuilder.java
+++ src/main/java/org/rutebanken/tiamat/rest/graphql/operations/MultiModalityOperationsBuilder.java
@@ -11,11 +11,13 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import static graphql.Scalars.GraphQLString;
 import static graphql.schema.GraphQLArgument.newArgument;
 import static graphql.schema.GraphQLFieldDefinition.newFieldDefinition;
 import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.*;
+import static org.rutebanken.tiamat.rest.graphql.resolvers.ObjectResolver.getEmbeddableString;
 import static org.rutebanken.tiamat.rest.graphql.types.CustomGraphQLTypes.embeddableMultiLingualStringInputObjectType;
 
 @Component
@@ -24,23 +26,18 @@ public class MultiModalityOperationsBuilder {
     @Autowired
     private MultiModalStopPlaceEditor parentStopPlaceCreator;
 
-    @Autowired
-    private DateScalar dateScalar;
-
     public List<GraphQLFieldDefinition> getMultiModalityOperations(GraphQLObjectType stopPlaceObjectType) {
         List<GraphQLFieldDefinition> operations = new ArrayList<>();
 
-        //Merge two StopPlaces
         operations.add(newFieldDefinition()
                 .type(stopPlaceObjectType)
                 .name(CREATE_MULTIMODAL_STOPPLACE)
                 .description("Creates a new multimodal parent StopPlace")
                 .argument(newArgument().name(STOP_PLACE_ID).type(new GraphQLList(GraphQLString)))
                 .argument(newArgument().name(NAME).type(new GraphQLNonNull(embeddableMultiLingualStringInputObjectType)))
-                .dataFetcher(environment -> parentStopPlaceCreator.createMultiModalParentStopPlace(environment.getArgument(STOP_PLACE_ID), environment.getArgument(NAME)))
+                .dataFetcher(environment -> parentStopPlaceCreator.createMultiModalParentStopPlace(environment.getArgument(STOP_PLACE_ID), getEmbeddableString((Map) environment.getArgument(NAME))))
                 .build());
 
-        //Merge two StopPlaces
         operations.add(newFieldDefinition()
                 .type(stopPlaceObjectType)
                 .name(ADD_TO_MULTIMODAL_STOPPLACE)
@@ -50,7 +47,6 @@ public class MultiModalityOperationsBuilder {
                 .dataFetcher(environment -> parentStopPlaceCreator.addToMultiModalParentStopPlace(environment.getArgument(PARENT_SITE_REF), environment.getArgument(STOP_PLACE_ID)))
                 .build());
 
-        //Merge two StopPlaces
         operations.add(newFieldDefinition()
                 .type(stopPlaceObjectType)
                 .name(REMOVE_FROM_MULTIMODAL_STOPPLACE)
