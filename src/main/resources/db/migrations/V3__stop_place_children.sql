db3f547fba2c1b252e7d0542b263a81578c5a35f
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-08-17 15:32:47 +0200

    NRP-1912 Fix incorrect database migration for stop place children

--- src/main/resources/db/migrations/V3__stop_place_children.sql
+++ src/main/resources/db/migrations/V3__stop_place_children.sql
@@ -1,4 +1,4 @@
-create table stop_place_children (stop_place_id int8 not null, children_id int8 not null, primary key (stop_place_id, children_id))
-alter table stop_place_children add constraint UK_kj0a7ruk5k2bub2028nbkqwtw unique (children_id)
-alter table stop_place_children add constraint FK4erbijiihg4wb6qblaolq3kwn foreign key (children_id) references stop_place
-alter table stop_place_children add constraint FKlljrfiip140imskbef529knwo foreign key (stop_place_id) references stop_place
+create table stop_place_children (stop_place_id int8 not null, children_id int8 not null, primary key (stop_place_id, children_id));
+alter table stop_place_children add constraint UK_kj0a7ruk5k2bub2028nbkqwtw unique (children_id);
+alter table stop_place_children add constraint FK4erbijiihg4wb6qblaolq3kwn foreign key (children_id) references stop_place;
+alter table stop_place_children add constraint FKlljrfiip140imskbef529knwo foreign key (stop_place_id) references stop_place;
