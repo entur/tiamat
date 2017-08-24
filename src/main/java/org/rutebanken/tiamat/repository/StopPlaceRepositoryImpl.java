3776ab783f01214f5bd4974e7e0ce67ea69d65a2
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-08-24 17:10:12 +0200

    NRP-2040 Make sure no stop place is returned if neither the child stop or the parent stop has from date set

--- src/main/java/org/rutebanken/tiamat/repository/StopPlaceRepositoryImpl.java
+++ src/main/java/org/rutebanken/tiamat/repository/StopPlaceRepositoryImpl.java
@@ -301,8 +301,8 @@ public class StopPlaceRepositoryImpl implements StopPlaceRepositoryCustom {
                              "      ON spkv.key_values_key in (:mappingIdKeys) AND spkv.key_values_id = v.value_id AND v.items NOT LIKE '' " +
                              "  INNER JOIN stop_place s ON s.id = spkv.stop_place_id " +
                              "  LEFT JOIN stop_place p ON s.parent_site_ref = p.netex_id AND s.parent_site_ref_version = CAST(p.version as text) " +
-                             "WHERE ((s.from_date IS NULL OR s.from_date <= :pointInTime) AND (s.to_date IS NULL OR s.to_date > :pointInTime)) " +
-                             "     OR ( p.netex_id IS NOT NULL AND ((p.from_date IS NULL OR p.from_date <= :pointInTime) AND (p.to_date IS NULL OR p.to_date > :pointInTime)))";
+                             "WHERE (p.netex_id IS NOT NULL AND (p.from_date <= :pointInTime AND (p.to_date IS NULL OR p.to_date > :pointInTime)))" +
+                             "  OR (p.netex_id IS NULL AND (s.from_date <= :pointInTime AND (s.to_date IS NULL OR s.to_date > :pointInTime)))";
 
 
         Query nativeQuery = entityManager.createNativeQuery(sql).setFirstResult(recordPosition).setMaxResults(recordsPerRoundTrip);
