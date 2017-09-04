60f09b8fe6d2b9d0c267070359a051fc86ca0615
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-09-04 15:00:35 +0200

    NRP-2091 Add toString to publication delivery structure page

--- src/main/java/org/rutebanken/tiamat/exporter/PublicationDeliveryStructurePage.java
+++ src/main/java/org/rutebanken/tiamat/exporter/PublicationDeliveryStructurePage.java
@@ -1,5 +1,6 @@
 package org.rutebanken.tiamat.exporter;
 
+import com.google.common.base.MoreObjects;
 import org.rutebanken.netex.model.PublicationDeliveryStructure;
 
 public class PublicationDeliveryStructurePage {
@@ -14,5 +15,14 @@ public class PublicationDeliveryStructurePage {
         this.publicationDeliveryStructure = publicationDeliveryStructure;
         this.totalElements = totalElements;
         this.hasNext = hasNext;
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("publicationDeliveryStructure", publicationDeliveryStructure)
+                .add("totalElements", totalElements)
+                .add("hasNext", hasNext)
+                .toString();
     }
 }
