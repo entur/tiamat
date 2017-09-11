905c75299c69ae6a73b914e54bf8a8b7983888ce
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-09-11 11:43:42 +0200

    NRP-2103 Test that terminating child stop is not accepted. Do not reopen in terminator test.

--- src/test/java/org/rutebanken/tiamat/service/stopplace/StopPlaceTerminatorTest.java
+++ src/test/java/org/rutebanken/tiamat/service/stopplace/StopPlaceTerminatorTest.java
@@ -3,27 +3,24 @@ package org.rutebanken.tiamat.service.stopplace;
 import org.junit.Test;
 import org.rutebanken.tiamat.TiamatIntegrationTest;
 import org.rutebanken.tiamat.model.EmbeddableMultilingualString;
+import org.rutebanken.tiamat.model.SiteRefStructure;
 import org.rutebanken.tiamat.model.StopPlace;
-import org.rutebanken.tiamat.service.stopplace.StopPlaceReopener;
-import org.rutebanken.tiamat.service.stopplace.StopPlaceTerminator;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.transaction.Transactional;
 import java.time.Instant;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StopPlaceTerminatorTest extends TiamatIntegrationTest {
 
     @Autowired
     private StopPlaceTerminator stopPlaceTerminator;
 
-    @Autowired
-    private StopPlaceReopener stopPlaceReopener;
-
     @Transactional
     @Test
-    public void testTerminateAndReopenStopPlace() {
+    public void testTerminateStopPlace() {
 
         StopPlace savedStopPlace = stopPlaceVersionedSaverService.saveNewVersion(new StopPlace(new EmbeddableMultilingualString("Name")));
         String stopPlaceNetexId = savedStopPlace.getNetexId();
@@ -32,16 +29,18 @@ public class StopPlaceTerminatorTest extends TiamatIntegrationTest {
 
         StopPlace terminatedStopPlace = stopPlaceTerminator.terminateStopPlace(stopPlaceNetexId, timeOfTermination, "Terminating Stop");
 
-        String reopenedVersionComment = "Reopened StopPlace";
+        assertThat(terminatedStopPlace.getValidBetween().getToDate()).isNotNull();
+    }
 
-        // Act
-        StopPlace reopenedStopPlace = stopPlaceReopener.reopenStopPlace(stopPlaceNetexId, reopenedVersionComment);
+    @Transactional
+    @Test
+    public void testCannotTerminateChild() {
 
-        assertThat(reopenedStopPlace).isNotNull();
-        assertThat(reopenedStopPlace.getVersion()).isGreaterThan(terminatedStopPlace.getVersion());
-        assertThat(reopenedStopPlace.getVersionComment()).isEqualTo(reopenedVersionComment);
-        assertThat(reopenedStopPlace.getValidBetween().getFromDate())
-                .as("Reopened stop place from date").isAfterOrEqualTo(timeOfTermination);
-        assertThat(reopenedStopPlace.getValidBetween().getToDate()).isNull();
+        StopPlace savedStopPlace = stopPlaceVersionedSaverService.saveNewVersion(new StopPlace(new EmbeddableMultilingualString("Name")));
+        savedStopPlace.setParentSiteRef(new SiteRefStructure("x", "2"));
+        stopPlaceRepository.save(savedStopPlace);
+        String stopPlaceNetexId = savedStopPlace.getNetexId();
+
+        assertThatThrownBy(() -> stopPlaceTerminator.terminateStopPlace(stopPlaceNetexId, Instant.now(), "Terminating Stop")).isInstanceOf(IllegalArgumentException.class);
     }
 }
