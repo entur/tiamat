10465eb3ef59347a43440ba87f0c5999d6bcc630
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-11-03 14:22:02 +0100

    NRP-2311 Cannot drop explicit_equipments_rel_structure because it is referenced to by installed_equipment which is marked as entity. It does not contain any values. But it subclassed

--- src/main/resources/db/migration/V18__drop_unused_tables.sql
+++ src/main/resources/db/migration/V18__drop_unused_tables.sql
@@ -1,7 +1,5 @@
-DROP TABLE vehicle_stopping_places_rel_structure;
+DROP TABLE vehicle_stopping_places_rel_structure_vehicle_stopping_place_re
 DROP SEQUENCE vehicle_stopping_places_rel_structure_seq;
+DROP TABLE vehicle_stopping_places_rel_structure;
 
-
-DROP TABLE explicit_equipments_rel_structure;
-DROP SEQUENCE explicit_equipments_rel_structure_seq;
 
