1ea737c158543c2286918f71ffc8c1d7ea59267f
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-11-03 14:24:31 +0100

    NRP-2311 Fix minor issue in migration file

--- src/main/resources/db/migration/V18__drop_unused_tables.sql
+++ src/main/resources/db/migration/V18__drop_unused_tables.sql
@@ -1,4 +1,4 @@
-DROP TABLE vehicle_stopping_places_rel_structure_vehicle_stopping_place_re
+DROP TABLE vehicle_stopping_places_rel_structure_vehicle_stopping_place_re;
 DROP SEQUENCE vehicle_stopping_places_rel_structure_seq;
 DROP TABLE vehicle_stopping_places_rel_structure;
 
