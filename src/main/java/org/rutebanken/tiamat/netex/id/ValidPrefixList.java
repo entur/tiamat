433da9517285b74b5be555ecaed14d1a81f43acf
Mathieu Hicauber <mhicauber@okina.fr>
2017-12-13 11:06:39 +0100

    Configurable NetexId

--- src/main/java/org/rutebanken/tiamat/netex/id/ValidPrefixList.java
+++ src/main/java/org/rutebanken/tiamat/netex/id/ValidPrefixList.java
@@ -15,27 +15,30 @@
 
 package org.rutebanken.tiamat.netex.id;
 
-import com.google.common.collect.ImmutableList;
-import org.rutebanken.tiamat.model.EntityInVersionStructure;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.util.*;
+import java.util.List;
+import java.util.Map;
 
 @Component
 public class ValidPrefixList {
 
     public static final String ANY_PREFIX = "*";
 
+    @Value("${netex.validPrefix}")
+    public static final String VALID_NETEX_PREFIX = "NSR";
+
+
     private static final Logger logger = LoggerFactory.getLogger(ValidPrefixList.class);
     private final Map<String, List<String>> validPrefixesPerType;
 
     @Autowired
     public ValidPrefixList(@Value("#{${netex.id.valid.prefix.list:{TopographicPlace:{'KVE','WOF','OSM'},TariffZone:{'*'}}}}") Map<String, List<String>> validPrefixesPerType) {
-        for(String type : validPrefixesPerType.keySet()) {
+        for (String type : validPrefixesPerType.keySet()) {
             List<String> validPrefixesForType = validPrefixesPerType.get(type);
             logger.info("Loaded valid prefixes for {}: {} ", type, validPrefixesForType);
         }
@@ -54,18 +57,18 @@ public class ValidPrefixList {
 
     public boolean isValidPrefixForType(String prefix, String type) {
 
-        if(prefix.equals(NetexIdHelper.NSR)) {
+        if (prefix.equals(VALID_NETEX_PREFIX)) {
             return true;
         }
 
         List<String> validPrefixes = validPrefixesPerType.get(type);
-        if(validPrefixes == null) {
+        if (validPrefixes == null) {
             return false;
         }
-        if(validPrefixes.contains(prefix)) {
+        if (validPrefixes.contains(prefix)) {
             return true;
         }
-        if(validPrefixes.contains(ANY_PREFIX)) {
+        if (validPrefixes.contains(ANY_PREFIX)) {
             return true;
         }
         return false;
