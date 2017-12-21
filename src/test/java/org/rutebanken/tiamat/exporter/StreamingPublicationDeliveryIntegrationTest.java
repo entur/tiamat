79054568a9ff84f48c4d3cdf0637b7897a972b73
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-12-21 14:44:46 +0100

    ROR-53 Improve test for async streaming publication delivery. Reproduce missing tariff zones. Test with three different versions of tariff zones. One stop place refers to tz v 1, another without explicit version ref. Then, both max version (3) and version one should be returned, but not version 2.

--- src/test/java/org/rutebanken/tiamat/exporter/StreamingPublicationDeliveryIntegrationTest.java
+++ src/test/java/org/rutebanken/tiamat/exporter/StreamingPublicationDeliveryIntegrationTest.java
@@ -23,10 +23,12 @@ import org.rutebanken.tiamat.exporter.params.ExportParams;
 import org.rutebanken.tiamat.exporter.params.StopPlaceSearch;
 import org.rutebanken.tiamat.model.*;
 import org.rutebanken.tiamat.netex.mapping.PublicationDeliveryHelper;
+import org.rutebanken.tiamat.netex.validation.NetexXmlReferenceValidator;
 import org.rutebanken.tiamat.rest.netex.publicationdelivery.PublicationDeliveryUnmarshaller;
 import org.rutebanken.tiamat.versioning.TariffZoneSaverService;
 import org.rutebanken.tiamat.versioning.TopographicPlaceVersionedSaverService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.xml.sax.SAXException;
 
 import javax.xml.bind.JAXBContext;
@@ -45,6 +47,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  * Test streaming publication delivery with h2 database
  * {@link StreamingPublicationDeliveryTest} is without database and spring context.
  */
+@Transactional
 public class StreamingPublicationDeliveryIntegrationTest extends TiamatIntegrationTest {
 
     @Autowired
@@ -62,6 +65,8 @@ public class StreamingPublicationDeliveryIntegrationTest extends TiamatIntegrati
     @Autowired
     private PublicationDeliveryHelper publicationDeliveryHelper;
 
+    private NetexXmlReferenceValidator netexXmlReferenceValidator = new NetexXmlReferenceValidator(true);
+
 
     @Test
     public void streamStopPlaceIntoPublicationDelivery() throws Exception {
@@ -70,20 +75,43 @@ public class StreamingPublicationDeliveryIntegrationTest extends TiamatIntegrati
 
         TopographicPlace topographicPlace = new TopographicPlace(new EmbeddableMultilingualString("Some municipality"));
         topographicPlace.setTopographicPlaceType(TopographicPlaceTypeEnumeration.MUNICIPALITY);
-        topographicPlaceVersionedSaverService.saveNewVersion(topographicPlace);
+        topographicPlace = topographicPlaceVersionedSaverService.saveNewVersion(topographicPlace);
 
-        TariffZone tariffZone = new TariffZone();
-        tariffZone.setNetexId("CRI:TariffZone:1");
-        tariffZoneSaverService.saveNewVersion(tariffZone);
+        String tariffZoneId = "CRI:TariffZone:1";
 
-        StopPlace stopPlace = new StopPlace(new EmbeddableMultilingualString("stop place in publication delivery"));
-        stopPlace.getTariffZones().add(new TariffZoneRef(tariffZone.getNetexId()));
-        stopPlace = stopPlaceVersionedSaverService.saveNewVersion(stopPlace);
+        TariffZone tariffZoneV1 = new TariffZone();
+        tariffZoneV1.setNetexId(tariffZoneId);
+        tariffZoneV1.setVersion(1L);
+        tariffZoneV1 = tariffZoneRepository.save(tariffZoneV1);
+
+        TariffZone tariffZoneV2 = new TariffZone();
+        tariffZoneV2.setNetexId(tariffZoneId);
+        tariffZoneV2.setVersion(2L);
+        tariffZoneV2 = tariffZoneRepository.save(tariffZoneV2);
+
+        TariffZone tariffZoneV3 = new TariffZone();
+        tariffZoneV3.setNetexId(tariffZoneId);
+        tariffZoneV3.setVersion(3L);
+        tariffZoneV3 = tariffZoneRepository.save(tariffZoneV3);
+
+        StopPlace stopPlace1 = new StopPlace(new EmbeddableMultilingualString("stop place in publication delivery"));
+        stopPlace1.getTariffZones().add(new TariffZoneRef(tariffZoneId)); // Without version, implicity v3
+        stopPlace1 = stopPlaceVersionedSaverService.saveNewVersion(stopPlace1);
+        final String stopPlace1NetexId = stopPlace1.getNetexId();
+
+        StopPlace stopPlace2 = new StopPlace(new EmbeddableMultilingualString("another stop place in publication delivery"));
+        // StopPlace 2 refers to tariffzone version 1. This must be included in the publication delivery, allthough v2 and v3 exists
+        stopPlace2.getTariffZones().add(new TariffZoneRef(tariffZoneV1));
+        stopPlace2 = stopPlaceVersionedSaverService.saveNewVersion(stopPlace2);
+        final String stopPlace2NetexId = stopPlace2.getNetexId();
 
         // Allows setting topographic place without lookup.
         // To have the lookup work, topographic place polygon must exist
-        stopPlace.setTopographicPlace(topographicPlace);
-        stopPlaceRepository.save(stopPlace);
+        stopPlace1.setTopographicPlace(topographicPlace);
+        stopPlaceRepository.save(stopPlace1);
+
+        stopPlaceRepository.flush();
+        tariffZoneRepository.flush();
 
         ExportParams exportParams = ExportParams.newExportParamsBuilder()
                 .setStopPlaceSearch(
@@ -98,14 +126,12 @@ public class StreamingPublicationDeliveryIntegrationTest extends TiamatIntegrati
 
         String xml = byteArrayOutputStream.toString();
 
+        System.out.println(xml);
+
         // The unmarshaller will validate as well. But only if validateAgainstSchema is true
         validate(xml);
-
-        assertThat(xml)
-                .contains("<StopPlace")
-                .contains("</PublicationDelivery")
-                .contains("</dataObjects>");
-
+        // Validate using own implementation of netex xml reference validator
+        netexXmlReferenceValidator.validateNetexReferences(new ByteArrayInputStream(xml.getBytes()), "publicationDelivery");
 
         PublicationDeliveryStructure publicationDeliveryStructure = publicationDeliveryUnmarshaller.unmarshal(new ByteArrayInputStream(xml.getBytes()));
 
@@ -113,24 +139,60 @@ public class StreamingPublicationDeliveryIntegrationTest extends TiamatIntegrati
         List<org.rutebanken.netex.model.StopPlace> stops = siteFrame.getStopPlaces().getStopPlace();
 
         assertThat(stops)
-                .hasSize(1)
+                .hasSize(2)
+                .as("stops expected")
                 .extracting(org.rutebanken.netex.model.StopPlace::getId)
-                .containsOnly(stopPlace.getNetexId());
+                .containsOnly(stopPlace1.getNetexId(), stopPlace2.getNetexId());
 
-        assertThat(stops.get(0).getTariffZones()).isNotNull();
-        assertThat(stops.get(0).getTariffZones().getTariffZoneRef().get(0).getRef()).isEqualTo(tariffZone.getNetexId());
+
+        // Make sure both stops have references to tariff zones and with correct version
+        org.rutebanken.netex.model.StopPlace actualStopPlace1 = stops.stream().filter(sp -> sp.getId().equals(stopPlace1NetexId)).findFirst().get();
+
+        assertThat(actualStopPlace1.getTariffZones())
+                .as("actual stop place 1 tariff zones")
+                .isNotNull();
+
+        org.rutebanken.netex.model.TariffZoneRef actualTariffZoneRefStopPlace1 = actualStopPlace1.getTariffZones().getTariffZoneRef().get(0);
+
+        // Stop place 1 refers to tariff zone v2 implicity beacuse the reference does not contain version value.
+        assertThat(actualTariffZoneRefStopPlace1.getRef())
+                .as("actual stop place 1 tariff zone ref")
+                .isEqualTo(tariffZoneId);
+
+        // Check stop place 2
+
+        org.rutebanken.netex.model.StopPlace actualStopPlace2 = stops.stream().filter(sp -> sp.getId().equals(stopPlace2NetexId)).findFirst().get();
+        org.rutebanken.netex.model.TariffZoneRef actualTariffZoneRefStopPlace2 = actualStopPlace2.getTariffZones().getTariffZoneRef().get(0);
+
+        assertThat(actualTariffZoneRefStopPlace2.getRef())
+                .as("actual stop place 2 tariff zone ref")
+                .isEqualTo(tariffZoneId);
+
+        assertThat(actualTariffZoneRefStopPlace2.getVersion())
+                .as("actual tariff zone ref for stop place 2 should point to version 1 of tariff zone")
+                .isEqualTo(String.valueOf(tariffZoneV1.getVersion()));
+
+        // Check topographic places
 
         assertThat(siteFrame.getTopographicPlaces()).isNotNull();
         assertThat(siteFrame.getTopographicPlaces().getTopographicPlace())
+                .as("site fra topopgraphic places")
                 .isNotNull()
                 .hasSize(1)
-                .extracting(org.rutebanken.netex.model.TopographicPlace::getId).containsOnly(topographicPlace.getNetexId());
+                .extracting(org.rutebanken.netex.model.TopographicPlace::getId)
+                .containsOnly(topographicPlace.getNetexId());
+
+        // Check tariff zones
 
         assertThat(siteFrame.getTariffZones())
+                .as("site fra tariff zones")
                 .isNotNull();
 
         assertThat(siteFrame.getTariffZones().getTariffZone())
-                .extracting(org.rutebanken.netex.model.TariffZone::getId).containsOnly(tariffZone.getNetexId());
+                .extracting(tariffZone -> tariffZone.getId() + "-" + tariffZone.getVersion())
+                .as("Both tariff zones exists in publication delivery. But not the one not being reffered to (v2)")
+                .containsOnly(tariffZoneId + "-" + 1, tariffZoneId + "-" + 3);
+
 
 
     }
