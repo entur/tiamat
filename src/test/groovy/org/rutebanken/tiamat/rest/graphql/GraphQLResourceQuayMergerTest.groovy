3441041e7dff8c6213377dcf9dde439bdff50e04
Cristoffer Solem <cristoffer.solem@gmail.com>
2018-08-06 13:30:48 +0200

    ROR-442 Fix syntax error in graphql merge quays test

--- src/test/groovy/org/rutebanken/tiamat/rest/graphql/GraphQLResourceQuayMergerTest.groovy
+++ src/test/groovy/org/rutebanken/tiamat/rest/graphql/GraphQLResourceQuayMergerTest.groovy
@@ -82,7 +82,6 @@ class GraphQLResourceQuayMergerTest extends AbstractGraphQLResourceIntegrationTe
                                 }
                             }
                         }
-                   }
                  }"""
 
 
