c26aaf4fb9b89838a45e61e485d57e0927a26981
Assad Riaz <assad.riaz@entur.org>
2020-02-02 22:00:12 +0100

    Refactoring added BlobStoreService Interface to implement  LocalBlobStoreService to run tiamat without gcs and GcsBlobStoreService

--- src/main/java/org/rutebanken/tiamat/service/BlobStoreService.java
+++ src/main/java/org/rutebanken/tiamat/service/BlobStoreService.java
@@ -1,79 +1,15 @@
-/*
- * Licensed under the EUPL, Version 1.2 or – as soon they will be approved by
- * the European Commission - subsequent versions of the EUPL (the "Licence");
- * You may not use this work except in compliance with the Licence.
- * You may obtain a copy of the Licence at:
- *
- *   https://joinup.ec.europa.eu/software/page/eupl
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the Licence is distributed on an "AS IS" basis,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the Licence for the specific language governing permissions and
- * limitations under the Licence.
- */
-
 package org.rutebanken.tiamat.service;
 
 import com.google.cloud.storage.Storage;
-import com.google.common.io.ByteStreams;
-import org.rutebanken.helper.gcp.BlobStoreHelper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
 
 import java.io.InputStream;
 
-@Service
-public class BlobStoreService {
+public interface BlobStoreService {
+    void upload(String fileName, InputStream inputStream);
 
-    private static final Logger logger = LoggerFactory.getLogger(BlobStoreService.class);
+    Storage getStorage();
 
-    private final String bucketName;
+    InputStream download(String fileName);
 
-    private final String blobPath;
-    private final String credentialPath;
-    private final String projectId;
-
-
-    public BlobStoreService(@Value("${blobstore.gcs.credential.path}") String credentialPath,
-                            @Value("${blobstore.gcs.bucket.name}") String bucketName,
-                            @Value("${blobstore.gcs.blob.path}") String blobPath,
-                            @Value("${blobstore.gcs.project.id}") String projectId) {
-
-        this.bucketName = bucketName;
-        this.blobPath = blobPath;
-        this.credentialPath = credentialPath;
-        this.projectId = projectId;
-    }
-
-    public void upload(String fileName, InputStream inputStream) {
-        Storage storage = getStorage();
-        String blobIdName = createBlobIdName(blobPath, fileName);
-        try {
-            logger.info("Uploading {} to path {} in bucket {}", fileName, blobPath, bucketName);
-            BlobStoreHelper.uploadBlobWithRetry(storage, bucketName, blobIdName, inputStream, false);
-        } catch (Exception e) {
-            throw new RuntimeException("Error uploading file " + fileName + ", blobIdName " + blobIdName + " to bucket " + bucketName, e);
-        }
-    }
-
-    private Storage getStorage() {
-        try {
-            logger.info("Get storage for project {}", projectId);
-            return BlobStoreHelper.getStorage(credentialPath, projectId);
-        } catch (RuntimeException e) {
-            throw new RuntimeException("Error setting up BlobStore from blobstore.gcs.credential.path '" + credentialPath + "' and blobstore.gcs.project.id '" + projectId + "'", e);
-        }
-    }
-
-    public InputStream download(String fileName) {
-        String blobIdName = createBlobIdName(blobPath, fileName);
-        return BlobStoreHelper.getBlob(getStorage(), bucketName, blobIdName);
-    }
-
-    public String createBlobIdName(String blobPath, String fileName) {
-        return blobPath + '/' + fileName;
-    }
+    String createBlobIdName(String blobPath, String fileName);
 }
---
+++ src/main/java/org/rutebanken/tiamat/service/LocalBlobStoreService.java
@@ -0,0 +1,72 @@
+package org.rutebanken.tiamat.service;
+
+import com.google.cloud.storage.Storage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Service
+@Profile("local-blobstore")
+public class LocalBlobStoreService implements BlobStoreService {
+    private static final Logger logger = LoggerFactory.getLogger(LocalBlobStoreService.class);
+
+
+    @Value("${blobstore.local.folder:files/blob}")
+    private String baseFolder;
+
+    @Override
+    public void upload(String fileName, InputStream inputStream) {
+        logger.debug("Upload blob called in local-disk blob store on " + fileName);
+        try {
+            Path localPath = Paths.get(fileName);
+
+            Path folder = Paths.get(baseFolder).resolve(localPath.getParent());
+            Files.createDirectories(folder);
+
+            Path fullPath = Paths.get(baseFolder).resolve(localPath);
+            Files.deleteIfExists(fullPath);
+
+            Files.copy(inputStream, fullPath);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public InputStream download(String fileName) {
+
+        logger.debug("get blob called in local-disk blob store on " + fileName);
+        Path path = Paths.get(baseFolder).resolve(fileName);
+        if (!path.toFile().exists()) {
+            logger.debug("getBlob(): File not found in local-disk blob store: " + path);
+            return null;
+        }
+        logger.debug("getBlob(): File found in local-disk blob store: " + path);
+        try {
+            // converted as ByteArrayInputStream so that Camel stream cache can reopen it
+            // since ByteArrayInputStream.close() does nothing
+            return new ByteArrayInputStream(Files.readAllBytes(path));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String createBlobIdName(String blobPath, String fileName) {
+        return blobPath + '/' + fileName;
+    }
+
+    @Override
+    public Storage getStorage() {
+        return null;
+    }
+}
