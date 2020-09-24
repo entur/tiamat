106939256823ff18e85dd0faed1175ab57dfcf89
Assad Riaz <assad.riaz@entur.org>
2020-09-24 18:11:23 +0200

    WIP:ServiceFrameExporter Implementation

---
+++ src/main/java/org/rutebanken/tiamat/model/ScheduledStopPointsInFrame_RelStructure.java
@@ -0,0 +1,16 @@
+package org.rutebanken.tiamat.model;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ScheduledStopPointsInFrame_RelStructure extends ContainmentAggregationStructure {
+    protected List<ScheduledStopPoint> scheduledStopPoint;
+
+    public List<ScheduledStopPoint> getScheduledStopPoint() {
+        if (scheduledStopPoint == null) {
+            scheduledStopPoint = new ArrayList<>();
+        }
+        return scheduledStopPoint;
+    }
+}
