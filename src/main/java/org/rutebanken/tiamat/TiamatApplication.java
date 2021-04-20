a1d5b0c81b0bbc6ba57f5702cd9d5532a4cb7d61
Assad Riaz <assad.riaz@entur.org>
2021-04-20 16:03:48 +0200

    Refactoring: optimized spring annotation

--- src/main/java/org/rutebanken/tiamat/TiamatApplication.java
+++ src/main/java/org/rutebanken/tiamat/TiamatApplication.java
@@ -17,21 +17,17 @@ package org.rutebanken.tiamat;
 
 import org.rutebanken.tiamat.model.StopPlace;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @Configuration
-@EnableAutoConfiguration
 @EnableTransactionManagement
 @EnableCaching
 @EntityScan(basePackageClasses = {StopPlace.class, Jsr310JpaConverters.class})
-@ComponentScan
 public class TiamatApplication {
 
     public static void main(String[] args) {
