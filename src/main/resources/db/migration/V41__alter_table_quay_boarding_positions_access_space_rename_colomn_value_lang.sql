3d0fad3a2e174345683de75d9ee28a8fb436a19e
Assad Riaz <assad.riaz@entur.org>
2021-07-23 12:18:11 +0200

    WKT-467 Implements following         * Fixed typo, Creates flyaway script to alter tables

--- src/main/resources/db/migration/V41__alter_table_quay_boarding_positions_access_space_rename_colomn_value_lang.sql
+++ src/main/resources/db/migration/V41__alter_table_quay_boarding_positions_access_space_rename_colomn_value_lang.sql
@@ -4,5 +4,5 @@ alter table public.quay rename COLUMN lang to label_lang;
 alter table public.boarding_position rename COLUMN value to label_value;
 alter table public.boarding_position rename COLUMN lang to label_lang;
 
-alter table public.access_space rename COLUMN value to label_value
+alter table public.access_space rename COLUMN value to label_value;
 alter table public.access_space rename COLUMN lang to label_lang;
