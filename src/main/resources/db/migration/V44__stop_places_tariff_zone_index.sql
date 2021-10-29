92eabaaa9528dbdfcdb2570a51771d2f6db2d56a
Assad Riaz <assad.riaz@entur.org>
2021-10-29 12:07:55 +0200

    Creates database index using flyway sql scripts

---
+++ src/main/resources/db/migration/V44__stop_places_tariff_zone_index.sql
@@ -0,0 +1,2 @@
+CREATE INDEX IF NOT EXISTS idx_accessibility_assessment_id ON accessibility_assessment_limitations(accessibility_assessment_id);
+CREATE INDEX IF NOT EXISTS idx_stop_place_id ON stop_place_tariff_zones(stop_place_id);
