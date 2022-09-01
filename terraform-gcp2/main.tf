4859a8ad550b7783fb99914ce9e226a86cb1b90e
Assad Riaz <assad.riaz@entur.org>
2022-09-01 18:01:11 +0200

    terraform config gcp2.0

--- terraform-gcp2/main.tf
+++ terraform-gcp2/main.tf
@@ -20,7 +20,7 @@ resource "google_storage_bucket" "storage_bucket" {
   storage_class      = var.storage_class
   bucket_policy_only = var.bucket_policy_only
   labels             = var.labels
-
+  uniform_bucket_level_access = true
   versioning {
     enabled = var.versioning
   }
