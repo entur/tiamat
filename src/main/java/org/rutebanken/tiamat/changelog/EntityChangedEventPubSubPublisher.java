7a88d4aef598fe5150cd2bb077c6ce10714e5625
Tom Erik Støwer <testower@gmail.com>
2022-10-07 12:31:44 +0200

    Move duplicated code to abstract class

--- src/main/java/org/rutebanken/tiamat/changelog/EntityChangedEventPubSubPublisher.java
+++ src/main/java/org/rutebanken/tiamat/changelog/EntityChangedEventPubSubPublisher.java
@@ -17,30 +17,22 @@ package org.rutebanken.tiamat.changelog;
 
 import org.rutebanken.tiamat.config.GooglePubSubConfig;
 import org.rutebanken.tiamat.model.EntityInVersionStructure;
-import org.rutebanken.tiamat.model.EntityStructure;
-import org.rutebanken.tiamat.model.StopPlace;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.Instant;
-import java.util.Optional;
-import java.util.UUID;
-
 @Component
 @Transactional
 @Profile("google-pubsub")
-public class EntityChangedEventPubSubPublisher implements EntityChangedListener {
+public class EntityChangedEventPubSubPublisher extends EntityChangedEventPublisher implements EntityChangedListener {
 
     @Autowired
     private GooglePubSubConfig.PubsubOutboundGateway pubsubOutboundGateway;
 
     @Value("${changelog.gcp.publish.enabled:true}")
     private boolean pubSubPublish;
-
-
 
     @Override
     public void onChange(EntityInVersionStructure entity) {
@@ -56,54 +48,5 @@ public class EntityChangedEventPubSubPublisher implements EntityChangedListener 
         if (pubSubPublish && isLoggedEntity(entity)) {
             pubsubOutboundGateway.sendToPubsub(toEntityChangedEvent(entity, true).toString());
         }
-    }
-
-    protected EntityChangedEvent toEntityChangedEvent(EntityInVersionStructure entity, boolean deleted) {
-        EntityChangedEvent event = new EntityChangedEvent();
-        event.msgId = UUID.randomUUID().toString();
-        event.entityType = getEntityType(entity);
-        event.entityId = entity.getNetexId();
-        event.entityVersion = (entity).getVersion();
-        event.entityChanged = getEntityChangedAsEpochMillis(entity);
-        event.crudAction = getCrudAction(entity, deleted);
-        return event;
-    }
-
-    private Long getEntityChangedAsEpochMillis(EntityInVersionStructure entity) {
-        if (entity.getChanged() != null) {
-            return entity.getChanged().toEpochMilli();
-        } else {
-            return null;
-        }
-    }
-
-    private EntityChangedEvent.CrudAction getCrudAction(EntityInVersionStructure entity, boolean deleted) {
-        if (deleted) {
-            return EntityChangedEvent.CrudAction.DELETE;
-        } else if (entity.getVersion() == 1) {
-            return EntityChangedEvent.CrudAction.CREATE;
-        } else if (isDeactivated(entity)) {
-            return EntityChangedEvent.CrudAction.REMOVE;
-        } else {
-            return EntityChangedEvent.CrudAction.UPDATE;
-        }
-    }
-
-    private boolean isDeactivated(EntityInVersionStructure entity) {
-        if (entity.getValidBetween() == null) {
-            return false;
-        }
-        return entity.getValidBetween().getToDate() != null;
-    }
-
-    private boolean isLoggedEntity(EntityStructure entity) {
-        return getEntityType(entity) != null;
-    }
-
-    private EntityChangedEvent.EntityType getEntityType(EntityStructure entity) {
-        if (entity instanceof StopPlace) {
-            return EntityChangedEvent.EntityType.STOP_PLACE;
-        }
-        return null;
     }
 }
--- src/test/java/org/rutebanken/tiamat/changelog/EntityChangedEventActiveMQPublisher.java
+++ src/test/java/org/rutebanken/tiamat/changelog/EntityChangedEventActiveMQPublisher.java
@@ -16,8 +16,6 @@
 package org.rutebanken.tiamat.changelog;
 
 import org.rutebanken.tiamat.model.EntityInVersionStructure;
-import org.rutebanken.tiamat.model.EntityStructure;
-import org.rutebanken.tiamat.model.StopPlace;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
@@ -25,14 +23,10 @@ import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.UUID;
-
 @Component
 @Transactional
 @Profile("activemq")
-public class EntityChangedEventActiveMQPublisher implements EntityChangedListener {
-
-
+public class EntityChangedEventActiveMQPublisher extends EntityChangedEventPublisher implements EntityChangedListener {
 
     @Autowired
     private JmsTemplate jmsTemplate;
@@ -55,54 +49,5 @@ public class EntityChangedEventActiveMQPublisher implements EntityChangedListene
         if (publish && isLoggedEntity(entity)) {
             jmsTemplate.convertAndSend(queueName, toEntityChangedEvent(entity, true).toString());
         }
-    }
-
-    protected EntityChangedEvent toEntityChangedEvent(EntityInVersionStructure entity, boolean deleted) {
-        EntityChangedEvent event = new EntityChangedEvent();
-        event.msgId = UUID.randomUUID().toString();
-        event.entityType = getEntityType(entity);
-        event.entityId = entity.getNetexId();
-        event.entityVersion = (entity).getVersion();
-        event.entityChanged = getEntityChangedAsEpochMillis(entity);
-        event.crudAction = getCrudAction(entity, deleted);
-        return event;
-    }
-
-    private Long getEntityChangedAsEpochMillis(EntityInVersionStructure entity) {
-        if (entity.getChanged() != null) {
-            return entity.getChanged().toEpochMilli();
-        } else {
-            return null;
-        }
-    }
-
-    private EntityChangedEvent.CrudAction getCrudAction(EntityInVersionStructure entity, boolean deleted) {
-        if (deleted) {
-            return EntityChangedEvent.CrudAction.DELETE;
-        } else if (entity.getVersion() == 1) {
-            return EntityChangedEvent.CrudAction.CREATE;
-        } else if (isDeactivated(entity)) {
-            return EntityChangedEvent.CrudAction.REMOVE;
-        } else {
-            return EntityChangedEvent.CrudAction.UPDATE;
-        }
-    }
-
-    private boolean isDeactivated(EntityInVersionStructure entity) {
-        if (entity.getValidBetween() == null) {
-            return false;
-        }
-        return entity.getValidBetween().getToDate() != null;
-    }
-
-    private boolean isLoggedEntity(EntityStructure entity) {
-        return getEntityType(entity) != null;
-    }
-
-    private EntityChangedEvent.EntityType getEntityType(EntityStructure entity) {
-        if (entity instanceof StopPlace) {
-            return EntityChangedEvent.EntityType.STOP_PLACE;
-        }
-        return null;
     }
 }
