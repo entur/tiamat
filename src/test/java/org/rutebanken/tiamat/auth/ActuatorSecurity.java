025a85e8b5703c3f87654f36212b82baa4c8324a
Assad Riaz <assad.riaz@entur.org>
2023-10-26 14:58:39 +0200

    Refactor ActuatorSecurity , removes deprecated WebSecurityConfigurerAdapter

--- src/test/java/org/rutebanken/tiamat/auth/ActuatorSecurity.java
+++ src/test/java/org/rutebanken/tiamat/auth/ActuatorSecurity.java
@@ -1,19 +1,24 @@
 package org.rutebanken.tiamat.auth;
 
-import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
 
 @Configuration
 @EnableWebSecurity
-public class ActuatorSecurity extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+public class ActuatorSecurity {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(withDefaults())
                 .csrf().disable()
                 .authorizeRequests()
-                .requestMatchers(EndpointRequest.to("health")).permitAll();
+                .antMatchers("health").permitAll();
+        return http.build();
     }
 }
