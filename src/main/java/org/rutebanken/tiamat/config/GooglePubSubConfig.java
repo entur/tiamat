4c2fae04467814d63cbb142264f2a319b0399dba
Assad Riaz <assad.riaz@entur.org>
2024-01-08 10:03:02 +0100

    update readme adds profile in GooglePubSubConfig

--- src/main/java/org/rutebanken/tiamat/config/GooglePubSubConfig.java
+++ src/main/java/org/rutebanken/tiamat/config/GooglePubSubConfig.java
@@ -5,11 +5,13 @@ import com.google.cloud.spring.pubsub.integration.outbound.PubSubMessageHandler;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.integration.annotation.MessagingGateway;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.messaging.MessageHandler;
 
 @Configuration
+@Profile("google-pubsub")
 public class GooglePubSubConfig {
     @Value("${changelog.topic.name:ror.tiamat.changelog}")
     private String pubSubTopic;
