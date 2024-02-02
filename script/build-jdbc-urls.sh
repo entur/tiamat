160cf1f7f33d8e6252199574d90de6768c2cd96a
Teemu Mäkinen <teemu.makinen@vincit.fi>
2024-02-02 15:34:37 +0200

    Remove default database connection values

--- script/build-jdbc-urls.sh
+++ script/build-jdbc-urls.sh
@@ -7,8 +7,5 @@ set -eu
 # if TIAMAT_DB_URL was not set, build it from parts
 if [ -z "${TIAMAT_DB_URL+x}" ]; then
   export TIAMAT_DB_PORT=${TIAMAT_DB_PORT:-"5432"}
-  # TODO: Remove default values once secrets work properly
-  export TIAMAT_DB_HOSTNAME=${TIAMAT_DB_HOSTNAME:-"jore4-testdb"}
-  export TIAMAT_DB_DATABASE=${TIAMAT_DB_DATABASE:-"stopdb"}
   export TIAMAT_DB_URL="jdbc:postgresql://${TIAMAT_DB_HOSTNAME}:${TIAMAT_DB_PORT}/${TIAMAT_DB_DATABASE}?stringtype=unspecified"
 fi
