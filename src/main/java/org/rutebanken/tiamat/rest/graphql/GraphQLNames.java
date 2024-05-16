HSLdevcom/jore4-tiamat

Add HSL specific accessibility properties for stop places

src/main/java/org/rutebanken/tiamat/rest/graphql/GraphQLNames.java
@@ -34,6 +34,9 @@ public class GraphQLNames {
     public static final String OUTPUT_TYPE_ACCESSIBILITY_LIMITATIONS = "AccessibilityLimitations";
     public static final String INPUT_TYPE_ACCESSIBILITY_LIMITATIONS = OUTPUT_TYPE_ACCESSIBILITY_LIMITATIONS + INPUT_TYPE_POSTFIX;
 
+    public static final String OUTPUT_TYPE_HSL_ACCESSIBILITY_PROPERTIES = "HslAccessibilityProperties";
+    public static final String INPUT_TYPE_HSL_ACCESSIBILITY_PROPERTIES = OUTPUT_TYPE_HSL_ACCESSIBILITY_PROPERTIES + INPUT_TYPE_POSTFIX;
+
     public static final String OUTPUT_TYPE_ACCESSIBILITY_ASSESSMENT = "AccessibilityAssessment";
     public static final String INPUT_TYPE_ACCESSIBILITY_ASSESSMENT = OUTPUT_TYPE_ACCESSIBILITY_ASSESSMENT + INPUT_TYPE_POSTFIX;
 
@@ -241,6 +244,7 @@ public class GraphQLNames {
     public static final String ACCESSIBILITY_ASSESSMENT = "accessibilityAssessment";
     public static final String LIMITATIONS = "limitations";
     public static final String MOBILITY_IMPAIRED_ACCESS = "mobilityImpairedAccess";
+    public static final String HSL_ACCESSIBILITY_PROPERTIES = "hslAccessibilityProperties";
 
     public static final String CHILDREN = "children";
 
@@ -249,6 +253,27 @@ public class GraphQLNames {
     public static final String ESCALATOR_FREE_ACCESS = "escalatorFreeAccess";
     public static final String LIFT_FREE_ACCESS = "liftFreeAccess";
     public static final String AUDIBLE_SIGNALS_AVAILABLE = "audibleSignalsAvailable";
+
+    // HslAccessibilityProperties
+    public static final String STOP_AREA_SIDE_SLOPE = "stopAreaSideSlope";
+    public static final String STOP_AREA_LENGTHWISE_SLOPE = "stopAreaLengthwiseSlope";
+    public static final String END_RAMP_SLOPE = "endRampSlope";
+    public static final String SHELTER_LANE_DISTANCE = "shelterLaneDistance";
+    public static final String CURB_BACK_OF_RAIL_DISTANCE = "curbBackOfRailDistance";
+    public static final String CURB_DRIVE_SIDE_OF_RAIL_DISTANCE = "curbDriveSideOfRailDistance";
+    public static final String STRUCTURE_LANE_DISTANCE = "structureLaneDistance";
+    public static final String STOP_HEIGHT_FROM_RAIL_TOP = "stopHeightFromRailTop";
+    public static final String STOP_HEIGHT_FROM_SIDEWALK = "stopHeightFromSidewalk";
+    public static final String LOWER_CLEAT_HEIGHT = "lowerCleatHeight";
+    public static final String SERVICE_AREA_WIDTH = "serviceAreaWidth";
+    public static final String SERVICE_AREA_LENGTH = "serviceAreaLength";
+    public static final String PLATFORM_EDGE_WARNING_AREA = "platformEdgeWarningArea";
+    public static final String GUIDANCE_TILES = "guidanceTiles";
+    public static final String GUIDANCE_STRIPE = "guidanceStripe";
+    public static final String SERVICE_AREA_STRIPES = "serviceAreaStripes";
+    public static final String SIDEWALK_ACCESSIBLE_CONNECTION = "sidewalkAccessibleConnection";
+    public static final String STOP_AREA_SURROUNDINGS_ACCESSIBLE = "stopAreaSurroundingsAccessible";
+    public static final String CURVED_STOP = "curvedStop";
 
     public static final String PLACE_EQUIPMENTS = "placeEquipments";
     public static final String TICKETING_EQUIPMENT = "ticketingEquipment";

src/main/java/org/rutebanken/tiamat/rest/graphql/mappers/HslAccessibilityPropertiesMapper.java
@@ -1,0 +1,94 @@
+package org.rutebanken.tiamat.rest.graphql.mappers;
+
+import org.rutebanken.tiamat.model.hsl.HslAccessibilityProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.CURB_BACK_OF_RAIL_DISTANCE;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.CURB_DRIVE_SIDE_OF_RAIL_DISTANCE;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.CURVED_STOP;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.END_RAMP_SLOPE;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.GUIDANCE_TILES;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.GUIDANCE_STRIPE;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.LOWER_CLEAT_HEIGHT;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.PLATFORM_EDGE_WARNING_AREA;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.SERVICE_AREA_LENGTH;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.SERVICE_AREA_STRIPES;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.SERVICE_AREA_WIDTH;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.SHELTER_LANE_DISTANCE;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.SIDEWALK_ACCESSIBLE_CONNECTION;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.STOP_AREA_LENGTHWISE_SLOPE;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.STOP_AREA_SIDE_SLOPE;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.STOP_AREA_SURROUNDINGS_ACCESSIBLE;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.STOP_HEIGHT_FROM_RAIL_TOP;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.STOP_HEIGHT_FROM_SIDEWALK;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.STRUCTURE_LANE_DISTANCE;
+
+@Component
+public class HslAccessibilityPropertiesMapper {
+    public HslAccessibilityProperties mapHslAccessibilityProperties(Map inputMap) {
+        HslAccessibilityProperties hslAccessibilityProperties = new HslAccessibilityProperties();
+
+        if (inputMap.containsKey(STOP_AREA_SIDE_SLOPE)) {
+            hslAccessibilityProperties.setStopAreaSideSlope((Double) inputMap.get(STOP_AREA_SIDE_SLOPE));
+        }
+        if (inputMap.containsKey(STOP_AREA_LENGTHWISE_SLOPE)) {
+            hslAccessibilityProperties.setStopAreaLengthwiseSlope((Double) inputMap.get(STOP_AREA_LENGTHWISE_SLOPE));
+        }
+        if (inputMap.containsKey(END_RAMP_SLOPE)) {
+            hslAccessibilityProperties.setEndRampSlope((Double) inputMap.get(END_RAMP_SLOPE));
+        }
+        if (inputMap.containsKey(SHELTER_LANE_DISTANCE)) {
+            hslAccessibilityProperties.setShelterLaneDistance((Double) inputMap.get(SHELTER_LANE_DISTANCE));
+        }
+        if (inputMap.containsKey(CURB_BACK_OF_RAIL_DISTANCE)) {
+            hslAccessibilityProperties.setCurbBackOfRailDistance((Double) inputMap.get(CURB_BACK_OF_RAIL_DISTANCE));
+        }
+        if (inputMap.containsKey(CURB_DRIVE_SIDE_OF_RAIL_DISTANCE)) {
+            hslAccessibilityProperties.setCurbDriveSideOfRailDistance((Double) inputMap.get(CURB_DRIVE_SIDE_OF_RAIL_DISTANCE));
+        }
+        if (inputMap.containsKey(STRUCTURE_LANE_DISTANCE)) {
+            hslAccessibilityProperties.setStructureLaneDistance((Double) inputMap.get(STRUCTURE_LANE_DISTANCE));
+        }
+        if (inputMap.containsKey(STOP_HEIGHT_FROM_RAIL_TOP)) {
+            hslAccessibilityProperties.setStopHeightFromRailTop((Double) inputMap.get(STOP_HEIGHT_FROM_RAIL_TOP));
+        }
+        if (inputMap.containsKey(STOP_HEIGHT_FROM_SIDEWALK)) {
+            hslAccessibilityProperties.setStopHeightFromSidewalk((Double) inputMap.get(STOP_HEIGHT_FROM_SIDEWALK));
+        }
+        if (inputMap.containsKey(LOWER_CLEAT_HEIGHT)) {
+            hslAccessibilityProperties.setLowerCleatHeight((Double) inputMap.get(LOWER_CLEAT_HEIGHT));
+        }
+        if (inputMap.containsKey(SERVICE_AREA_WIDTH)) {
+            hslAccessibilityProperties.setServiceAreaWidth((Double) inputMap.get(SERVICE_AREA_WIDTH));
+        }
+        if (inputMap.containsKey(SERVICE_AREA_LENGTH)) {
+            hslAccessibilityProperties.setServiceAreaLength((Double) inputMap.get(SERVICE_AREA_LENGTH));
+        }
+        if (inputMap.containsKey(PLATFORM_EDGE_WARNING_AREA)) {
+            hslAccessibilityProperties.setPlatformEdgeWarningArea((Boolean) inputMap.get(PLATFORM_EDGE_WARNING_AREA));
+        }
+        if (inputMap.containsKey(GUIDANCE_TILES)) {
+            hslAccessibilityProperties.setGuidanceTiles((Boolean) inputMap.get(GUIDANCE_TILES));
+        }
+        if (inputMap.containsKey(GUIDANCE_STRIPE)) {
+            hslAccessibilityProperties.setGuidanceStripe((Boolean) inputMap.get(GUIDANCE_STRIPE));
+        }
+        if (inputMap.containsKey(SERVICE_AREA_STRIPES)) {
+            hslAccessibilityProperties.setServiceAreaStripes((Boolean) inputMap.get(SERVICE_AREA_STRIPES));
+        }
+        if (inputMap.containsKey(SIDEWALK_ACCESSIBLE_CONNECTION)) {
+            hslAccessibilityProperties.setSidewalkAccessibleConnection((Boolean) inputMap.get(SIDEWALK_ACCESSIBLE_CONNECTION));
+        }
+        if (inputMap.containsKey(STOP_AREA_SURROUNDINGS_ACCESSIBLE)) {
+            hslAccessibilityProperties.setStopAreaSurroundingsAccessible((Boolean) inputMap.get(STOP_AREA_SURROUNDINGS_ACCESSIBLE));
+        }
+        if (inputMap.containsKey(CURVED_STOP)) {
+            hslAccessibilityProperties.setCurvedStop((Boolean) inputMap.get(CURVED_STOP));
+        }
+
+        return hslAccessibilityProperties;
+    }
+
+}

src/main/resources/db/migration/V47__add_hsl_accessibility_properties.sql
@@ -1,0 +1,51 @@
+-- HSL specific accessibility and measurement fields.
+
+CREATE TABLE hsl_accessibility_properties
+(
+    id                                bigint NOT NULL,
+    netex_id                          character varying(255),
+    changed                           timestamp without time zone,
+    created                           timestamp without time zone,
+    from_date                         timestamp without time zone,
+    to_date                           timestamp without time zone,
+    version                           bigint NOT NULL,
+    version_comment                   character varying(255),
+    changed_by                        character varying(255),
+
+    stop_area_side_slope              numeric(5, 2), -- (%)
+    stop_area_lengthwise_slope        numeric(5, 2), -- (%)
+    end_ramp_slope                    numeric(5, 2), -- (%)
+    shelter_lane_distance             numeric(7, 2), -- (cm)
+    curb_back_of_rail_distance        numeric(7, 2), -- (cm)
+    curb_drive_side_of_rail_distance  numeric(7, 2), -- (cm)
+    structure_lane_distance           numeric(7, 2), -- (cm)
+    stop_height_from_rail_top         numeric(7, 2), -- (cm)
+    stop_height_from_sidewalk         numeric(7, 2), -- (cm)
+    lower_cleat_height                numeric(7, 2), -- (cm)
+    service_area_width                numeric(7, 2), -- (cm)
+    service_area_length               numeric(7, 2), -- (cm)
+    platform_edge_warning_area        boolean,
+    guidance_tiles                    boolean,
+    guidance_stripe                   boolean,
+    service_area_stripes              boolean,
+    sidewalk_accessible_connection    boolean,
+    stop_area_surroundings_accessible boolean,
+    curved_stop                       boolean
+);
+
+ALTER TABLE ONLY hsl_accessibility_properties
+    ADD CONSTRAINT hsl_accessibility_properties_pkey PRIMARY KEY (id);
+
+CREATE SEQUENCE hsl_accessibility_properties_seq
+    START WITH 1
+    INCREMENT BY 10
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+ALTER TABLE hsl_accessibility_properties_seq OWNER TO tiamat;
+
+ALTER TABLE ONLY accessibility_assessment
+    ADD COLUMN hsl_accessibility_properties_id bigint;
+ALTER TABLE ONLY accessibility_assessment
+    ADD CONSTRAINT fk_accessibility_assessment_hsl_accessibility_properties FOREIGN KEY (hsl_accessibility_properties_id) REFERENCES hsl_accessibility_properties (id);

src/test/java/org/rutebanken/tiamat/versioning/AccessibilityAssessmentVersioningTest.java
@@ -20,6 +20,7 @@ import org.rutebanken.tiamat.TiamatIntegrationTest;
 import org.rutebanken.tiamat.model.AccessibilityAssessment;
 import org.rutebanken.tiamat.model.AccessibilityLimitation;
 import org.rutebanken.tiamat.model.EmbeddableMultilingualString;
+import org.rutebanken.tiamat.model.hsl.HslAccessibilityProperties;
 import org.rutebanken.tiamat.model.LimitationStatusEnumeration;
 import org.rutebanken.tiamat.model.Quay;
 import org.rutebanken.tiamat.model.StopPlace;
@@ -182,7 +183,51 @@ public class AccessibilityAssessmentVersioningTest extends TiamatIntegrationTest
 
     }
 
+    @Test
+    public void testAccessibilityVersioningWithHslAccessibilityProperties() {
+        /**
+         * Create StopPlace with a single quay with HslAccessibilityProperties.
+         */
+        HslAccessibilityProperties hslAccessibilityProperties = new HslAccessibilityProperties();
+        hslAccessibilityProperties.setStopAreaSideSlope(5.3);
+        hslAccessibilityProperties.setStopAreaLengthwiseSlope(1.8);
+        hslAccessibilityProperties.setEndRampSlope(5.);
+        hslAccessibilityProperties.setShelterLaneDistance(123.);
+        hslAccessibilityProperties.setCurbBackOfRailDistance(145.);
+        hslAccessibilityProperties.setCurbDriveSideOfRailDistance(5.);
+        hslAccessibilityProperties.setStructureLaneDistance(15.);
+        hslAccessibilityProperties.setStopHeightFromRailTop(10.);
+        hslAccessibilityProperties.setStopHeightFromSidewalk(7.);
+        hslAccessibilityProperties.setLowerCleatHeight(5.);
+        hslAccessibilityProperties.setServiceAreaWidth(460.);
+        hslAccessibilityProperties.setServiceAreaLength(552.);
+        hslAccessibilityProperties.setPlatformEdgeWarningArea(true);
+        hslAccessibilityProperties.setGuidanceTiles(true);
+        hslAccessibilityProperties.setGuidanceStripe(false);
+        hslAccessibilityProperties.setServiceAreaStripes(false);
+        hslAccessibilityProperties.setSidewalkAccessibleConnection(true);
+        hslAccessibilityProperties.setStopAreaSurroundingsAccessible(false);
+        hslAccessibilityProperties.setCurvedStop(true);
 
+        AccessibilityAssessment accessibilityAssessment = createAccessibilityAssessment(TRUE);
+        accessibilityAssessment.setHslAccessibilityProperties(hslAccessibilityProperties);
+        Quay quay1 = new Quay();
+        quay1.setName(new EmbeddableMultilingualString("quay1"));
+        quay1.setAccessibilityAssessment(accessibilityAssessment);
+
+        StopPlace stopPlace = new StopPlace();
+        stopPlace.getQuays().add(quay1);
+
+        StopPlace stopPlace_v1 = stopPlaceVersionedSaverService.saveNewVersion(stopPlace);
+
+        assertThat(stopPlace_v1.getQuays()).isNotNull();
+        assertThat(stopPlace_v1.getAccessibilityAssessment()).isNotNull();
+        assertThat(stopPlace_v1.getAccessibilityAssessment().getVersion()).isEqualTo(1);
+
+        HslAccessibilityProperties actualHslAccessibilityProperties = stopPlace_v1.getAccessibilityAssessment().getHslAccessibilityProperties();
+        assertThat(actualHslAccessibilityProperties).isNotNull();
+        assertThat(actualHslAccessibilityProperties.getVersion()).isEqualTo(1);
+     }
 
 
     protected AccessibilityAssessment createAccessibilityAssessment(LimitationStatusEnumeration limitation) {

entur/tiamat

src/main/java/org/rutebanken/tiamat/rest/graphql/GraphQLNames.java
@@ -34,6 +34,9 @@ public class GraphQLNames {
     public static final String OUTPUT_TYPE_ACCESSIBILITY_LIMITATIONS = "AccessibilityLimitations";
     public static final String INPUT_TYPE_ACCESSIBILITY_LIMITATIONS = OUTPUT_TYPE_ACCESSIBILITY_LIMITATIONS + INPUT_TYPE_POSTFIX;
 
+    public static final String OUTPUT_TYPE_HSL_ACCESSIBILITY_PROPERTIES = "HslAccessibilityProperties";
+    public static final String INPUT_TYPE_HSL_ACCESSIBILITY_PROPERTIES = OUTPUT_TYPE_HSL_ACCESSIBILITY_PROPERTIES + INPUT_TYPE_POSTFIX;
+
     public static final String OUTPUT_TYPE_ACCESSIBILITY_ASSESSMENT = "AccessibilityAssessment";
     public static final String INPUT_TYPE_ACCESSIBILITY_ASSESSMENT = OUTPUT_TYPE_ACCESSIBILITY_ASSESSMENT + INPUT_TYPE_POSTFIX;
 
@@ -241,6 +244,7 @@ public class GraphQLNames {
     public static final String ACCESSIBILITY_ASSESSMENT = "accessibilityAssessment";
     public static final String LIMITATIONS = "limitations";
     public static final String MOBILITY_IMPAIRED_ACCESS = "mobilityImpairedAccess";
+    public static final String HSL_ACCESSIBILITY_PROPERTIES = "hslAccessibilityProperties";
 
     public static final String CHILDREN = "children";
 
@@ -249,6 +253,27 @@ public class GraphQLNames {
     public static final String ESCALATOR_FREE_ACCESS = "escalatorFreeAccess";
     public static final String LIFT_FREE_ACCESS = "liftFreeAccess";
     public static final String AUDIBLE_SIGNALS_AVAILABLE = "audibleSignalsAvailable";
+
+    // HslAccessibilityProperties
+    public static final String STOP_AREA_SIDE_SLOPE = "stopAreaSideSlope";
+    public static final String STOP_AREA_LENGTHWISE_SLOPE = "stopAreaLengthwiseSlope";
+    public static final String END_RAMP_SLOPE = "endRampSlope";
+    public static final String SHELTER_LANE_DISTANCE = "shelterLaneDistance";
+    public static final String CURB_BACK_OF_RAIL_DISTANCE = "curbBackOfRailDistance";
+    public static final String CURB_DRIVE_SIDE_OF_RAIL_DISTANCE = "curbDriveSideOfRailDistance";
+    public static final String STRUCTURE_LANE_DISTANCE = "structureLaneDistance";
+    public static final String STOP_ELEVATION_FROM_RAIL_TOP = "stopElevationFromRailTop";
+    public static final String STOP_ELEVATION_FROM_SIDEWALK = "stopElevationFromSidewalk";
+    public static final String LOWER_CLEAT_HEIGHT = "lowerCleatHeight";
+    public static final String SERVICE_AREA_WIDTH = "serviceAreaWidth";
+    public static final String SERVICE_AREA_LENGTH = "serviceAreaLength";
+    public static final String PLATFORM_EDGE_WARNING_AREA = "platformEdgeWarningArea";
+    public static final String GUIDANCE_TILES = "guidanceTiles";
+    public static final String GUIDANCE_STRIPE = "guidanceStripe";
+    public static final String SERVICE_AREA_STRIPES = "serviceAreaStripes";
+    public static final String SIDEWALK_ACCESSIBLE_CONNECTION = "sidewalkAccessibleConnection";
+    public static final String STOP_AREA_SURROUNDINGS_ACCESSIBLE = "stopAreaSurroundingsAccessible";
+    public static final String CURVED_STOP = "curvedStop";
 
     public static final String PLACE_EQUIPMENTS = "placeEquipments";
     public static final String TICKETING_EQUIPMENT = "ticketingEquipment";

src/main/java/org/rutebanken/tiamat/rest/graphql/mappers/HslAccessibilityPropertiesMapper.java
@@ -1,0 +1,94 @@
+package org.rutebanken.tiamat.rest.graphql.mappers;
+
+import org.rutebanken.tiamat.model.hsl.HslAccessibilityProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.CURB_BACK_OF_RAIL_DISTANCE;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.CURB_DRIVE_SIDE_OF_RAIL_DISTANCE;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.CURVED_STOP;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.END_RAMP_SLOPE;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.GUIDANCE_TILES;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.GUIDANCE_STRIPE;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.LOWER_CLEAT_HEIGHT;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.PLATFORM_EDGE_WARNING_AREA;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.SERVICE_AREA_LENGTH;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.SERVICE_AREA_STRIPES;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.SERVICE_AREA_WIDTH;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.SHELTER_LANE_DISTANCE;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.SIDEWALK_ACCESSIBLE_CONNECTION;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.STOP_AREA_LENGTHWISE_SLOPE;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.STOP_AREA_SIDE_SLOPE;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.STOP_AREA_SURROUNDINGS_ACCESSIBLE;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.STOP_HEIGHT_FROM_RAIL_TOP;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.STOP_HEIGHT_FROM_SIDEWALK;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.STRUCTURE_LANE_DISTANCE;
+
+@Component
+public class HslAccessibilityPropertiesMapper {
+    public HslAccessibilityProperties mapHslAccessibilityProperties(Map inputMap) {
+        HslAccessibilityProperties hslAccessibilityProperties = new HslAccessibilityProperties();
+
+        if (inputMap.containsKey(STOP_AREA_SIDE_SLOPE)) {
+            hslAccessibilityProperties.setStopAreaSideSlope((Double) inputMap.get(STOP_AREA_SIDE_SLOPE));
+        }
+        if (inputMap.containsKey(STOP_AREA_LENGTHWISE_SLOPE)) {
+            hslAccessibilityProperties.setStopAreaLengthwiseSlope((Double) inputMap.get(STOP_AREA_LENGTHWISE_SLOPE));
+        }
+        if (inputMap.containsKey(END_RAMP_SLOPE)) {
+            hslAccessibilityProperties.setEndRampSlope((Double) inputMap.get(END_RAMP_SLOPE));
+        }
+        if (inputMap.containsKey(SHELTER_LANE_DISTANCE)) {
+            hslAccessibilityProperties.setShelterLaneDistance((Double) inputMap.get(SHELTER_LANE_DISTANCE));
+        }
+        if (inputMap.containsKey(CURB_BACK_OF_RAIL_DISTANCE)) {
+            hslAccessibilityProperties.setCurbBackOfRailDistance((Double) inputMap.get(CURB_BACK_OF_RAIL_DISTANCE));
+        }
+        if (inputMap.containsKey(CURB_DRIVE_SIDE_OF_RAIL_DISTANCE)) {
+            hslAccessibilityProperties.setCurbDriveSideOfRailDistance((Double) inputMap.get(CURB_DRIVE_SIDE_OF_RAIL_DISTANCE));
+        }
+        if (inputMap.containsKey(STRUCTURE_LANE_DISTANCE)) {
+            hslAccessibilityProperties.setStructureLaneDistance((Double) inputMap.get(STRUCTURE_LANE_DISTANCE));
+        }
+        if (inputMap.containsKey(STOP_ELEVATION_FROM_RAIL_TOP)) {
+            hslAccessibilityProperties.setStopElevationFromRailTop((Double) inputMap.get(STOP_ELEVATION_FROM_RAIL_TOP));
+        }
+        if (inputMap.containsKey(STOP_ELEVATION_FROM_SIDEWALK)) {
+            hslAccessibilityProperties.setStopElevationFromSidewalk((Double) inputMap.get(STOP_ELEVATION_FROM_SIDEWALK));
+        }
+        if (inputMap.containsKey(LOWER_CLEAT_HEIGHT)) {
+            hslAccessibilityProperties.setLowerCleatHeight((Double) inputMap.get(LOWER_CLEAT_HEIGHT));
+        }
+        if (inputMap.containsKey(SERVICE_AREA_WIDTH)) {
+            hslAccessibilityProperties.setServiceAreaWidth((Double) inputMap.get(SERVICE_AREA_WIDTH));
+        }
+        if (inputMap.containsKey(SERVICE_AREA_LENGTH)) {
+            hslAccessibilityProperties.setServiceAreaLength((Double) inputMap.get(SERVICE_AREA_LENGTH));
+        }
+        if (inputMap.containsKey(PLATFORM_EDGE_WARNING_AREA)) {
+            hslAccessibilityProperties.setPlatformEdgeWarningArea((Boolean) inputMap.get(PLATFORM_EDGE_WARNING_AREA));
+        }
+        if (inputMap.containsKey(GUIDANCE_TILES)) {
+            hslAccessibilityProperties.setGuidanceTiles((Boolean) inputMap.get(GUIDANCE_TILES));
+        }
+        if (inputMap.containsKey(GUIDANCE_STRIPE)) {
+            hslAccessibilityProperties.setGuidanceStripe((Boolean) inputMap.get(GUIDANCE_STRIPE));
+        }
+        if (inputMap.containsKey(SERVICE_AREA_STRIPES)) {
+            hslAccessibilityProperties.setServiceAreaStripes((Boolean) inputMap.get(SERVICE_AREA_STRIPES));
+        }
+        if (inputMap.containsKey(SIDEWALK_ACCESSIBLE_CONNECTION)) {
+            hslAccessibilityProperties.setSidewalkAccessibleConnection((Boolean) inputMap.get(SIDEWALK_ACCESSIBLE_CONNECTION));
+        }
+        if (inputMap.containsKey(STOP_AREA_SURROUNDINGS_ACCESSIBLE)) {
+            hslAccessibilityProperties.setStopAreaSurroundingsAccessible((Boolean) inputMap.get(STOP_AREA_SURROUNDINGS_ACCESSIBLE));
+        }
+        if (inputMap.containsKey(CURVED_STOP)) {
+            hslAccessibilityProperties.setCurvedStop((Boolean) inputMap.get(CURVED_STOP));
+        }
+
+        return hslAccessibilityProperties;
+    }
+
+}

src/main/resources/db/migration/V47__add_hsl_accessibility_properties.sql
@@ -1,0 +1,51 @@
+-- HSL specific accessibility and measurement fields.
+
+CREATE TABLE hsl_accessibility_properties
+(
+    id                                bigint NOT NULL,
+    netex_id                          character varying(255),
+    changed                           timestamp without time zone,
+    created                           timestamp without time zone,
+    from_date                         timestamp without time zone,
+    to_date                           timestamp without time zone,
+    version                           bigint NOT NULL,
+    version_comment                   character varying(255),
+    changed_by                        character varying(255),
+
+    stop_area_side_slope              numeric(5, 2), -- (%)
+    stop_area_lengthwise_slope        numeric(5, 2), -- (%)
+    end_ramp_slope                    numeric(5, 2), -- (%)
+    shelter_lane_distance             numeric(7, 2), -- (cm)
+    curb_back_of_rail_distance        numeric(7, 2), -- (cm)
+    curb_drive_side_of_rail_distance  numeric(7, 2), -- (cm)
+    structure_lane_distance           numeric(7, 2), -- (cm)
+    stop_elevation_from_rail_top      numeric(7, 2), -- (cm)
+    stop_elevation_from_sidewalk      numeric(7, 2), -- (cm)
+    lower_cleat_height                numeric(7, 2), -- (cm)
+    service_area_width                numeric(7, 2), -- (cm)
+    service_area_length               numeric(7, 2), -- (cm)
+    platform_edge_warning_area        boolean,
+    guidance_tiles                    boolean,
+    guidance_stripe                   boolean,
+    service_area_stripes              boolean,
+    sidewalk_accessible_connection    boolean,
+    stop_area_surroundings_accessible boolean,
+    curved_stop                       boolean
+);
+
+ALTER TABLE ONLY hsl_accessibility_properties
+    ADD CONSTRAINT hsl_accessibility_properties_pkey PRIMARY KEY (id);
+
+CREATE SEQUENCE hsl_accessibility_properties_seq
+    START WITH 1
+    INCREMENT BY 10
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+ALTER TABLE hsl_accessibility_properties_seq OWNER TO tiamat;
+
+ALTER TABLE ONLY accessibility_assessment
+    ADD COLUMN hsl_accessibility_properties_id bigint;
+ALTER TABLE ONLY accessibility_assessment
+    ADD CONSTRAINT fk_accessibility_assessment_hsl_accessibility_properties FOREIGN KEY (hsl_accessibility_properties_id) REFERENCES hsl_accessibility_properties (id);

src/test/java/org/rutebanken/tiamat/versioning/AccessibilityAssessmentVersioningTest.java
@@ -20,6 +20,7 @@ import org.rutebanken.tiamat.TiamatIntegrationTest;
 import org.rutebanken.tiamat.model.AccessibilityAssessment;
 import org.rutebanken.tiamat.model.AccessibilityLimitation;
 import org.rutebanken.tiamat.model.EmbeddableMultilingualString;
+import org.rutebanken.tiamat.model.hsl.HslAccessibilityProperties;
 import org.rutebanken.tiamat.model.LimitationStatusEnumeration;
 import org.rutebanken.tiamat.model.Quay;
 import org.rutebanken.tiamat.model.StopPlace;
@@ -182,7 +183,51 @@ public class AccessibilityAssessmentVersioningTest extends TiamatIntegrationTest
 
     }
 
+    @Test
+    public void testAccessibilityVersioningWithHslAccessibilityProperties() {
+        /**
+         * Create StopPlace with a single quay with HslAccessibilityProperties.
+         */
+        HslAccessibilityProperties hslAccessibilityProperties = new HslAccessibilityProperties();
+        hslAccessibilityProperties.setStopAreaSideSlope(5.3);
+        hslAccessibilityProperties.setStopAreaLengthwiseSlope(1.8);
+        hslAccessibilityProperties.setEndRampSlope(5.);
+        hslAccessibilityProperties.setShelterLaneDistance(123.);
+        hslAccessibilityProperties.setCurbBackOfRailDistance(145.);
+        hslAccessibilityProperties.setCurbDriveSideOfRailDistance(5.);
+        hslAccessibilityProperties.setStructureLaneDistance(15.);
+        hslAccessibilityProperties.setStopElevationFromRailTop(10.);
+        hslAccessibilityProperties.setStopElevationFromSidewalk(7.);
+        hslAccessibilityProperties.setLowerCleatHeight(5.);
+        hslAccessibilityProperties.setServiceAreaWidth(460.);
+        hslAccessibilityProperties.setServiceAreaLength(552.);
+        hslAccessibilityProperties.setPlatformEdgeWarningArea(true);
+        hslAccessibilityProperties.setGuidanceTiles(true);
+        hslAccessibilityProperties.setGuidanceStripe(false);
+        hslAccessibilityProperties.setServiceAreaStripes(false);
+        hslAccessibilityProperties.setSidewalkAccessibleConnection(true);
+        hslAccessibilityProperties.setStopAreaSurroundingsAccessible(false);
+        hslAccessibilityProperties.setCurvedStop(true);
 
+        AccessibilityAssessment accessibilityAssessment = createAccessibilityAssessment(TRUE);
+        accessibilityAssessment.setHslAccessibilityProperties(hslAccessibilityProperties);
+        Quay quay1 = new Quay();
+        quay1.setName(new EmbeddableMultilingualString("quay1"));
+        quay1.setAccessibilityAssessment(accessibilityAssessment);
+
+        StopPlace stopPlace = new StopPlace();
+        stopPlace.getQuays().add(quay1);
+
+        StopPlace stopPlace_v1 = stopPlaceVersionedSaverService.saveNewVersion(stopPlace);
+
+        assertThat(stopPlace_v1.getQuays()).isNotNull();
+        assertThat(stopPlace_v1.getAccessibilityAssessment()).isNotNull();
+        assertThat(stopPlace_v1.getAccessibilityAssessment().getVersion()).isEqualTo(1);
+
+        HslAccessibilityProperties actualHslAccessibilityProperties = stopPlace_v1.getAccessibilityAssessment().getHslAccessibilityProperties();
+        assertThat(actualHslAccessibilityProperties).isNotNull();
+        assertThat(actualHslAccessibilityProperties.getVersion()).isEqualTo(1);
+     }
 
 
     protected AccessibilityAssessment createAccessibilityAssessment(LimitationStatusEnumeration limitation) {