b5ff8d8603031ca72bcb8f9eafcb762c88f0b9a7
Tommi Leinamo <tommi.leinamo@vincit.fi>
2024-05-17 09:46:53 +0300

    Add SQL comments to HslAccessibilityProperties table fields
    Identical what is already in GraphQL API comments.

--- src/main/resources/db/migration/V47__add_hsl_accessibility_properties.sql
+++ src/main/resources/db/migration/V47__add_hsl_accessibility_properties.sql
@@ -38,6 +38,32 @@ CREATE TABLE hsl_accessibility_properties
     pedestrian_crossing_ramp_type     character varying(255)
 );
 
+-- Add comments mostly for the direct Tiamat DB connection.
+COMMENT ON COLUMN hsl_accessibility_properties.stop_area_side_slope IS 'Pysäkkialueen sivukaltevuus (%)';
+COMMENT ON COLUMN hsl_accessibility_properties.stop_area_lengthwise_slope IS 'Pysäkkialueen pituuskaltevuus (%)';
+COMMENT ON COLUMN hsl_accessibility_properties.end_ramp_slope IS 'Päätyluiskan kaltevuus (%)';
+COMMENT ON COLUMN hsl_accessibility_properties.shelter_lane_distance IS 'Katoksen ja ajoradan välinen leveys (cm)';
+COMMENT ON COLUMN hsl_accessibility_properties.curb_back_of_rail_distance IS 'Reunakiven etäisyys kiskon selästä (cm)';
+COMMENT ON COLUMN hsl_accessibility_properties.curb_drive_side_of_rail_distance IS 'Reunakiven etäisyys kiskon ajoreunasta (cm)';
+COMMENT ON COLUMN hsl_accessibility_properties.structure_lane_distance IS 'Rakenteiden ja ajoradan välinen pienin leveys (cm)';
+COMMENT ON COLUMN hsl_accessibility_properties.stop_elevation_from_rail_top IS 'Pysäkin korotus kiskon ajopintaan nähden (cm)';
+COMMENT ON COLUMN hsl_accessibility_properties.stop_elevation_from_sidewalk IS 'Pysäkin korotus jalkakäytävään nähden (cm)';
+COMMENT ON COLUMN hsl_accessibility_properties.lower_cleat_height IS 'Alapienan korkeus (cm)';
+COMMENT ON COLUMN hsl_accessibility_properties.service_area_width IS 'Palvelualueen leveys (m)';
+COMMENT ON COLUMN hsl_accessibility_properties.service_area_length IS 'Palvelualueen pituus (m)';
+COMMENT ON COLUMN hsl_accessibility_properties.platform_edge_warning_area IS 'Pysäkkialueen varoitusalue';
+COMMENT ON COLUMN hsl_accessibility_properties.guidance_tiles IS 'Opaslaatat';
+COMMENT ON COLUMN hsl_accessibility_properties.guidance_stripe IS 'Opasteraita';
+COMMENT ON COLUMN hsl_accessibility_properties.service_area_stripes IS 'Palvelualueen raidoitus';
+COMMENT ON COLUMN hsl_accessibility_properties.sidewalk_accessible_connection IS 'Esteetön yhteys jalkakäytävältä pysäkille';
+COMMENT ON COLUMN hsl_accessibility_properties.stop_area_surroundings_accessible IS 'Pysäkin ympäristo: Esteellinen / Esteetön';
+COMMENT ON COLUMN hsl_accessibility_properties.curved_stop IS 'Kaareva pysäkki';
+COMMENT ON COLUMN hsl_accessibility_properties.stop_type IS 'Pysäkin tyyppi: Syvennys (pullOut) / Uloke (busBulb) / Ajoradalla (inLane) / Muu (other)';
+COMMENT ON COLUMN hsl_accessibility_properties.shelter_type IS 'Katoksen tyyppi: Leveä (wide) / Kapea (narrow) / Muu (other)';
+COMMENT ON COLUMN hsl_accessibility_properties.guidance_type IS 'Opasteiden tyyppi: Pisteopaste (braille) / Ei opastetta (none) / Muu opastus (other)';
+COMMENT ON COLUMN hsl_accessibility_properties.map_type IS 'Kartan tyyppi: Kohokartta (tactile) / Ei karttaa (none) / Muu kartta (other)';
+COMMENT ON COLUMN hsl_accessibility_properties.pedestrian_crossing_ramp_type IS 'Suojatien luiskaus';
+
 ALTER TABLE ONLY hsl_accessibility_properties
     ADD CONSTRAINT hsl_accessibility_properties_pkey PRIMARY KEY (id);
 
