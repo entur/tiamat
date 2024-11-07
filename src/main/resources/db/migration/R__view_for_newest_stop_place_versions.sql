e2d81c097eaa63516f70fb6808e28d440a4b0838
Jesse Jaara <jesse.jaara@vincit.fi>
2024-11-07 13:42:28 +0200

    Add quay publicCode & streetAddress to stop_place_newest_version

--- src/main/resources/db/migration/R__view_for_newest_stop_place_versions.sql
+++ src/main/resources/db/migration/R__view_for_newest_stop_place_versions.sql
@@ -1,5 +1,19 @@
-create or replace view stop_place_newest_version as
-  select distinct on (netex_id) *
-  from stop_place
-  order by netex_id, version desc;
+DROP VIEW IF EXISTS stop_place_newest_version;
+
+CREATE VIEW stop_place_newest_version AS
+SELECT DISTINCT ON (sp.netex_id)
+    sp.*,
+    quay.public_code AS quay_public_code,
+    address_value.items as street_address
+
+FROM stop_place AS sp
+
+         INNER JOIN stop_place_quays AS spq ON sp.id = spq.stop_place_id
+         INNER JOIN quay ON spq.quays_id = quay.id
+
+         LEFT JOIN stop_place_key_values AS spkv ON
+             sp.id = spkv.stop_place_id AND spkv.key_values_key = 'streetAddress'
+         LEFT JOIN value_items AS address_value ON spkv.key_values_id = address_value.value_id
+
+ORDER BY netex_id ASC, version DESC
 
