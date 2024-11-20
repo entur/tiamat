bdf12920ddaa2075e068bb5a9bc6163d7e5c658d
Jesse Jaara <jesse.jaara@vincit.fi>
2024-11-20 14:13:37 +0200

    Add priority, validityStart/end to newest_stop_place view

--- src/main/resources/db/migration/R__view_for_newest_stop_place_versions.sql
+++ src/main/resources/db/migration/R__view_for_newest_stop_place_versions.sql
@@ -4,16 +4,31 @@ CREATE VIEW stop_place_newest_version AS
 SELECT DISTINCT ON (sp.netex_id)
     sp.*,
     quay.public_code AS quay_public_code,
-    address_value.items as street_address
+    streetAddress.items AS street_address,
+    priority.items AS priority,
+    validityStart.items AS validity_start,
+    validityEnd.items AS validity_end
 
 FROM stop_place AS sp
 
          INNER JOIN stop_place_quays AS spq ON sp.id = spq.stop_place_id
          INNER JOIN quay ON spq.quays_id = quay.id
 
-         LEFT JOIN stop_place_key_values AS spkv ON
-             sp.id = spkv.stop_place_id AND spkv.key_values_key = 'streetAddress'
-         LEFT JOIN value_items AS address_value ON spkv.key_values_id = address_value.value_id
+         LEFT JOIN stop_place_key_values AS spkvAddress ON
+             sp.id = spkvAddress.stop_place_id AND spkvAddress.key_values_key = 'streetAddress'
+         LEFT JOIN value_items AS streetAddress ON spkvAddress.key_values_id = streetAddress.value_id
+
+         LEFT JOIN stop_place_key_values AS spkvPriority ON
+            sp.id = spkvPriority.stop_place_id AND spkvPriority.key_values_key = 'priority'
+         LEFT JOIN value_items AS priority ON spkvPriority.key_values_id = priority.value_id
+
+         LEFT JOIN stop_place_key_values AS spkvValidityStart ON
+            sp.id = spkvValidityStart.stop_place_id AND spkvValidityStart.key_values_key = 'validityStart'
+         LEFT JOIN value_items AS validityStart ON spkvValidityStart.key_values_id = validityStart.value_id
+
+         LEFT JOIN stop_place_key_values AS spkvValidityEnd ON
+            sp.id = spkvValidityEnd.stop_place_id AND spkvValidityEnd.key_values_key = 'validityEnd'
+         LEFT JOIN value_items AS validityEnd ON spkvValidityEnd.key_values_id = validityEnd.value_id
 
 ORDER BY netex_id ASC, version DESC
 
