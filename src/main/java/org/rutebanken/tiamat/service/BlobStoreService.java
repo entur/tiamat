d0cac6c70a0fd88b69ed3b5610c3858344426f14
Esko Suomi <esko.suomi@solita.fi>
2024-12-13 13:22:54 +0200

    remove unnecessary methods from BlobStoreService which are GCS implementation specific and are not used outside it either

--- src/main/java/org/rutebanken/tiamat/service/BlobStoreService.java
+++ src/main/java/org/rutebanken/tiamat/service/BlobStoreService.java
@@ -1,15 +1,9 @@
 package org.rutebanken.tiamat.service;
 
-import com.google.cloud.storage.Storage;
-
 import java.io.InputStream;
 
 public interface BlobStoreService {
     void upload(String fileName, InputStream inputStream);
 
-    Storage getStorage();
-
     InputStream download(String fileName);
-
-    String createBlobIdName(String blobPath, String fileName);
 }
