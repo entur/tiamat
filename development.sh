c71458d9cb95cdec308f544b3dbab5609cbb5cf7
Jarkko Kaura <jarkko.kaura@vincit.fi>
2025-01-10 20:29:30 +0200

    From now on, download the JORE4 Docker Compose bundle from the "jore4-docker-compose-bundle" repository.

--- development.sh
+++ development.sh
@@ -8,8 +8,70 @@ cd "${WD}"
 
 DOCKER_COMPOSE_CMD="docker  compose -f ./docker/docker-compose.yml -f ./docker/docker-compose.custom.yml"
 
-function download_docker_compose_bundle {
-  curl https://raw.githubusercontent.com/HSLdevcom/jore4-tools/main/docker/download-docker-bundle.sh | bash
+# Download Docker Compose bundle from the "jore4-docker-compose-bundle"
+# repository. GitHub CLI is required to be installed.
+#
+# A commit reference can be given as an argument. It can contain, for example,
+# only a substring of an actual SHA digest.
+download_docker_compose_bundle() {
+  local commit_ref="${1:-main}"
+
+  local repo_name="jore4-docker-compose-bundle"
+  local repo_owner="HSLdevcom"
+
+  # Check GitHub CLI availability.
+  if ! command -v gh &> /dev/null; then
+    echo "Please install the GitHub CLI (gh) on your machine."
+    exit 1
+  fi
+
+  # Make sure the user is authenticated to GitHub.
+  gh auth status || gh auth login
+
+  echo "Using the commit reference '${commit_ref}' to fetch a Docker Compose bundle..."
+
+  # First, try to find a commit on GitHub that matches the given reference.
+  # This function exits with an error code if no matching commit is found.
+  local commit_sha
+  commit_sha=$(
+    gh api \
+      -H "Accept: application/vnd.github+json" \
+      -H "X-GitHub-Api-Version: 2022-11-28" \
+      "repos/${repo_owner}/${repo_name}/commits/${commit_ref}" \
+      --jq '.sha'
+  )
+
+  echo "Commit with the following SHA digest was found: ${commit_sha}"
+
+  local zip_file="/tmp/${repo_name}.zip"
+  local unzip_target_dir_prefix="/tmp/${repo_owner}-${repo_name}"
+
+  # Remove old temporary directories if any remain.
+  rm -fr "$unzip_target_dir_prefix"-*
+
+  echo "Downloading the JORE4 Docker Compose bundle..."
+
+  # Download the latest Docker Compose bundle from the
+  # jore4-docker-compose-bundle repository as a ZIP file and extract its
+  # contents to a temporary directory.
+  gh api "repos/${repo_owner}/${repo_name}/zipball/${commit_sha}" > "$zip_file" \
+    && unzip -q "$zip_file" -d /tmp
+
+  # Clean untracked files from `docker` directory even if they are git-ignored.
+  git clean -fx ./docker
+
+  # Copy files from the `docker-compose` directory of the ZIP file to your
+  # local `docker` directory.
+  mv "$unzip_target_dir_prefix"-*/docker-compose/* ./docker
+
+  # Remove the temporary files and directories created above.
+  rm -fr "$zip_file" "$unzip_target_dir_prefix"-*
+
+  echo "Generating a release version file for the downloaded bundle..."
+
+  # Create a release version file containing the SHA digest of the referenced
+  # commit.
+  echo "$commit_sha" > ./docker/RELEASE_VERSION.txt
 }
 
 function start {
@@ -43,11 +105,21 @@ function print_usage {
   build
     Build the project locally
 
-  start
-    Start Tiamat service in Docker container
+  start [<commit SHA>]
+    Start Tiamat service in Docker container.
 
-  start:deps
-    Start the dependencies of jore4-tiamat
+    Optionally, you can pass an SHA digest as an argument (or its initial
+    substring) to point to a commit (of the jore4-docker-compose-bundle
+    repository), which specifies the Docker Compose bundle version to fetch. By
+    default, the tip of the main branch is used.
+
+  start:deps [<commit SHA>]
+    Start the dependencies of jore4-tiamat.
+
+    Optionally, you can pass an SHA digest as an argument (or its initial
+    substring) to point to a commit (of the jore4-docker-compose-bundle
+    repository), which specifies the Docker Compose bundle version to fetch. By
+    default, the tip of the main branch is used.
 
   stop
     Stop Tiamat Docker container and all dependencies
@@ -70,14 +142,18 @@ if [[ -z $COMMAND ]]; then
   exit 1
 fi
 
+# Shift other arguments after the command so that we can refer to them later
+# with "$@".
+shift
+
 case $COMMAND in
   start)
-    download_docker_compose_bundle
+    download_docker_compose_bundle "$@"
     start
     ;;
 
   start:deps)
-    download_docker_compose_bundle
+    download_docker_compose_bundle "$@"
     start_dependencies
     ;;
 
