4a0f6ae25ec4a2ed5bd9f57aa05073b64169d0a9
Jarkko Kaura <jarkko.kaura@vincit.fi>
2025-01-10 20:24:53 +0200

    Remove the `docker` directory creation from the `development.sh` file, as the directory has already been checked into version control.

--- development.sh
+++ development.sh
@@ -6,9 +6,6 @@ set -euo pipefail
 WD=$(dirname "$0")
 cd "${WD}"
 
-# initialize package folder
-mkdir -p ./docker
-
 DOCKER_COMPOSE_CMD="docker  compose -f ./docker/docker-compose.yml -f ./docker/docker-compose.custom.yml"
 
 function check_docker {
