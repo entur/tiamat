023bf637f67764d83be1e13daf18f2a8386a6d45
Esko Suomi <esko.suomi@solita.fi>
2025-03-13 13:05:48 +0200

    rework permissions to cascade checks to allow higher level definitions and use transport mode instead of codespace as the middle part to produce a lot simpler and easier to manage data structure for permission management

--- src/ext/java/org/rutebanken/tiamat/ext/fintraffic/auth/FintrafficAuthorizationService.java
+++ src/ext/java/org/rutebanken/tiamat/ext/fintraffic/auth/FintrafficAuthorizationService.java
@@ -1,58 +1,30 @@
 package org.rutebanken.tiamat.ext.fintraffic.auth;
 
 import org.locationtech.jts.geom.Point;
-import org.rutebanken.helper.organisation.RoleAssignment;
 import org.rutebanken.tiamat.auth.AuthorizationService;
-import org.rutebanken.tiamat.model.AirSubmodeEnumeration;
-import org.rutebanken.tiamat.model.BusSubmodeEnumeration;
-import org.rutebanken.tiamat.model.CoachSubmodeEnumeration;
+import org.rutebanken.tiamat.diff.generic.SubmodeEnumuration;
 import org.rutebanken.tiamat.model.EntityStructure;
-import org.rutebanken.tiamat.model.FunicularSubmodeEnumeration;
-import org.rutebanken.tiamat.model.MetroSubmodeEnumeration;
-import org.rutebanken.tiamat.model.RailSubmodeEnumeration;
-import org.rutebanken.tiamat.model.SelfDriveSubmodeEnumeration;
+import org.rutebanken.tiamat.model.StopPlace;
 import org.rutebanken.tiamat.model.StopTypeEnumeration;
-import org.rutebanken.tiamat.model.TaxiSubmodeEnumeration;
-import org.rutebanken.tiamat.model.TelecabinSubmodeEnumeration;
-import org.rutebanken.tiamat.model.TramSubmodeEnumeration;
-import org.rutebanken.tiamat.model.VehicleModeEnumeration;
-import org.rutebanken.tiamat.model.WaterSubmodeEnumeration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.access.AccessDeniedException;
 
-import java.util.Arrays;
 import java.util.Collection;
+import java.util.EnumSet;
 import java.util.Set;
-import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
+import static org.rutebanken.tiamat.ext.fintraffic.auth.TrivoreAuthorizations.ENTITY_TYPE_ALL;
+import static org.rutebanken.tiamat.ext.fintraffic.auth.TrivoreAuthorizations.TRANSPORT_MODE_ALL;
+import static org.rutebanken.tiamat.ext.fintraffic.auth.TrivorePermission.ADMINISTER;
+import static org.rutebanken.tiamat.ext.fintraffic.auth.TrivorePermission.MANAGE;
 
 public class FintrafficAuthorizationService implements AuthorizationService {
-    private static final Set<String> ALL_STOP_PLACE_TYPES = Arrays.stream(StopTypeEnumeration.values())
-            .map(StopTypeEnumeration::value)
-            .collect(Collectors.toSet());
+    private static final Set<StopTypeEnumeration> ALL_STOP_PLACE_TYPES = EnumSet.allOf(StopTypeEnumeration.class);
 
-    //private static final Set<String> ALL_STOP_PLACE_TYPES = Set.of("airSubmode", "busSubmode", "coachSubmode", "funicularSubmode", "metroSubmode", "tramSubmode", "telecabinSubmode", "railSubmode", "waterSubmode");
-    //private static final Set<String> ALL_SUBMODES = Set.of("airSubmode", "busSubmode", "coachSubmode", "funicularSubmode", "metroSubmode", "tramSubmode", "telecabinSubmode", "railSubmode", "waterSubmode");
-
-    private static final Set<String> ALL_SUBMODES = Stream.of(
-                    Arrays.stream(AirSubmodeEnumeration.values()).map(AirSubmodeEnumeration::value),
-                    Arrays.stream(BusSubmodeEnumeration.values()).map(BusSubmodeEnumeration::value),
-                    Arrays.stream(CoachSubmodeEnumeration.values()).map(CoachSubmodeEnumeration::value),
-                    Arrays.stream(FunicularSubmodeEnumeration.values()).map(FunicularSubmodeEnumeration::value),
-                    Arrays.stream(MetroSubmodeEnumeration.values()).map(MetroSubmodeEnumeration::value),
-                    Arrays.stream(RailSubmodeEnumeration.values()).map(RailSubmodeEnumeration::value),
-                    Arrays.stream(SelfDriveSubmodeEnumeration.values()).map(SelfDriveSubmodeEnumeration::value),
-                    Arrays.stream(TaxiSubmodeEnumeration.values()).map(TaxiSubmodeEnumeration::value),
-                    Arrays.stream(TelecabinSubmodeEnumeration.values()).map(TelecabinSubmodeEnumeration::value),
-                    Arrays.stream(TramSubmodeEnumeration.values()).map(TramSubmodeEnumeration::value),
-                    Arrays.stream(VehicleModeEnumeration.values()).map(VehicleModeEnumeration::value),
-                    Arrays.stream(WaterSubmodeEnumeration.values()).map(WaterSubmodeEnumeration::value))
-            .flatMap(Function.identity())
-            .collect(Collectors.toSet());
+    private static final Set<SubmodeEnumuration> ALL_SUBMODES = EnumSet.allOf(SubmodeEnumuration.class);
 
     private final Logger logger = LoggerFactory.getLogger(FintrafficAuthorizationService.class);
 
@@ -63,19 +35,14 @@ public class FintrafficAuthorizationService implements AuthorizationService {
     }
 
     @Override
-    public boolean verifyCanEditAllEntities() {
-        boolean allowed = trivoreAuthorizations.canEditAllEntities();
-        logger.debug("verifyCanEditAllEntities() = {}", allowed);
-        return allowed;
+    public boolean canEditAllEntities() {
+        return trivoreAuthorizations.hasAccess(ENTITY_TYPE_ALL, TRANSPORT_MODE_ALL, MANAGE);
     }
 
     @Override
     public boolean canEditEntities(Collection<? extends EntityStructure> entities) {
-        boolean allowed = trivoreAuthorizations.canManageAllEntities()
-                || trivoreAuthorizations.canEditAllEntities()
+        return trivoreAuthorizations.hasAccess(ENTITY_TYPE_ALL, TRANSPORT_MODE_ALL, MANAGE)
                 || entities.stream().allMatch(this::canEditEntity);
-        logger.debug("canEditEntities(...) = {}", allowed);
-        return allowed;
     }
 
     @Override
@@ -95,98 +62,88 @@ public class FintrafficAuthorizationService implements AuthorizationService {
     @Override
     public boolean canDeleteEntity(EntityStructure entity) {
         String codespace = getCodespace(entity.getNetexId());
-        boolean allowed = trivoreAuthorizations.canManageAllEntities()
-                || trivoreAuthorizations.canDeleteAllEntities()
-                || trivoreAuthorizations.canManageCodespaceEntities(codespace)
-                || trivoreAuthorizations.canDeleteCodespaceEntities(codespace);
-        logger.debug("canDeleteEntity({}:{}, {}) = {}", entity.getId(), entity.getClass().getSimpleName(), entity.getNetexId(), allowed);
-        return allowed;
+        return trivoreAuthorizations.hasAccess(detectEntityType(entity), detectTransportMode(entity), ADMINISTER)
+                && trivoreAuthorizations.hasAccessToCodespace(codespace);
     }
 
     @Override
     public boolean canEditEntity(EntityStructure entity) {
         String codespace = getCodespace(entity.getNetexId());
-        boolean allowed = trivoreAuthorizations.canManageAllEntities()
-                || trivoreAuthorizations.canEditAllEntities()
-                || trivoreAuthorizations.canManageCodespaceEntities(codespace)
-                || trivoreAuthorizations.canEditEntities(codespace);
-        logger.debug("canEditEntity({}:{}, {}) = {}", entity.getId(), entity.getClass().getSimpleName(), entity.getNetexId(), allowed);
-        return allowed;
+        return trivoreAuthorizations.hasAccess(detectEntityType(entity), detectTransportMode(entity), MANAGE)
+                && trivoreAuthorizations.hasAccessToCodespace(codespace);
+    }
+
+    private static String detectEntityType(EntityStructure entity) {
+        return entity.getClass().getSimpleName();
+    }
+
+    private static String detectTransportMode(EntityStructure entity) {
+        return switch (entity) {
+            case StopPlace sp -> sp.getTransportMode().value();
+            default -> TRANSPORT_MODE_ALL;
+        };
     }
 
     @Override
     public boolean canEditEntity(Point point) {
         logger.trace("FintrafficAuthorizationService.canEditEntity({})", point);
-        return true;  // TODO: implement geography support
+        return true;  // TODO: implement geofencing support
     }
 
     @Override
-    public <T extends EntityStructure> Set<String> getRelevantRolesForEntity(T entity) {
-        // TODO: will be removed
-        return Set.of();
-    }
-
-    @Override
-    public <T extends EntityStructure> boolean canEditEntity(RoleAssignment roleAssignment, T entity) {
-        // TODO: will be removed
-        return canEditEntity(entity);
-    }
-
-    @Override
-    public Set<String> getAllowedStopPlaceTypes(EntityStructure entity) {
+    public Set<StopTypeEnumeration> getAllowedStopPlaceTypes(EntityStructure entity) {
         logger.trace("FintrafficAuthorizationService.getAllowedStopPlaceTypes({}, {})", entity.getId(), entity.getNetexId());
         return ALL_STOP_PLACE_TYPES;
     }
 
     @Override
-    public Set<String> getLocationAllowedStopPlaceTypes(boolean canEdit, Point point) {
+    public Set<StopTypeEnumeration> getLocationAllowedStopPlaceTypes(boolean canEdit, Point point) {
         logger.trace("FintrafficAuthorizationService.getLocationAllowedStopPlaceTypes({}, {})", canEdit, point);
         return ALL_STOP_PLACE_TYPES;
     }
 
     @Override
-    public Set<String> getBannedStopPlaceTypes(EntityStructure entity) {
+    public Set<StopTypeEnumeration> getBannedStopPlaceTypes(EntityStructure entity) {
         logger.trace("FintrafficAuthorizationService.getBannedStopPlaceTypes({}, {})", entity.getId(), entity.getNetexId());
         return Set.of();
     }
 
     @Override
-    public Set<String> getLocationBannedStopPlaceTypes(boolean canEdit, Point point) {
+    public Set<StopTypeEnumeration> getLocationBannedStopPlaceTypes(boolean canEdit, Point point) {
         logger.trace("FintrafficAuthorizationService.getLocationBannedStopPlaceTypes({}, {})", canEdit, point);
         return Set.of();
     }
 
     @Override
-    public Set<String> getAllowedSubmodes(EntityStructure entity) {
+    public Set<SubmodeEnumuration> getAllowedSubmodes(EntityStructure entity) {
         logger.trace("FintrafficAuthorizationService.getAllowedSubmodes({}, {})", entity.getId(), entity.getNetexId());
         return ALL_SUBMODES;
     }
 
     @Override
-    public Set<String> getLocationAllowedSubmodes(boolean canEdit, Point point) {
+    public Set<SubmodeEnumuration> getLocationAllowedSubmodes(boolean canEdit, Point point) {
         logger.trace("FintrafficAuthorizationService.getLocationAllowedSubmodes({}, {})", canEdit, point);
         return ALL_SUBMODES;
     }
 
     @Override
-    public Set<String> getBannedSubmodes(EntityStructure entity) {
+    public Set<SubmodeEnumuration> getBannedSubmodes(EntityStructure entity) {
         logger.trace("FintrafficAuthorizationService.getBannedSubmodes({}, {})", entity.getId(), entity.getNetexId());
         return Set.of();
     }
 
     @Override
-    public Set<String> getLocationBannedSubmodes(boolean canEdit, Point point) {
+    public Set<SubmodeEnumuration> getLocationBannedSubmodes(boolean canEdit, Point point) {
         logger.trace("FintrafficAuthorizationService.getLocationBannedSubmodes({}, {})", canEdit, point);
         return Set.of();
     }
 
     @Override
     public boolean isGuest() {
-        logger.trace("FintrafficAuthorizationService.isGuest()");
         return !trivoreAuthorizations.isAuthenticated();
     }
 
-    private final Pattern netexIdPattern = Pattern.compile("[A-Z]{3}:([^:]*):([^:]*)");
+    private final Pattern netexIdPattern = Pattern.compile("([A-Z]{3}):([^:]*):([^:]*)");
     private String getCodespace(String netexId) {
         Matcher matcher = netexIdPattern.matcher(netexId);
         if (matcher.matches()) {
--- src/ext/java/org/rutebanken/tiamat/ext/fintraffic/auth/model/GroupMembership.java
+++ src/ext/java/org/rutebanken/tiamat/ext/fintraffic/auth/model/GroupMembership.java
@@ -1,8 +1,12 @@
 package org.rutebanken.tiamat.ext.fintraffic.auth.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.Splitter;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public record GroupMembership(String id,
                               String name,
@@ -11,4 +15,23 @@ public record GroupMembership(String id,
                               String eligibleFrom,
                               String eligibleUntil,
                               @JsonProperty("custom_fields") Map<String, Object> customFields) {
+
+
+    /**
+     * Metadata field which contains list of NeTEx codespaces members of the group are allowed to access.
+     */
+    private static final String CUSTOM_FIELD_CODESPACE = "codespaces";
+
+    public Set<String> getCodespaces() {
+        if (customFields() != null) {
+            String codespaceField = customFields().getOrDefault(CUSTOM_FIELD_CODESPACE, "").toString();
+            List<String> codespaces = Splitter.on(",")
+                    .trimResults()
+                    .omitEmptyStrings()
+                    .splitToList(codespaceField);
+            return new HashSet<>(codespaces);
+        }
+        return Set.of();
+    }
+
 }
