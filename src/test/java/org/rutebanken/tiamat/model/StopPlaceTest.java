dd519720b272e139d3a624c259bc16e14f078d37
Assad Riaz <assad.riaz@entur.org>
2026-01-25 22:27:39 +0100

    removed unused import from StopPlaceTest

--- src/test/java/org/rutebanken/tiamat/model/StopPlaceTest.java
+++ src/test/java/org/rutebanken/tiamat/model/StopPlaceTest.java
@@ -36,7 +36,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+
 
 import static org.assertj.core.api.Assertions.assertThat;
 
